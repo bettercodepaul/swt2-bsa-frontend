0014c47c12698e54bf23cc302b8c965b0e1eed74
Alexa Eble <aeble@exxcellent.local>
2018-09-12 16:47:24 +0200

    BSAPP-15: Sidebar and Navigationbar

--- bogenliga/src/app/app.component.ts
+++ bogenliga/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bla';
+
+  public isActive = true;
+
+  public toggle(): void {
+    this.isActive = !this.isActive;
+  }
 }
--- bogenliga/src/app/app.module.ts
+++ bogenliga/src/app/app.module.ts
@@ -13,4 +13,6 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
+
+
