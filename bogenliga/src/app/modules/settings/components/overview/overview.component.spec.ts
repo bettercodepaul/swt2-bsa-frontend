9584ed84a537773f29d22619b6d1727402ef209e
Alexa Eble <aeble@exxcellent.local>
2018-10-02 08:41:38 +0200

    BSAPP-15: Tests Overview

--- bogenliga/src/app/modules/settings/components/overview/overview.component.spec.ts
+++ bogenliga/src/app/modules/settings/components/overview/overview.component.spec.ts
@@ -9,12 +9,15 @@ import {LoginModule} from '../../../login/login.module';
 import {StoreModule} from '@ngrx/store';
 import {APP_REDUCERS} from '../../../shared/redux-store';
 import {HttpClientModule} from '@angular/common/http';
-import {DebugElement} from '@angular/core';
 import {By} from '@angular/platform-browser';
+import {DataService} from '../../services/data.service';
+import {Data} from '../../types/data';
+import {Observable} from 'rxjs';
 
 describe('OverviewComponent', () => {
   let component: OverviewComponent;
   let fixture: ComponentFixture<OverviewComponent>;
+  let service: DataService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -34,6 +37,10 @@ describe('OverviewComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(OverviewComponent);
     component = fixture.componentInstance;
+    service = TestBed.get(DataService);
+    spyOn(service, 'findAll').and.callFake(function(): Observable<Data[]> {
+      return new Observable<Data[]>(); // return no data, because data is manually added
+    });
     fixture.detectChanges();
   });
 
@@ -178,4 +185,35 @@ describe('OverviewComponent', () => {
     component.sortDataByValue();
     expect(component.datas).toEqual(orderedDatas);
   });
+
+  it('calculate first page', () => {
+    component.activePage = 2;
+    component.maxOnPage = 5;
+    component.first = 1;
+    component.last = 5;
+    fixture.detectChanges();
+    component.calculatePagination(9);
+    fixture.detectChanges();
+    component.firstPage();
+    fixture.detectChanges();
+    expect(component.first).toBe(1);
+    expect(component.last).toBe(5);
+    expect(component.activePage).toBe(1);
+  });
+
+  it('calculate last page', () => {
+    component.activePage = 1;
+    component.first = 1;
+    component.last = 3;
+    component.maxOnPage = 3;
+    fixture.detectChanges();
+    component.calculatePagination(9);
+    fixture.detectChanges();
+    component.lastPage();
+    fixture.detectChanges();
+    expect(component.first).toBe(7);
+    expect(component.last).toBe(9);
+    expect(component.activePage).toBe(3);
+  });
+
 });
