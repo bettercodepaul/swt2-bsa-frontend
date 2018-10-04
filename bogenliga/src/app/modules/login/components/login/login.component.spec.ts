8a119786e6f341866ae2e82b5aadc23b4712561a
Lehnert Andre; EXXCELLENT SOLUTIONS <Andre.Lehnert@partner.bmw.de>
2018-10-04 23:54:01 +0200

    BSAPP-14: Fix test dependency

--- bogenliga/src/app/modules/login/components/login/login.component.spec.ts
+++ bogenliga/src/app/modules/login/components/login/login.component.spec.ts
@@ -1,15 +1,14 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { LoginComponent } from './login.component';
+import {LoginComponent} from './login.component';
 import {RouterTestingModule} from '@angular/router/testing';
 import {ROUTES} from '../../../../app.routing';
 import {HomeModule} from '../../../home/home.module';
-import {LoginModule} from '../../login.module';
 import {TranslateModule} from '@ngx-translate/core';
 import {StoreModule} from '@ngrx/store';
 import {APP_REDUCERS} from '../../../shared/redux-store';
-import {HttpClientModule} from '@angular/common/http';
 import {SharedModule} from '../../../shared/shared.module';
+import {LocalDataProviderModule} from '../../../shared/local-data-provider/local-data-provider.module';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -23,7 +22,8 @@ describe('LoginComponent', () => {
         HomeModule,
         TranslateModule.forRoot(),
         StoreModule.forRoot(APP_REDUCERS),
-        SharedModule
+        SharedModule,
+        LocalDataProviderModule
       ]
     })
     .compileComponents();
