f2229ff4153a966d34290a996e829a2eb83eff7a
Lehnert Andre; EXXCELLENT SOLUTIONS <Andre.Lehnert@partner.bmw.de>
2018-10-07 18:16:10 +0200

    BSAPP-14: Show login result notification

--- bogenliga/src/app/modules/login/components/login/login.component.ts
+++ bogenliga/src/app/modules/login/components/login/login.component.ts
@@ -5,6 +5,11 @@ import {ButtonSize} from '../../../shared/components/buttons';
 import {AlertType} from '../../../shared/components/alerts';
 import {ActivatedRoute, Router} from '@angular/router';
 import {isUndefined} from 'util';
+import {LoginResult} from '../../types/login-result.enum';
+import {of} from 'rxjs';
+import {delay, tap} from 'rxjs/operators';
+
+const LOGIN_REDIRECT_QUERY_PARAM = 'destination';
 
 @Component({
   selector: 'bla-login',
@@ -16,32 +21,59 @@ export class LoginComponent implements OnInit {
 
   public credentials = new CredentialsDO();
   public loading = false;
+  public loginResult: LoginResult = LoginResult.PENDING;
   public ButtonSize = ButtonSize;
   public AlertType = AlertType;
+  public LoginResult = LoginResult;
   private destinationRouteAfterLogin = '/home';
 
   constructor(private loginDataProviderService: LoginDataProviderService, private route: ActivatedRoute, private router: Router) {
   }
 
   ngOnInit() {
-    this.route.params.subscribe(params => {
-      if (!isUndefined(params['destination'])) {
-        this.destinationRouteAfterLogin = params['destination'];
+    this.route.queryParams.subscribe(params => {
+      if (!isUndefined(params[LOGIN_REDIRECT_QUERY_PARAM])) {
+        this.destinationRouteAfterLogin = params[LOGIN_REDIRECT_QUERY_PARAM];
       }
     });
   }
 
+  /**
+   * I send the login credentials to the backend sign in service.
+   *
+   * Show a success notification and redirect to the destination route from the url query parameter.
+   *
+   * On error, show an error notification.
+   *
+   * @param $event ignore
+   */
   public onLogin($event: any): void {
     this.loading = true;
 
-    this.loginDataProviderService.signIn(this.credentials).then(
-      (success: boolean) => {
-        this.loading = false;
-        this.router.navigateByUrl(this.destinationRouteAfterLogin);
-      },
-      (err: boolean) => {
-        this.loading = false;
-      }
-    );
+    this.loginDataProviderService.signIn(this.credentials)
+      .then(
+        () => {
+          this.handleSuccessfulLogin();
+        },
+        () => {
+          this.handleFailedLogin();
+        }
+      );
+  }
+
+  private handleFailedLogin() {
+    this.loading = false;
+    this.loginResult = LoginResult.FAILURE;
+  }
+
+  private handleSuccessfulLogin() {
+    this.loading = false;
+    this.loginResult = LoginResult.SUCCESS;
+
+    // timeout function
+    of(true).pipe(
+      tap(v => console.log('Login successful. Delay between login success notification and redirect...')),
+      delay(2000)
+    ).subscribe(v => this.router.navigateByUrl(this.destinationRouteAfterLogin));
   }
 }
