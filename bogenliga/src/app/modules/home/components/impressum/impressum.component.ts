2069864db4a8c487c13d9ec3a86359c981be1351
Lehnert Andre; EXXCELLENT SOLUTIONS <Andre.Lehnert@partner.bmw.de>
2018-10-17 09:10:46 +0200

    BSAPP-14: Impressum uses common dialog

--- bogenliga/src/app/modules/home/components/impressum/impressum.component.ts
+++ bogenliga/src/app/modules/home/components/impressum/impressum.component.ts
@@ -1,15 +1,14 @@
 import {Component, OnInit} from '@angular/core';
+import {IMPRESSUM_CONFIG} from './impressum.config';
 
 @Component({
   selector:    'bla-impressum',
   templateUrl: './impressum.component.html',
-  styleUrls:   [
-    './impressum.component.scss',
-    './../../../../app.component.scss'
-  ]
+  styleUrls:   ['./impressum.component.scss']
 })
 export class ImpressumComponent implements OnInit {
 
+  public config = IMPRESSUM_CONFIG;
   constructor() {
   }
 
