c49f8791a1186a974e09f0f999b952e3a95ba060
Lehnert Andre; EXXCELLENT SOLUTIONS <Andre.Lehnert@partner.bmw.de>
2018-10-17 08:08:40 +0200

    BSAPP-14: Sportjahresplan components using common-dialog

---
+++ bogenliga/src/app/modules/sportjahresplan/components/sportjahresplan/sportjahresplan.config.ts
@@ -0,0 +1,6 @@
+import {CommonDialogConfig} from '../../../shared/components/dialogs';
+
+export const SPORTJAHRESPLAN_CONFIG: CommonDialogConfig = {
+  moduleTranslationKey:    'SPORTJAHRESPLAN',
+  pageTitleTranslationKey: 'SPORTJAHRESPLAN.SPORTJAHRESPLAN.TITLE'
+};
