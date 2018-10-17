fa46a6e53a6881f523089adb5692e4777f6f10b1
Lehnert Andre; EXXCELLENT SOLUTIONS <Andre.Lehnert@partner.bmw.de>
2018-10-17 08:43:40 +0200

    BSAPP-14: Grid layout: Default alignment = left

--- bogenliga/src/app/modules/shared/components/layouts/grid-layout/grid-layout.component.ts
+++ bogenliga/src/app/modules/shared/components/layouts/grid-layout/grid-layout.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector:    'bla-grid-layout',
@@ -7,6 +7,8 @@ import {Component, OnInit} from '@angular/core';
 })
 export class GridLayoutComponent implements OnInit {
 
+  @Input() public alignCenter = false;
+  
   constructor() {
   }
 
