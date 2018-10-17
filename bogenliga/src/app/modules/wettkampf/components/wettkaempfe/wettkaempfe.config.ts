2e25e4e1bc8490d43b2c2f3db916d42d0db99b26
Lehnert Andre; EXXCELLENT SOLUTIONS <Andre.Lehnert@partner.bmw.de>
2018-10-17 08:02:58 +0200

    BSAPP-14: Wettkaempfe components using common-dialog

---
+++ bogenliga/src/app/modules/wettkampf/components/wettkaempfe/wettkaempfe.config.ts
@@ -0,0 +1,6 @@
+import {CommonDialogConfig} from '../../../shared/components/dialogs';
+
+export const WETTKAEMPFE_CONFIG: CommonDialogConfig = {
+  moduleTranslationKey:    'WETTKAEMPFE',
+  pageTitleTranslationKey: 'WETTKAEMPFE.WETTKAEMPFE.TITLE'
+};
