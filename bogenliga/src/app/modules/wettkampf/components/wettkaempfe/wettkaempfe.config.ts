995bd277c70082d0dc7b31b20ad6abb7872e5bb0
Lehnert Andre; EXXCELLENT SOLUTIONS <Andre.Lehnert@partner.bmw.de>
2018-10-17 09:15:07 +0200

    BSAPP-14: Sportjahresplan and Wettkaempfe uses navigation component

--- bogenliga/src/app/modules/wettkampf/components/wettkaempfe/wettkaempfe.config.ts
+++ bogenliga/src/app/modules/wettkampf/components/wettkaempfe/wettkaempfe.config.ts
@@ -1,6 +1,9 @@
-import {CommonDialogConfig} from '../../../shared/components/dialogs';
+import {NavigationDialogConfig} from '../../../shared/components/dialogs';
 
-export const WETTKAEMPFE_CONFIG: CommonDialogConfig = {
+export const WETTKAEMPFE_CONFIG: NavigationDialogConfig = {
   moduleTranslationKey:    'WETTKAEMPFE',
-  pageTitleTranslationKey: 'WETTKAEMPFE.WETTKAEMPFE.TITLE'
+  pageTitleTranslationKey: 'WETTKAEMPFE.WETTKAEMPFE.TITLE',
+  navigationCardsConfig:   {
+    navigationCards: []
+  }
 };
