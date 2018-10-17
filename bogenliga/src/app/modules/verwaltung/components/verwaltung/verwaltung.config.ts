0d8d186a6c6933b15d4f84c9e2de5091f6f4158a
Lehnert Andre; EXXCELLENT SOLUTIONS <Andre.Lehnert@partner.bmw.de>
2018-10-17 09:06:21 +0200

    BSAPP-14: Use navigation dialog

--- bogenliga/src/app/modules/verwaltung/components/verwaltung/verwaltung.config.ts
+++ bogenliga/src/app/modules/verwaltung/components/verwaltung/verwaltung.config.ts
@@ -1,6 +1,16 @@
-import {CommonDialogConfig} from '../../../shared/components/dialogs';
+import {NavigationDialogConfig} from '../../../shared/components/dialogs';
 
-export const VERWALTUNG_CONFIG: CommonDialogConfig = {
+export const VERWALTUNG_CONFIG: NavigationDialogConfig = {
   moduleTranslationKey:    'MANAGEMENT',
   pageTitleTranslationKey: 'MANAGEMENT.VERWALTUNG.TITLE',
+  navigationCardsConfig:   {
+    navigationCards: [
+      {
+        labelKey:       'MANAGEMENT.VERWALTUNG.NAVIGATION.DSBMITGLIEDER.LABEL',
+        descriptionKey: 'MANAGEMENT.VERWALTUNG.NAVIGATION.DSBMITGLIEDER.DESCRIPTION',
+        icon:           'users',
+        route:          'dsbmitglieder'
+      }
+    ]
+  }
 };
