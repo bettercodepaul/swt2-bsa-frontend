26519e9a0fe0ad48bf378bf7a794fa2d2fe2466f
Lehnert Andre; EXXCELLENT SOLUTIONS <Andre.Lehnert@partner.bmw.de>
2018-10-17 08:57:21 +0200

    BSAPP-14: Use navigation cards

---
+++ bogenliga/src/app/modules/verwaltung/components/verwaltung/verwaltung-navigation.config.ts
@@ -0,0 +1,12 @@
+import {NavigationCardsConfig} from '../../../shared/components/navigation-cards';
+
+export const VERWALTUNG_NAVIGATION_CONFIG: NavigationCardsConfig = {
+  navigationCards: [
+    {
+      labelKey:       'MANAGEMENT.VERWALTUNG.NAVIGATION.DSBMITGLIEDER.LABEL',
+      descriptionKey: 'MANAGEMENT.VERWALTUNG.NAVIGATION.DSBMITGLIEDER.DESCRIPTION',
+      icon:           'users',
+      route:          'dsbmitglieder'
+    }
+  ]
+};
