20b575753cd1a4a1ec2eec2cdcbed8a228a4c218
Filip Stanicak <stanicakfilip@gmail.com>
2018-11-10 16:32:14 +0100

    Mannschafts Button und Tabelle für das Frontend erstellt

---
+++ bogenliga/src/app/modules/verwaltung/types/mannschaft-do.class.ts
@@ -0,0 +1,10 @@
+import {VersionedDataObject} from '../../shared/data-provider/models/versioned-data-object.interface';
+
+export class DsbMitgliedDO implements VersionedDataObject {
+  id: number;
+  version: number;
+
+  mannschaftsnummer: string;
+  vereinsId: number;
+  userId: number;
+}
