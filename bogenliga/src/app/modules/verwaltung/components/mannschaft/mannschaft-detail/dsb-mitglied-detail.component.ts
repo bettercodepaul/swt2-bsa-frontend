eb7a308a1fc2d0af4e461cf5fa8e3bae62affc8d
Jan <Thor@134.103.104.217>
2018-11-10 17:52:38 +0100

    Frontend zwischenspeicher

---
+++ bogenliga/src/app/modules/verwaltung/components/mannschaft/mannschaft-detail/dsb-mitglied-detail.component.ts
@@ -0,0 +1,240 @@
+import {Component, OnInit} from '@angular/core';
+import {DSB_MITGLIED_DETAIL_CONFIG} from './dsb-mitglied-detail.config';
+import {Response} from '../../../../shared/data-provider';
+import {ButtonType, CommonComponent} from '../../../../shared/components';
+import {DsbMitgliedDataProviderService} from '../../../services/dsb-mitglied-data-provider.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import {isNullOrUndefined, isUndefined} from 'util';
+import {DsbMitgliedDO} from '../../../types/dsb-mitglied-do.class';
+import {
+  Notification,
+  NotificationOrigin,
+  NotificationService,
+  NotificationSeverity,
+  NotificationType,
+  NotificationUserAction
+} from '../../../../shared/services/notification';
+
+const ID_PATH_PARAM = 'id';
+const NOTIFICATION_DELETE_DSB_MITGLIED = 'dsb_mitglied_detail_delete';
+const NOTIFICATION_DELETE_DSB_MITGLIED_SUCCESS = 'dsb_mitglied_detail_delete_success';
+const NOTIFICATION_DELETE_DSB_MITGLIED_FAILURE = 'dsb_mitglied_detail_delete_failure';
+const NOTIFICATION_SAVE_DSB_MITGLIED = 'dsb_mitglied_detail_save';
+const NOTIFICATION_UPDATE_DSB_MITGLIED = 'dsb_mitglied_detail_update';
+
+
+@Component({
+  selector:    'bla-dsb-mitglied-detail',
+  templateUrl: './dsb-mitglied-detail.component.html',
+  styleUrls:   ['./dsb-mitglied-detail.component.scss']
+})
+export class DsbMitgliedDetailComponent extends CommonComponent implements OnInit {
+
+  public config = DSB_MITGLIED_DETAIL_CONFIG;
+  public ButtonType = ButtonType;
+  public currentMitglied: DsbMitgliedDO = new DsbMitgliedDO();
+
+  public deleteLoading = false;
+  public saveLoading = false;
+
+  constructor(private dsbMitgliedDataProvider: DsbMitgliedDataProviderService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private notificationService: NotificationService) {
+    super();
+  }
+
+  ngOnInit() {
+    this.loading = true;
+
+    this.notificationService.discardNotification();
+
+    this.route.params.subscribe(params => {
+      if (!isUndefined(params[ID_PATH_PARAM])) {
+        const id = params[ID_PATH_PARAM];
+        if (id === 'add') {
+          this.currentMitglied = new DsbMitgliedDO();
+          this.loading = false;
+          this.deleteLoading = false;
+          this.saveLoading = false;
+        } else {
+          this.loadById(params[ID_PATH_PARAM]);
+        }
+      }
+    });
+  }
+
+  public onSave(ignore: any): void {
+    this.saveLoading = true;
+
+    // persist
+    this.dsbMitgliedDataProvider.create(this.currentMitglied)
+        .then((response: Response<DsbMitgliedDO>) => {
+          if (!isNullOrUndefined(response)
+            && !isNullOrUndefined(response.payload)
+            && !isNullOrUndefined(response.payload.id)) {
+            console.log('Saved with id: ' + response.payload.id);
+
+            const notification: Notification = {
+              id:          NOTIFICATION_SAVE_DSB_MITGLIED,
+              title:       'MANAGEMENT.DSBMITGLIEDER_DETAIL.NOTIFICATION.SAVE.TITLE',
+              description: 'MANAGEMENT.DSBMITGLIEDER_DETAIL.NOTIFICATION.SAVE.DESCRIPTION',
+              severity:    NotificationSeverity.INFO,
+              origin:      NotificationOrigin.USER,
+              type:        NotificationType.OK,
+              userAction:  NotificationUserAction.PENDING
+            };
+
+            this.notificationService.observeNotification(NOTIFICATION_SAVE_DSB_MITGLIED)
+                .subscribe(myNotification => {
+                  if (myNotification.userAction === NotificationUserAction.ACCEPTED) {
+                    this.saveLoading = false;
+                    this.router.navigateByUrl('/verwaltung/dsbmitglieder/' + response.payload.id);
+                  }
+                });
+
+            this.notificationService.showNotification(notification);
+          }
+        }, (response: Response<DsbMitgliedDO>) => {
+          console.log('Failed');
+          this.saveLoading = false;
+
+
+        });
+    // show response message
+  }
+
+  public onUpdate(ignore: any): void {
+    this.saveLoading = true;
+
+    // persist
+    this.dsbMitgliedDataProvider.update(this.currentMitglied)
+        .then((response: Response<DsbMitgliedDO>) => {
+          if (!isNullOrUndefined(response)
+            && !isNullOrUndefined(response.payload)
+            && !isNullOrUndefined(response.payload.id)) {
+
+            const id = this.currentMitglied.id;
+
+            const notification: Notification = {
+              id:          NOTIFICATION_UPDATE_DSB_MITGLIED + id,
+              title:       'MANAGEMENT.DSBMITGLIEDER_DETAIL.NOTIFICATION.SAVE.TITLE',
+              description: 'MANAGEMENT.DSBMITGLIEDER_DETAIL.NOTIFICATION.SAVE.DESCRIPTION',
+              severity:    NotificationSeverity.INFO,
+              origin:      NotificationOrigin.USER,
+              type:        NotificationType.OK,
+              userAction:  NotificationUserAction.PENDING
+            };
+
+            this.notificationService.observeNotification(NOTIFICATION_UPDATE_DSB_MITGLIED + id)
+                .subscribe(myNotification => {
+                  if (myNotification.userAction === NotificationUserAction.ACCEPTED) {
+                    this.saveLoading = false;
+                    this.router.navigateByUrl('/verwaltung/dsbmitglieder');
+                  }
+                });
+
+            this.notificationService.showNotification(notification);
+          }
+        }, (response: Response<DsbMitgliedDO>) => {
+          console.log('Failed');
+          this.saveLoading = false;
+        });
+    // show response message
+  }
+
+  public onDelete(ignore: any): void {
+    this.deleteLoading = true;
+    this.notificationService.discardNotification();
+
+    const id = this.currentMitglied.id;
+
+    const notification: Notification = {
+      id:               NOTIFICATION_DELETE_DSB_MITGLIED + id,
+      title:            'MANAGEMENT.DSBMITGLIEDER_DETAIL.NOTIFICATION.DELETE.TITLE',
+      description:      'MANAGEMENT.DSBMITGLIEDER_DETAIL.NOTIFICATION.DELETE.DESCRIPTION',
+      descriptionParam: '' + id,
+      severity:         NotificationSeverity.QUESTION,
+      origin:           NotificationOrigin.USER,
+      type:             NotificationType.YES_NO,
+      userAction:       NotificationUserAction.PENDING
+    };
+
+    this.notificationService.observeNotification(NOTIFICATION_DELETE_DSB_MITGLIED + id)
+        .subscribe(myNotification => {
+
+          if (myNotification.userAction === NotificationUserAction.ACCEPTED) {
+            this.dsbMitgliedDataProvider.deleteById(id)
+                .then(response => this.handleDeleteSuccess(response))
+                .catch(response => this.handleDeleteFailure(response));
+          }
+        });
+
+    this.notificationService.showNotification(notification);
+  }
+
+  public entityExists(): boolean {
+    return this.currentMitglied.id > 0;
+  }
+
+  private loadById(id: number) {
+    this.dsbMitgliedDataProvider.findById(id)
+        .then((response: Response<DsbMitgliedDO>) => this.handleSuccess(response))
+        .catch((response: Response<DsbMitgliedDO>) => this.handleFailure(response));
+  }
+
+  private handleSuccess(response: Response<DsbMitgliedDO>) {
+    this.currentMitglied = response.payload;
+    this.loading = false;
+  }
+
+  private handleFailure(response: Response<DsbMitgliedDO>) {
+    this.loading = false;
+
+  }
+
+  private handleDeleteSuccess(response: Response<void>): void {
+
+    const notification: Notification = {
+      id:          NOTIFICATION_DELETE_DSB_MITGLIED_SUCCESS,
+      title:       'MANAGEMENT.DSBMITGLIEDER_DETAIL.NOTIFICATION.DELETE_SUCCESS.TITLE',
+      description: 'MANAGEMENT.DSBMITGLIEDER_DETAIL.NOTIFICATION.DELETE_SUCCESS.DESCRIPTION',
+      severity:    NotificationSeverity.INFO,
+      origin:      NotificationOrigin.USER,
+      type:        NotificationType.OK,
+      userAction:  NotificationUserAction.PENDING
+    };
+
+    this.notificationService.observeNotification(NOTIFICATION_DELETE_DSB_MITGLIED_SUCCESS)
+        .subscribe(myNotification => {
+          if (myNotification.userAction === NotificationUserAction.ACCEPTED) {
+            this.router.navigateByUrl('/verwaltung/dsbmitglieder');
+            this.deleteLoading = false;
+          }
+        });
+
+    this.notificationService.showNotification(notification);
+  }
+
+  private handleDeleteFailure(response: Response<void>): void {
+
+    const notification: Notification = {
+      id:          NOTIFICATION_DELETE_DSB_MITGLIED_FAILURE,
+      title:       'MANAGEMENT.DSBMITGLIEDER_DETAIL.NOTIFICATION.DELETE_FAILURE.TITLE',
+      description: 'MANAGEMENT.DSBMITGLIEDER_DETAIL.NOTIFICATION.DELETE_FAILURE.DESCRIPTION',
+      severity:    NotificationSeverity.ERROR,
+      origin:      NotificationOrigin.USER,
+      type:        NotificationType.OK,
+      userAction:  NotificationUserAction.PENDING
+    };
+
+    this.notificationService.observeNotification(NOTIFICATION_DELETE_DSB_MITGLIED_FAILURE)
+        .subscribe(myNotification => {
+          if (myNotification.userAction === NotificationUserAction.ACCEPTED) {
+            this.deleteLoading = false;
+          }
+        });
+
+    this.notificationService.showNotification(notification);
+  }
+}
---
+++ bogenliga/src/app/modules/verwaltung/components/mannschaft/mannschaft-overview/dsb-mitglied-overview.config.ts
@@ -0,0 +1,46 @@
+import {OverviewDialogConfig} from '../../../../shared/components/dialogs';
+import {TableColumnSortOrder} from '../../../../shared/components/tables/types/table-column-sort-order.enum';
+import {TableActionType} from '../../../../shared/components/tables/types/table-action-type.enum';
+import {TableColumnType} from '../../../../shared/components/tables/types/table-column-type.enum';
+
+export const DSB_MITGLIED_OVERVIEW_CONFIG: OverviewDialogConfig = {
+  moduleTranslationKey:    'MANAGEMENT',
+  pageTitleTranslationKey: 'MANAGEMENT.DSBMITGLIEDER.TITLE',
+
+  tableConfig: {
+    columns: [
+      {
+        translationKey:   'MANAGEMENT.DSBMITGLIEDER.TABLE.HEADERS.ID',
+        propertyName:     'id',
+        width:            7,
+        type:             TableColumnType.NUMBER,
+        currentSortOrder: TableColumnSortOrder.ASCENDING
+      },
+      {
+        translationKey: 'MANAGEMENT.DSBMITGLIEDER.TABLE.HEADERS.VORNAME',
+        propertyName:   'vorname',
+        width:          20,
+      },
+      {
+        translationKey: 'MANAGEMENT.DSBMITGLIEDER.TABLE.HEADERS.NACHNAME',
+        propertyName:   'nachname',
+        width:          20,
+      },
+      {
+        translationKey: 'MANAGEMENT.DSBMITGLIEDER.TABLE.HEADERS.GEBURTSDATUM',
+        propertyName:   'geburtsdatum',
+        type:           TableColumnType.DATE,
+        width:          20,
+      },
+      {
+        translationKey: 'MANAGEMENT.DSBMITGLIEDER.TABLE.HEADERS.MITGLIEDSNUMMER',
+        propertyName:   'mitgliedsnummer',
+        width:          20,
+      }
+    ],
+    actions: {
+      actionTypes: [TableActionType.EDIT, TableActionType.DELETE],
+      width:       6
+    },
+  }
+};
