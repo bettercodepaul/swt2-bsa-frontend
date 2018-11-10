d37720f66b423eb89e76c79e8c9e7e51dbcad8ec
Jan <Thor@134.103.104.217>
2018-11-10 16:43:39 +0100

    Mannschaft Frontend

---
+++ bogenliga/src/app/modules/verwaltung/components/mannschaft/mannschaft-detail/mannschaft-detail.config.ts
@@ -0,0 +1,6 @@
+import {CommonDialogConfig} from '../../../../shared/components/dialogs';
+
+export const DSB_MITGLIED_DETAIL_CONFIG: CommonDialogConfig = {
+  moduleTranslationKey:    'MANAGEMENT',
+  pageTitleTranslationKey: 'MANAGEMENT.MANNSCHAFT_DETAIL.TITLE'
+};
