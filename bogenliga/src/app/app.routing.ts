8929f48038f7f2e25ee1848c1478c6842809e63b
SteCarl <stefano.carluccio@student.reutlingen-university.de>
2018-11-11 18:50:54 +0100

    "Vereine"-component started, routing issue somewhere

--- bogenliga/src/app/app.routing.ts
+++ bogenliga/src/app/app.routing.ts
@@ -5,11 +5,12 @@ import {HomeComponent} from './modules/home/components/home/home.component';
 export const ROUTES: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
+  {path: 'vereine', loadChildren: 'src/app/modules/vereine/vereine.module#VereineModule'},
   {path: 'wettkaempfe', loadChildren: 'src/app/modules/wettkampf/wettkampf.module#WettkampfModule'},
   {path: 'settings', loadChildren: 'src/app/modules/settings/settings.module#SettingsModule'},
   {path: 'verwaltung', loadChildren: 'src/app/modules/verwaltung/verwaltung.module#VerwaltungModule'},
   {
-    path:         'sportjahresplan',
+    path: 'sportjahresplan',
     loadChildren: 'src/app/modules/sportjahresplan/sportjahresplan.module#SportjahresplanModule'
   },
   {path: 'user', loadChildren: 'src/app/modules/user/user.module#UserModule'},
