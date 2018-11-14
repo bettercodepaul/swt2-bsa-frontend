359f912d678ee55f3da5fdfd145b5bc223ea681d
Nachbarino <brunomichaelt@gmail.com>
2018-11-14 20:47:21 +0100

    BSAPP-168: Minor changes on Class-Detail Component

--- bogenliga/src/app/modules/verwaltung/services/wettkampfklassen-data-provider.service.ts
+++ bogenliga/src/app/modules/verwaltung/services/wettkampfklassen-data-provider.service.ts
@@ -13,6 +13,8 @@ import {HttpErrorResponse} from '@angular/common/http';
 import {WettkampfKlasseDO} from '../types/wettkampfklasse-do.class';
 import {fromPayload, fromPayloadArray} from '../mapper/wettkampfklasse-mapper';
 
+
+
 @Injectable({
   providedIn: 'root'
 })
@@ -31,19 +33,77 @@ export class WettkampfklassenDataProviderService  extends DataProviderService {
     // sign in failure -> reject promise with result
     return new Promise((resolve, reject) => {
       this.restClient.GET<Array<VersionedDataTransferObject>>(this.getUrl())
-          .then((data: VersionedDataTransferObject[]) => {
-            console.log(data);
-            resolve({result: RequestResult.SUCCESS, payload: fromPayloadArray(data)});
+        .then((data: VersionedDataTransferObject[]) => {
+          console.log(data);
+          resolve({result: RequestResult.SUCCESS, payload: fromPayloadArray(data)});
 
-          }, (error: HttpErrorResponse) => {
+        }, (error: HttpErrorResponse) => {
 
-            if (error.status === 0) {
-              reject({result: RequestResult.CONNECTION_PROBLEM});
-            } else {
-              reject({result: RequestResult.FAILURE});
-            }
-          });
+          if (error.status === 0) {
+            reject({result: RequestResult.CONNECTION_PROBLEM});
+          } else {
+            reject({result: RequestResult.FAILURE});
+          }
+        });
+    });
+  }
+  public findById(id: string | number): Promise<Response<WettkampfKlasseDO>> {
+    // return promise
+    // sign in success -> resolve promise
+    // sign in failure -> reject promise with result
+    return new Promise((resolve, reject) => {
+      this.restClient.GET<VersionedDataTransferObject>(new UriBuilder().fromPath(this.getUrl()).path(id).build())
+        .then((data: VersionedDataTransferObject) => {
+
+          resolve({result: RequestResult.SUCCESS, payload: fromPayload(data)});
+
+        }, (error: HttpErrorResponse) => {
+
+          if (error.status === 0) {
+            reject({result: RequestResult.CONNECTION_PROBLEM});
+          } else {
+            reject({result: RequestResult.FAILURE});
+          }
+        });
+    });
+  }
+
+  public update(payload: VersionedDataTransferObject): Promise<Response<WettkampfKlasseDO>> {
+    // return promise
+    // sign in success -> resolve promise
+    // sign in failure -> reject promise with result
+    return new Promise((resolve, reject) => {
+      this.restClient.PUT<VersionedDataTransferObject>(new UriBuilder().fromPath(this.getUrl()).build(), payload)
+        .then((data: VersionedDataTransferObject) => {
+          resolve({result: RequestResult.SUCCESS, payload: fromPayload(data)});
+
+        }, (error: HttpErrorResponse) => {
+
+          if (error.status === 0) {
+            reject({result: RequestResult.CONNECTION_PROBLEM});
+          } else {
+            reject({result: RequestResult.FAILURE});
+          }
+        });
+    });
+  }
+  public create(payload: WettkampfKlasseDO): Promise<Response<WettkampfKlasseDO>> {
+    // return promise
+    // sign in success -> resolve promise
+    // sign in failure -> reject promise with result
+    return new Promise((resolve, reject) => {
+      this.restClient.POST<VersionedDataTransferObject>(new UriBuilder().fromPath(this.getUrl()).build(), payload)
+        .then((data: VersionedDataTransferObject) => {
+          resolve({result: RequestResult.SUCCESS, payload: fromPayload(data)});
+
+        }, (error: HttpErrorResponse) => {
+
+          if (error.status === 0) {
+            reject({result: RequestResult.CONNECTION_PROBLEM});
+          } else {
+            reject({result: RequestResult.FAILURE});
+          }
+        });
     });
   }
 }
-
