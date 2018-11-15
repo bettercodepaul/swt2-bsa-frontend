c224fbfa25c600f98ff8911e7bd717ae7addd3dd
Filip Stanicak <stanicakfilip@gmail.com>
2018-11-15 18:32:20 +0100

    A lot of Angular

--- bogenliga/src/app/modules/verwaltung/components/dsb-mannschaft/dsb-mannschaft-overview/dsb-mannschaft-overview.component.ts
+++ bogenliga/src/app/modules/verwaltung/components/dsb-mannschaft/dsb-mannschaft-overview/dsb-mannschaft-overview.component.ts
@@ -4,7 +4,7 @@ import {DsbMannschaftDataProviderService} from '../../../services/dsb-mannschaft
 import {TableRow} from '../../../../shared/components/tables/types/table-row.class';
 import {CommonComponent} from '../../../../shared/components/common';
 import {Response} from '../../../../shared/data-provider';
-import {MannschaftDTO} from '../../../types/datatransfer/mannschaft-dto.class';
+import {DsbMannschaftDTO} from '../../../types/datatransfer/dsb-mannschaft-dto.class';
 import {VersionedDataObject} from '../../../../shared/data-provider/models/versioned-data-object.interface';
 import {Router} from '@angular/router';
 import {
@@ -17,10 +17,10 @@ import {
 } from '../../../../shared/services/notification';
 import {hideLoadingIndicator, showDeleteLoadingIndicatorIcon, toTableRows} from '../../../../shared/components/tables';
 
-export const NOTIFICATION_DELETE_MANNSCHAFT = 'mannschaft_overview_delete';
+export const NOTIFICATION_DELETE_DSB_MANNSCHAFT = 'dsb_mannschaft_overview_delete';
 
 @Component({
-  selector:    'bla-mannschaft-overview',
+  selector:    'bla-dsb-mannschaft-overview',
   templateUrl: './dsb-mannschaft-overview.component.html',
   styleUrls:   ['./dsb-mannschaft-overview.component.scss']
 })
@@ -29,7 +29,7 @@ export class DsbMannschaftOverviewComponent extends CommonComponent implements O
   public config = DSB_MANNSCHAFT_OVERVIEW_CONFIG;
   public rows: TableRow[];
 
-  constructor(private MannschaftDataProvider: DsbMannschaftDataProviderService, private router: Router, private notificationService: NotificationService) {
+  constructor(private DsbMannschaftDataProvider: DsbMannschaftDataProviderService, private router: Router, private notificationService: NotificationService) {
     super();
   }
 
@@ -53,9 +53,9 @@ export class DsbMannschaftOverviewComponent extends CommonComponent implements O
     this.rows = showDeleteLoadingIndicatorIcon(this.rows, id);
 
     const notification: Notification = {
-      id:               NOTIFICATION_DELETE_MANNSCHAFT + id,
-      title:            'MANAGEMENT.MANNSCHAFT.NOTIFICATION.DELETE.TITLE',
-      description:      'MANAGEMENT.MANNSCHAFT.NOTIFICATION.DELETE.DESCRIPTION',
+      id:               NOTIFICATION_DELETE_DSB_MANNSCHAFT + id,
+      title:            'MANAGEMENT.DSBMANNSCHAFT.NOTIFICATION.DELETE.TITLE',
+      description:      'MANAGEMENT.DSBMANNSCHAFT.NOTIFICATION.DELETE.DESCRIPTION',
       descriptionParam: '' + id,
       severity:         NotificationSeverity.QUESTION,
       origin:           NotificationOrigin.USER,
@@ -63,10 +63,10 @@ export class DsbMannschaftOverviewComponent extends CommonComponent implements O
       userAction:       NotificationUserAction.PENDING
     };
 
-    this.notificationService.observeNotification(NOTIFICATION_DELETE_MANNSCHAFT + id)
+    this.notificationService.observeNotification(NOTIFICATION_DELETE_DSB_MANNSCHAFT + id)
         .subscribe(myNotification => {
           if (myNotification.userAction === NotificationUserAction.ACCEPTED) {
-            this.MannschaftDataProvider.deleteById(id)
+            this.DsbMannschaftDataProvider.deleteById(id)
                 .then(response => this.loadTableRows())
                 .catch(response => this.rows = hideLoadingIndicator(this.rows, id));
           }
@@ -79,21 +79,21 @@ export class DsbMannschaftOverviewComponent extends CommonComponent implements O
   private loadTableRows() {
     this.loading = true;
 
-    this.MannschaftDataProvider.findAll()
-        .then((response: Response<MannschaftDTO[]>) => this.handleLoadTableRowsSuccess(response))
-        .catch((response: Response<MannschaftDTO[]>) => this.handleLoadTableRowsFailure(response));
+    this.DsbMannschaftDataProvider.findAll()
+        .then((response: Response<DsbMannschaftDTO[]>) => this.handleLoadTableRowsSuccess(response))
+        .catch((response: Response<DsbMannschaftDTO[]>) => this.handleLoadTableRowsFailure(response));
   }
 
   private navigateToDetailDialog(versionedDataObject: VersionedDataObject) {
     this.router.navigateByUrl('/verwaltung/dsbmannschaft/' + versionedDataObject.id);
   }
 
-  private handleLoadTableRowsFailure(response: Response<MannschaftDTO[]>): void {
+  private handleLoadTableRowsFailure(response: Response<DsbMannschaftDTO[]>): void {
     this.rows = [];
     this.loading = false;
   }
 
-  private handleLoadTableRowsSuccess(response: Response<MannschaftDTO[]>): void {
+  private handleLoadTableRowsSuccess(response: Response<DsbMannschaftDTO[]>): void {
     this.rows = []; // reset array to ensure change detection
     this.rows = toTableRows(response.payload);
     this.loading = false;
