808f756e94c97066789579d73efe90e262b8491c
Philipp Schiweck <philipp_marius.schiweck@student.reutlingen-university.de>
2018-11-27 16:31:59 +0100

    Implement mapper, service, DTO and DO for Vereine module

--- bogenliga/src/app/modules/vereine/components/vereine/vereine.component.ts
+++ bogenliga/src/app/modules/vereine/components/vereine/vereine.component.ts
@@ -1,18 +1,59 @@
 import { Component, OnInit } from '@angular/core';
-import {VEREINE_CONFIG} from "./vereine.config";
+import {VEREINE_CONFIG} from './vereine.config';
+import {CommonComponent} from '../../../shared/components/common';
+import {TableRow} from '../../../shared/components/tables/types/table-row.class';
+import {Response} from '../../../shared/data-provider';
+import {Router} from '@angular/router';
+import {NotificationService} from '../../../shared/services/notification';
+import {VereineDataProviderService} from '../../services/vereine-data-provider.service';
+import {toTableRows} from '../../../shared/components/tables';
+import {VereinDO} from '../../types/vereine-do.class';
+import {VereinDTO} from '../../types/datatransfer/vereine-dto.class';
+import {VersionedDataObject} from '../../../shared/data-provider/models/versioned-data-object.interface';
 
 @Component({
   selector: 'bla-vereine',
   templateUrl: './vereine.component.html',
   styleUrls: ['./vereine.component.scss']
 })
-export class VereineComponent implements OnInit {
+export class VereineComponent extends CommonComponent implements OnInit {
 
   public config = VEREINE_CONFIG;
+  public rows: TableRow[];
 
-  constructor() { }
+  constructor(private vereineDataProvider: VereineDataProviderService, private router: Router, private notificationService: NotificationService) {
+    super();
+  }
 
   ngOnInit() {
+    this.loadTableRows();
+  }
+
+  public onView(versionedDataObject: VersionedDataObject): void {
+    this.navigateToDetailDialog(versionedDataObject);
+  }
+
+  private navigateToDetailDialog(versionedDataObject: VersionedDataObject) {
+    this.router.navigateByUrl('/vereine/' + versionedDataObject.id);
+  }
+
+  private loadTableRows() {
+    this.loading = true;
+
+    this.vereineDataProvider.findAll()
+      .then((response: Response<VereinDTO[]>) => this.handleLoadTableRowsSuccess(response))
+      .catch((response: Response<VereinDTO[]>) => this.handleLoadTableRowsFailure(response));
+  }
+
+  private handleLoadTableRowsSuccess(response: Response<VereinDO[]>): void {
+    this.rows = []; // reset array to ensure change detection
+    this.rows = toTableRows(response.payload);
+    this.loading = false;
+  }
+
+  private handleLoadTableRowsFailure(response: Response<VereinDO[]>): void {
+    this.rows = [];
+    this.loading = false;
   }
 
 }
---
+++ bogenliga/src/app/modules/vereine/mapper/vereine-mapper.ts
@@ -0,0 +1,12 @@
+import {VersionedDataTransferObject} from '../../shared/data-provider';
+import {VereinDTO} from '../types/datatransfer/vereine-dto.class';
+
+export function fromPayload(payload: VersionedDataTransferObject): VereinDTO {
+  return VereinDTO.copyFrom(payload);
+}
+
+export function fromPayloadArray(payload: VersionedDataTransferObject[]): VereinDTO[] {
+  const list: VereinDTO[] = [];
+  payload.forEach(single => list.push(fromPayload(single)));
+  return list;
+}
