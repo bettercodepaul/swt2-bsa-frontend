2e588e1e85e2e3cb59647e7ed3a65028a2829095
Philipp Schiweck <philipp_marius.schiweck@student.reutlingen-university.de>
2018-12-03 14:03:04 +0100

    Add basic vereine mannschaften detail files

---
+++ bogenliga/src/app/modules/vereine/components/mannschaften/vereine-mannschaften.config.ts
@@ -0,0 +1,6 @@
+import {CommonDialogConfig} from '../../../shared/components/dialogs';
+
+export const VEREIN_MANNSCHAFTEN_CONFIG: CommonDialogConfig = {
+  moduleTranslationKey:    'VEREINE',
+  pageTitleTranslationKey: 'VEREINE.VEREINE.TITLE',
+};
