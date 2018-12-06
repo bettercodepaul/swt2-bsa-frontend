76cf9870962d05f7db377e26cce211534bc7e553
Yann Philippczyk <yann.philippczyk@exxcellent.de>
2018-12-06 15:48:08 +0100

    WIP: Reworked permissions for accessing user profile

--- bogenliga/src/app/modules/user/components/user-profile/user-profile.component.ts
+++ bogenliga/src/app/modules/user/components/user-profile/user-profile.component.ts
@@ -4,7 +4,7 @@ import {Response} from '../../../shared/data-provider';
 import {ButtonType, CommonComponent} from '../../../shared/components';
 import {UserProfileDataProviderService} from '../../services/user-profile-data-provider.service';
 import {ActivatedRoute, Router} from '@angular/router';
-import {isNullOrUndefined, isUndefined} from 'util';
+import {isNullOrUndefined} from 'util';
 import {UserProfileDO} from '../../types/user-profile-do.class';
 import {
   Notification,
@@ -48,25 +48,7 @@ export class UserProfileComponent extends CommonComponent implements OnInit {
     this.loading = true;
 
     this.notificationService.discardNotification();
-    console.log('TeeeeeeeeestOnInit');
-    this.loadById(5);
-    this.route.params.subscribe(params => {
-      if (!isUndefined(params[ID_PATH_PARAM])) {
-        const id = params[ID_PATH_PARAM];
-        if (id === 'add') {
-          console.log('Params');
-          console.log(params[ID_PATH_PARAM]);
-          this.currentUserProfile = new UserProfileDO();
-          this.loading = false;
-          this.deleteLoading = false;
-          this.saveLoading = false;
-        } else {
-
-          console.log('Teeeeeeeeest');
-          this.loadById(params[ID_PATH_PARAM]);
-        }
-      }
-    });
+    this.loadCurrentUserProfile();
   }
 
   public onSave(ignore: any): void {
@@ -148,43 +130,18 @@ export class UserProfileComponent extends CommonComponent implements OnInit {
     // show response message
   }
 
-  //TODO delete funktionalittäten löschen?
-  public onDelete(ignore: any): void {
-    this.deleteLoading = true;
-    this.notificationService.discardNotification();
-
-    const id = this.currentUserProfile.id;
-
-    const notification: Notification = {
-      id: NOTIFICATION_DELETE_USER_PROFILE + id,
-      title: 'MANAGEMENT.USERPROFILE.NOTIFICATION.DELETE.TITLE',
-      description: 'MANAGEMENT.USERPROFILE.NOTIFICATION.DELETE.DESCRIPTION',
-      descriptionParam: '' + id,
-      severity: NotificationSeverity.QUESTION,
-      origin: NotificationOrigin.USER,
-      type: NotificationType.YES_NO,
-      userAction: NotificationUserAction.PENDING
-    };
-
-    this.notificationService.observeNotification(NOTIFICATION_DELETE_USER_PROFILE + id)
-      .subscribe(myNotification => {
-
-        if (myNotification.userAction === NotificationUserAction.ACCEPTED) {
-          this.userProfileDataProvider.deleteById(id)
-            .then(response => this.handleDeleteSuccess(response))
-            .catch(response => this.handleDeleteFailure(response));
-        }
-      });
-
-    this.notificationService.showNotification(notification);
-  }
-
   public entityExists(): boolean {
     return this.currentUserProfile.id > 0;
   }
 
   private loadById(id: number) {
     this.userProfileDataProvider.findById(id)
+      .then((response: Response<UserProfileDO>) => this.handleSuccess(response))
+      .catch((response: Response<UserProfileDO>) => this.handleFailure(response));
+  }
+
+  private loadCurrentUserProfile() {
+    this.userProfileDataProvider.findCurrentUserProfile()
       .then((response: Response<UserProfileDO>) => this.handleSuccess(response))
       .catch((response: Response<UserProfileDO>) => this.handleFailure(response));
   }
@@ -197,50 +154,5 @@ export class UserProfileComponent extends CommonComponent implements OnInit {
   private handleFailure(response: Response<UserProfileDO>) {
     this.loading = false;
 
-  }
-
-  private handleDeleteSuccess(response: Response<void>): void {
-
-    const notification: Notification = {
-      id: NOTIFICATION_DELETE_USER_PROFILE_SUCCESS,
-      title: 'MANAGEMENT.USERPROFILE.NOTIFICATION.DELETE_SUCCESS.TITLE',
-      description: 'MANAGEMENT.USERPROFILE.NOTIFICATION.DELETE_SUCCESS.DESCRIPTION',
-      severity: NotificationSeverity.INFO,
-      origin: NotificationOrigin.USER,
-      type: NotificationType.OK,
-      userAction: NotificationUserAction.PENDING
-    };
-
-    this.notificationService.observeNotification(NOTIFICATION_DELETE_USER_PROFILE_SUCCESS)
-      .subscribe(myNotification => {
-        if (myNotification.userAction === NotificationUserAction.ACCEPTED) {
-          this.router.navigateByUrl('/user');
-          this.deleteLoading = false;
-        }
-      });
-
-    this.notificationService.showNotification(notification);
-  }
-
-  private handleDeleteFailure(response: Response<void>): void {
-
-    const notification: Notification = {
-      id: NOTIFICATION_DELETE_USER_PROFILE_FAILURE,
-      title: 'MANAGEMENT.USERPROFILE.NOTIFICATION.DELETE_FAILURE.TITLE',
-      description: 'MANAGEMENT.USERPROFILE.NOTIFICATION.DELETE_FAILURE.DESCRIPTION',
-      severity: NotificationSeverity.ERROR,
-      origin: NotificationOrigin.USER,
-      type: NotificationType.OK,
-      userAction: NotificationUserAction.PENDING
-    };
-
-    this.notificationService.observeNotification(NOTIFICATION_DELETE_USER_PROFILE_FAILURE)
-      .subscribe(myNotification => {
-        if (myNotification.userAction === NotificationUserAction.ACCEPTED) {
-          this.deleteLoading = false;
-        }
-      });
-
-    this.notificationService.showNotification(notification);
   }
 }
