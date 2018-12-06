ce9cf144221fcf88dad5788e8fb5bfd003852791
Yann Philippczyk <yann.philippczyk@exxcellent.de>
2018-12-06 16:11:15 +0100

    Removed unused code; set profile to read-only

--- bogenliga/src/app/modules/user/services/user-profile-data-provider.service.ts
+++ bogenliga/src/app/modules/user/services/user-profile-data-provider.service.ts
@@ -17,46 +17,6 @@ export class UserProfileDataProviderService extends DataProviderService {
     super();
   }
 
-  public create(payload: UserProfileDO): Promise<Response<UserProfileDO>> {
-    // return promise
-    // sign in success -> resolve promise
-    // sign in failure -> reject promise with result
-    return new Promise((resolve, reject) => {
-      this.restClient.POST<VersionedDataTransferObject>(new UriBuilder().fromPath(this.getUrl()).build(), payload)
-        .then((data: VersionedDataTransferObject) => {
-          resolve({result: RequestResult.SUCCESS, payload: fromPayload(data)});
-
-        }, (error: HttpErrorResponse) => {
-
-          if (error.status === 0) {
-            reject({result: RequestResult.CONNECTION_PROBLEM});
-          } else {
-            reject({result: RequestResult.FAILURE});
-          }
-        });
-    });
-  }
-
-  public deleteById(id: number): Promise<Response<void>> {
-    // return promise
-    // sign in success -> resolve promise
-    // sign in failure -> reject promise with result
-    return new Promise((resolve, reject) => {
-      this.restClient.DELETE<void>(new UriBuilder().fromPath(this.getUrl()).path(id).build())
-        .then(noData => {
-          resolve({result: RequestResult.SUCCESS});
-
-        }, (error: HttpErrorResponse) => {
-
-          if (error.status === 0) {
-            reject({result: RequestResult.CONNECTION_PROBLEM});
-          } else {
-            reject({result: RequestResult.FAILURE});
-          }
-        });
-    });
-  }
-
 
   public findAll(): Promise<Response<UserProfileDO[]>> {
     // return promise
@@ -110,26 +70,6 @@ export class UserProfileDataProviderService extends DataProviderService {
       this.restClient.GET<VersionedDataTransferObject>(new UriBuilder().fromPath(this.getUrl()).path(id).build())
         .then((data: VersionedDataTransferObject) => {
 
-          resolve({result: RequestResult.SUCCESS, payload: fromPayload(data)});
-
-        }, (error: HttpErrorResponse) => {
-
-          if (error.status === 0) {
-            reject({result: RequestResult.CONNECTION_PROBLEM});
-          } else {
-            reject({result: RequestResult.FAILURE});
-          }
-        });
-    });
-  }
-
-  public update(payload: VersionedDataTransferObject): Promise<Response<UserProfileDO>> {
-    // return promise
-    // sign in success -> resolve promise
-    // sign in failure -> reject promise with result
-    return new Promise((resolve, reject) => {
-      this.restClient.PUT<VersionedDataTransferObject>(new UriBuilder().fromPath(this.getUrl()).build(), payload)
-        .then((data: VersionedDataTransferObject) => {
           resolve({result: RequestResult.SUCCESS, payload: fromPayload(data)});
 
         }, (error: HttpErrorResponse) => {
