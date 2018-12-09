660b07aa214416d11c27e3cd98d7ebe5858ea54d
Stefano Carluccio <stefano.carluccio@student.reutlingen-university.de>
2018-12-09 11:37:58 +0100

    Routing to single Verein-statistics fixed
    Mannschaften werden angezeigt
    -> Filter für die Vereinsmannschaften muss eingefuegt werden (vielleicht durch neue Methode findByVereinsID() ?)

--- bogenliga/src/app/modules/vereine/components/mannschaften/vereine-mannschaften.component.ts
+++ bogenliga/src/app/modules/vereine/components/mannschaften/vereine-mannschaften.component.ts
@@ -1,19 +1,51 @@
-import {Component, OnInit} from '@angular/core';
-import {CommonComponent} from '../../../shared/components/common';
+import {Component, Input, OnInit} from '@angular/core';
 
-// TODO
-/*@Component({
+import {CommonComponent} from '../../../shared/components/common';
+import {DSB_MANNSCHAFT_OVERVIEW_CONFIG} from "../../../verwaltung/components/dsb-mannschaft/dsb-mannschaft-overview/dsb-mannschaft-overview.config";
+import {TableRow} from "../../../shared/components/tables/types/table-row.class";
+import {DsbMannschaftDataProviderService} from "../../../verwaltung/services/dsb-mannschaft-data-provider.service";
+import {toTableRows} from "../../../shared/components/tables";
+import {Response} from "../../../shared/data-provider";
+import {DsbMannschaftDTO} from "../../../verwaltung/types/datatransfer/dsb-mannschaft-dto.class";
+
+
+@Component({
   selector: 'bla-vereine-mannschaften',
   templateUrl: './vereine-mannschaften.component.html',
   styleUrls: ['./vereine-mannschaften.component.scss']
-})*/
+})
 export class VereineMannschaftenComponent extends CommonComponent implements OnInit {
 
-  constructor( ) {
+  @Input() vereinID: string = "0";
+
+  public config = DSB_MANNSCHAFT_OVERVIEW_CONFIG;
+  public rows: TableRow[];
+
+  constructor(private DsbMannschaftDataProvider: DsbMannschaftDataProviderService) {
     super();
   }
 
-  ngOnInit( ) {
+  ngOnInit() {
+    this.loadTableRows();
+  }
 
+  private loadTableRows() {
+    this.loading = true;
+
+    this.DsbMannschaftDataProvider.findAll()
+      .then((response: Response<DsbMannschaftDTO[]>) => this.handleLoadTableRowsSuccess(response))
+      .catch((response: Response<DsbMannschaftDTO[]>) => this.handleLoadTableRowsFailure(response));
+  }
+
+  private handleLoadTableRowsFailure(response: Response<DsbMannschaftDTO[]>): void {
+    this.rows = [];
+    this.loading = false;
+  }
+
+  private handleLoadTableRowsSuccess(response: Response<DsbMannschaftDTO[]>): void {
+    this.rows = []; // reset array to ensure change detection
+    //FILTER
+    this.rows = toTableRows(response.payload);
+    this.loading = false;
   }
 }
--- bogenliga/src/app/modules/vereine/vereine.routing.ts
+++ bogenliga/src/app/modules/vereine/vereine.routing.ts
@@ -1,4 +1,5 @@
 import {Routes} from '@angular/router';
+
 import {VereineComponent}from './components/vereine/vereine.component';
 import {VereineMannschaftenComponent} from './components/mannschaften/vereine-mannschaften.component';
 
@@ -6,5 +7,5 @@ import {VereineMannschaftenComponent} from './components/mannschaften/vereine-ma
 
 export const VEREINE_ROUTES: Routes = [
   {path: '', component: VereineComponent},
-  {path: '/vereine/:id', component: VereineMannschaftenComponent}
+  {path: ':id', component: VereineMannschaftenComponent}
 ];
