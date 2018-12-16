a5dec2bb38c7199429a48a782f237380f510bcab
Lehnert Andre; EXXCELLENT SOLUTIONS <Andre.Lehnert@partner.bmw.de>
2018-12-16 21:24:16 +0100

    PLAYGROUND: Layout example

--- bogenliga/src/app/modules/playground/components/playground/components/index.ts
+++ bogenliga/src/app/modules/playground/components/playground/components/index.ts
@@ -1,3 +1,4 @@
 export * from './tooltip-example/tooltip-example.component';
 export * from './dropdown-menu-example/dropdown-menu-example.component';
 export * from './selectionlist-example/selectionlist-example.component';
+export * from './layout-example/layout-example.component';
