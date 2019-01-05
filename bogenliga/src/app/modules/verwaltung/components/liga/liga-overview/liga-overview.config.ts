2a67c46ea25a41d56220c4aea22740f6df7a2e97
Ge2theZ <giuseppe.ferrera@student.reutlingen-university.de>
2019-01-05 12:56:16 +0100

    BSAPP-211: fixed liga overview

--- bogenliga/src/app/modules/verwaltung/components/liga/liga-overview/liga-overview.config.ts
+++ bogenliga/src/app/modules/verwaltung/components/liga/liga-overview/liga-overview.config.ts
@@ -18,15 +18,8 @@ export const LIGA_OVERVIEW_CONFIG: OverviewDialogConfig = {
       },**/
       {
         translationKey: 'MANAGEMENT.LIGA.TABLE.HEADERS.LIGANAME',
-        propertyName:   'ligaName',
+        propertyName:   'name',
         width:          20,
-      },
-      {
-        translationKey: 'MANAGEMENT.LIGA.TABLE.HEADERS.REGIONID',
-        propertyName:   'regionId',
-        width:            7,
-        type:             TableColumnType.NUMBER,
-        currentSortOrder: TableColumnSortOrder.ASCENDING
       },
       {
         translationKey: 'MANAGEMENT.LIGA.TABLE.HEADERS.REGIONNAME',
@@ -34,23 +27,9 @@ export const LIGA_OVERVIEW_CONFIG: OverviewDialogConfig = {
         width:          20,
       },
       {
-        translationKey: 'MANAGEMENT.LIGA.TABLE.HEADERS.UEBERGEORDNETID',
-        propertyName:   'ligaUebergeordnetId',
-        width:            7,
-        type:             TableColumnType.NUMBER,
-        currentSortOrder: TableColumnSortOrder.ASCENDING
-      },
-      {
         translationKey: 'MANAGEMENT.LIGA.TABLE.HEADERS.UEBERGEORDNETNAME',
         propertyName:   'ligaUebergeordnetName',
         width:          20,
-      },
-      {
-        translationKey: 'MANAGEMENT.LIGA.TABLE.HEADERS.VERANTWORTLICHID',
-        propertyName:   'ligaVerantwortlichId',
-        width:            7,
-        type:             TableColumnType.NUMBER,
-        currentSortOrder: TableColumnSortOrder.ASCENDING
       },
       {
         translationKey: 'MANAGEMENT.LIGA.TABLE.HEADERS.VERANTWORTLICHMAIL',
@@ -61,7 +40,7 @@ export const LIGA_OVERVIEW_CONFIG: OverviewDialogConfig = {
       }
     ],
     actions: {
-      actionTypes: [TableActionType.EDIT, /**TableActionType.DELETE**/],
+      actionTypes: [TableActionType.EDIT, TableActionType.DELETE],
       width:       6
     },
   }
