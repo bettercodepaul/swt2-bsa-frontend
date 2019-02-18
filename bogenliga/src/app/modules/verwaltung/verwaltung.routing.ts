0d5b821088ea11296cea2740cb1bcec0fd39d485
Lehnert Andre; EXXCELLENT SOLUTIONS <Andre.Lehnert@partner.bmw.de>
2019-02-18 15:07:51 +0100

    Fix merge problems

--- bogenliga/src/app/modules/verwaltung/verwaltung.routing.ts
+++ bogenliga/src/app/modules/verwaltung/verwaltung.routing.ts
@@ -5,81 +5,81 @@ import {
   DsbMannschaftOverviewGuard,
   DsbMitgliedDetailGuard,
   DsbMitgliedOverviewGuard,
+  LigaDetailGuard,
+  LigaOverviewGuard,
+  SportjahrLigaAuswahlGuard,
+  VereinDetailGuard,
+  VereinOverviewGuard,
   VerwaltungGuard,
   WettkampfklasseDetailGuard,
-  WettkampfklasseOverviewGuard,
-  VereinOverviewGuard,
-  VereinDetailGuard,
-  LigaOverviewGuard,
-  LigaDetailGuard,
-  SportjahrLigaAuswahlGuard
+  WettkampfklasseOverviewGuard
 } from './guards';
 import {
   DsbMannschaftDetailComponent,
   DsbMannschaftOverviewComponent,
   DsbMitgliedDetailComponent,
   DsbMitgliedOverviewComponent,
-  VerwaltungComponent,
-  WettkampfklasseDetailComponent,
-  WettkampfklasseOverviewComponent,
-  VereinOverviewComponent,
-  VereinDetailComponent,
   LigaDetailComponent,
   LigaOverviewComponent,
   SportjahrLigaAuswahlComponent,
-  SportjahrOverviewComponent
+  SportjahrOverviewComponent,
+  VereinDetailComponent,
+  VereinOverviewComponent,
+  VerwaltungComponent,
+  WettkampfklasseDetailComponent,
+  WettkampfklasseOverviewComponent
 } from './components';
 import {SportjahrOverviewGuard} from './guards/sportjahr-overview.guard';
 
 export const VERWALTUNG_ROUTES: Routes = [
   {path: '', component: VerwaltungComponent, canActivate: [VerwaltungGuard]},
   {
-    path:        'dsbmitglieder',
-    component:   DsbMitgliedOverviewComponent,
-    pathMatch:   'full',
+    path: 'dsbmitglieder',
+    component: DsbMitgliedOverviewComponent,
+    pathMatch: 'full',
     canActivate: [DsbMitgliedOverviewGuard]
   },
   {path: 'dsbmitglieder/:id', component: DsbMitgliedDetailComponent, canActivate: [DsbMitgliedDetailGuard]}
   ,
   {path: '', component: VerwaltungComponent, canActivate: [VerwaltungGuard]},
   {
-    path:        'dsbmannschaft',
-    component:   DsbMannschaftOverviewComponent,
-    pathMatch:   'full',
+    path: 'dsbmannschaft',
+    component: DsbMannschaftOverviewComponent,
+    pathMatch: 'full',
     canActivate: [DsbMannschaftOverviewGuard]
   },
   {path: 'dsbmannschaft/:id', component: DsbMannschaftDetailComponent, canActivate: [DsbMannschaftDetailGuard]},
   {
-    path:        'klassen',
-    component:   WettkampfklasseOverviewComponent,
-    pathMatch:   'full',
+    path: 'klassen',
+    component: WettkampfklasseOverviewComponent,
+    pathMatch: 'full',
     canActivate: [WettkampfklasseOverviewGuard]
   },
   {path: 'klassen/:id', component: WettkampfklasseDetailComponent, canActivate: [WettkampfklasseDetailGuard]},
   {
-    path:        'vereine',
-    component:   VereinOverviewComponent,
-    pathMatch:   'full',
+    path: 'vereine',
+    component: VereinOverviewComponent,
+    pathMatch: 'full',
     canActivate: [VereinOverviewGuard]
   },
   {path: 'vereine/:id', component: VereinDetailComponent, canActivate: [VereinDetailGuard]},
   {
-    path:        'liga',
-    component:   LigaOverviewComponent,
-    pathMatch:   'full',
+    path: 'liga',
+    component: LigaOverviewComponent,
+    pathMatch: 'full',
     canActivate: [LigaOverviewGuard]
   },
-  {path: 'liga/:id', component: LigaDetailComponent, canActivate: [LigaDetailGuard]}
-
-  {path: 'dsbmitglieder/:id', component: DsbMitgliedDetailComponent, canActivate: [DsbMitgliedDetailGuard]}
-  ,
+  {path: 'liga/:id', component: LigaDetailComponent, canActivate: [LigaDetailGuard]},
   {
     path: 'sportjahr',
-    pathMatch:   'full',
+    pathMatch: 'full',
     component: SportjahrLigaAuswahlComponent,
-
     canActivate: [SportjahrLigaAuswahlGuard]
   },
-  {path: 'sportjahr/liga/:id', pathMatch: 'full', component: SportjahrOverviewComponent, canActivate: [SportjahrOverviewGuard]},
-  { path: 'liga', component:   LigaOverviewComponent, canActivate: [LigaOverviewGuard]}
-  ];
+  {
+    path: 'sportjahr/liga/:id',
+    pathMatch: 'full',
+    component: SportjahrOverviewComponent,
+    canActivate: [SportjahrOverviewGuard]
+  }
+];
