1bc407e91cf3a67ac779b34b119c9a4729ee3dbb
Delkuras <delkuras.96@web.de>
2019-04-24 15:24:42 +0200

    BSAPP144 vereine reiter angelegt

---
+++ bogenliga/src/app/modules/vereine/services/verein-data-provider.service.ts
@@ -0,0 +1,126 @@
+import {HttpErrorResponse} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {
+  BogenligaResponse,
+  DataProviderService,
+  RequestResult,
+  RestClient,
+  UriBuilder,
+  VersionedDataTransferObject
+} from '../../shared/data-provider';
+import {CurrentUserService} from '../../shared/services/current-user';
+import {fromPayload, fromPayloadArray} from '../mapper/verein-mapper';
+import {VereinDO} from '../types/verein-do.class';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VereinDataProviderService extends DataProviderService {
+
+  serviceSubUrl = 'v1/vereine';
+
+
+  constructor(private restClient: RestClient, private currentUserService: CurrentUserService) {
+    super();
+  }
+
+  public findAll(): Promise<BogenligaResponse<VereinDO[]>> {
+    // return promise
+    // sign in success -> resolve promise
+    // sign in failure -> reject promise with result
+    return new Promise((resolve, reject) => {
+      this.restClient.GET<Array<VersionedDataTransferObject>>(this.getUrl())
+        .then((data: VersionedDataTransferObject[]) => {
+          resolve({result: RequestResult.SUCCESS, payload: fromPayloadArray(data)});
+        }, (error: HttpErrorResponse) => {
+
+          if (error.status === 0) {
+            reject({result: RequestResult.CONNECTION_PROBLEM});
+          } else {
+            reject({result: RequestResult.FAILURE});
+          }
+        });
+    });
+  }
+
+  public deleteById(id: number): Promise<BogenligaResponse<void>> {
+    // return promise
+    // sign in success -> resolve promise
+    // sign in failure -> reject promise with result
+    return new Promise((resolve, reject) => {
+      this.restClient.DELETE<void>(new UriBuilder().fromPath(this.getUrl()).path(id).build())
+        .then((noData) => {
+          resolve({result: RequestResult.SUCCESS});
+
+        }, (error: HttpErrorResponse) => {
+
+          if (error.status === 0) {
+            reject({result: RequestResult.CONNECTION_PROBLEM});
+          } else {
+            reject({result: RequestResult.FAILURE});
+          }
+        });
+    });
+  }
+
+  public findById(id: string | number): Promise<BogenligaResponse<VereinDO>> {
+    // return promise
+    // sign in success -> resolve promise
+    // sign in failure -> reject promise with result
+    return new Promise((resolve, reject) => {
+      this.restClient.GET<VersionedDataTransferObject>(new UriBuilder().fromPath(this.getUrl()).path(id).build())
+        .then((data: VersionedDataTransferObject) => {
+
+          resolve({result: RequestResult.SUCCESS, payload: fromPayload(data)});
+
+        }, (error: HttpErrorResponse) => {
+
+          if (error.status === 0) {
+            reject({result: RequestResult.CONNECTION_PROBLEM});
+          } else {
+            reject({result: RequestResult.FAILURE});
+          }
+        });
+    });
+  }
+
+  public update(payload: VersionedDataTransferObject): Promise<BogenligaResponse<VereinDO>> {
+    // return promise
+    // sign in success -> resolve promise
+    // sign in failure -> reject promise with result
+    return new Promise((resolve, reject) => {
+      this.restClient.PUT<VersionedDataTransferObject>(new UriBuilder().fromPath(this.getUrl()).build(), payload)
+        .then((data: VersionedDataTransferObject) => {
+          resolve({result: RequestResult.SUCCESS, payload: fromPayload(data)});
+
+        }, (error: HttpErrorResponse) => {
+
+          if (error.status === 0) {
+            reject({result: RequestResult.CONNECTION_PROBLEM});
+          } else {
+            reject({result: RequestResult.FAILURE});
+          }
+        });
+    });
+  }
+
+  public create(payload: VereinDO): Promise<BogenligaResponse<VereinDO>> {
+    // return promise
+    // sign in success -> resolve promise
+    // sign in failure -> reject promise with result
+    return new Promise((resolve, reject) => {
+      this.restClient.POST<VersionedDataTransferObject>(new UriBuilder().fromPath(this.getUrl()).build(), payload)
+        .then((data: VersionedDataTransferObject) => {
+          resolve({result: RequestResult.SUCCESS, payload: fromPayload(data)});
+
+        }, (error: HttpErrorResponse) => {
+
+          if (error.status === 0) {
+            reject({result: RequestResult.CONNECTION_PROBLEM});
+          } else {
+            reject({result: RequestResult.FAILURE});
+          }
+        });
+    });
+  }
+}
