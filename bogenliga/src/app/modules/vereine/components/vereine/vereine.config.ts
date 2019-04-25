b09b393b19e10123f8e55aef0e225f3400b30f8f
mki4 <baisch.manuel@gmail.com>
2019-04-25 16:56:58 +0200

    BSAPP-292, Wettkampf/Mannschaftstabelle angelegt

--- bogenliga/src/app/modules/vereine/components/vereine/vereine.config.ts
+++ bogenliga/src/app/modules/vereine/components/vereine/vereine.config.ts
@@ -1,9 +1,34 @@
-import {NavigationDialogConfig} from '@shared/components';
+import {NavigationDialogConfig, OverviewDialogConfig} from '@shared/components';
+import {TableActionType} from '@shared/components/tables/types/table-action-type.enum';
+import {tableConfigWithDefaults} from '@shared/components/tables/control/table-config-mapper';
+import {TableConfig} from '@shared/components/tables/types/table-config.interface';
 
 export const VEREINE_CONFIG: NavigationDialogConfig = {
   moduleTranslationKey: 'VEREINE',
   pageTitleTranslationKey: 'VEREINE.VEREINE.TITLE',
   navigationCardsConfig: {
     navigationCards: []
-  }
+  },
 };
+
+export const VEREINE_TABLE_CONFIG: TableConfig = {
+
+    columns: [
+      {
+        translationKey: 'VEREINE.VEREINE.TABLE.HEADERS.WETTKAMPF',
+        propertyName:   'Wettkampf',
+        width:          20,
+      },
+      {
+        translationKey: 'VEREINE.VEREINE.TABLE.HEADERS.MANNSCHAFT',
+        propertyName:   'nummer',
+        width:          20,
+      }
+    ],
+    actions: {
+      actionTypes: [TableActionType.EDIT, TableActionType.DELETE],
+      width:       6
+    },
+
+};
+
