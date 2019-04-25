395afb26bf964c59db7772380cc8a3d66d91df3a
mki4 <baisch.manuel@gmail.com>
2019-04-25 16:56:58 +0200

    BSAPP-292, Wettkampf/Mannschaftstabelle angelegt

--- bogenliga/src/app/modules/verwaltung/services/dsb-mannschaft-data-provider.service.ts
+++ bogenliga/src/app/modules/verwaltung/services/dsb-mannschaft-data-provider.service.ts
@@ -102,6 +102,27 @@ export class DsbMannschaftDataProviderService extends DataProviderService {
     });
   }
 
+  public findAllByVereinsId(id: number): Promise<BogenligaResponse<DsbMannschaftDO[]>> {
+    // return promise
+    // sign in success -> resolve promise
+    // sign in failure -> reject promise with result
+    return new Promise((resolve, reject) => {
+      this.restClient.GET<Array<VersionedDataTransferObject>>(new UriBuilder().fromPath(this.getUrl()).path(id).build())
+          .then((data: VersionedDataTransferObject[]) => {
+
+            resolve({result: RequestResult.SUCCESS, payload: fromPayloadArray(data)});
+
+          }, (error: HttpErrorResponse) => {
+
+            if (error.status === 0) {
+              reject({result: RequestResult.CONNECTION_PROBLEM});
+            } else {
+              reject({result: RequestResult.FAILURE});
+            }
+          });
+    });
+  }
+
 
   public findById(id: string | number): Promise<BogenligaResponse<DsbMannschaftDO>> {
     // return promise
