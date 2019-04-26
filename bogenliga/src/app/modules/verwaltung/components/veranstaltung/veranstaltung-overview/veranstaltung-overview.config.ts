4054571513378514752e1e61ec6e298c4bbd43bd
miriamLang1 <miriam.lang@student.reutlingen-university.de>
2019-04-26 09:34:35 +0200

    BSAPP-252, Eintrag im Menü erstellt; Ordnerstuktur angelegt

---
+++ bogenliga/src/app/modules/verwaltung/components/veranstaltung/veranstaltung-overview/veranstaltung-overview.config.ts
@@ -0,0 +1,40 @@
+import {OverviewDialogConfig} from '@shared/components';
+import {TableActionType} from '@shared/components/tables/types/table-action-type.enum';
+import {TableColumnSortOrder} from '@shared/components/tables/types/table-column-sort-order.enum';
+import {TableColumnType} from '@shared/components/tables/types/table-column-type.enum';
+
+export const LIGA_OVERVIEW_CONFIG: OverviewDialogConfig = {
+  moduleTranslationKey:    'MANAGEMENT',
+  pageTitleTranslationKey: 'MANAGEMENT.LIGA.TITLE',
+
+  tableConfig: {
+    columns: [
+      {
+        translationKey: 'MANAGEMENT.LIGA.TABLE.HEADERS.LIGANAME',
+        propertyName:   'name',
+        width:          20,
+      },
+      {
+        translationKey: 'MANAGEMENT.LIGA.TABLE.HEADERS.REGIONNAME',
+        propertyName:   'regionName',
+        width:          20,
+      },
+      {
+        translationKey: 'MANAGEMENT.LIGA.TABLE.HEADERS.UEBERGEORDNETNAME',
+        propertyName:   'ligaUebergeordnetName',
+        width:          20,
+      },
+      {
+        translationKey:   'MANAGEMENT.LIGA.TABLE.HEADERS.VERANTWORTLICHMAIL',
+        propertyName:     'ligaVerantwortlichMail',
+        width:            7,
+        type:             TableColumnType.NUMBER,
+        currentSortOrder: TableColumnSortOrder.ASCENDING
+      }
+    ],
+    actions: {
+      actionTypes: [TableActionType.EDIT, TableActionType.DELETE],
+      width:       6
+    },
+  }
+};
