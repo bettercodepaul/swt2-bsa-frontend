e94039235d4084873632c927dcfd6d24022b4ddf
Ozan <ozan.guder@gmail.com>
2019-05-05 17:36:46 +0200

    adjustments to schusszettel component: added tables for match2 with correct bindings

--- bogenliga/src/app/modules/schusszettel/components/schusszettel/schusszettel.component.ts
+++ bogenliga/src/app/modules/schusszettel/components/schusszettel/schusszettel.component.ts
@@ -11,11 +11,7 @@ import {PasseDO} from '../../types/passe-do.class';
 export class SchusszettelComponent implements OnInit {
 
   match1: MatchDO;
-  sum1: number = 0;
-  sum2: number = 0;
-  sum3: number = 0;
-  sum4: number = 0;
-  sum5: number = 0;
+  match2: MatchDO;
 
   constructor() {
     console.log('Schusszettel Component')
@@ -25,16 +21,34 @@ export class SchusszettelComponent implements OnInit {
     //initialwert schützen inputs
 
     this.match1 = new MatchDO();
+    this.match1.matchNr = 1;
+    this.match1.sumSatz1 = 0;
+    this.match1.sumSatz2 = 0;
+    this.match1.sumSatz3 = 0;
+    this.match1.sumSatz4 = 0;
+    this.match1.sumSatz5 = 0;
+
+    this.match2 = new MatchDO();
+    this.match2.matchNr = 1;
+    this.match2.sumSatz1 = 0;
+    this.match2.sumSatz2 = 0;
+    this.match2.sumSatz3 = 0;
+    this.match2.sumSatz4 = 0;
+    this.match2.sumSatz5 = 0;
 
     for (let i = 0; i < 3; i++) {
       this.match1.schuetzen.push(new Array<PasseDO>());
+      this.match2.schuetzen.push(new Array<PasseDO>());
       for (let j = 0; j < 5; j++) {
         if (i == 0) {
-          this.match1.schuetzen[i].push(new PasseDO(null, this.match1.mannschaftId, this.match1.wettkampfId, this.match1.matchNr, i + 1))
+          this.match1.schuetzen[i].push(new PasseDO(null, this.match1.mannschaftId, this.match1.wettkampfId, this.match1.matchNr, i + 1));
+          this.match2.schuetzen[i].push(new PasseDO(null, this.match2.mannschaftId, this.match2.wettkampfId, this.match2.matchNr, i + 1));
         } else if (i == 1) {
-          this.match1.schuetzen[i].push(new PasseDO(null, this.match1.mannschaftId, this.match1.wettkampfId, this.match1.matchNr, i + 1))
+          this.match1.schuetzen[i].push(new PasseDO(null, this.match1.mannschaftId, this.match1.wettkampfId, this.match1.matchNr, i + 1));
+          this.match2.schuetzen[i].push(new PasseDO(null, this.match2.mannschaftId, this.match2.wettkampfId, this.match2.matchNr, i + 1));
         } else {
-          this.match1.schuetzen[i].push(new PasseDO(null, this.match1.mannschaftId, this.match1.wettkampfId, this.match1.matchNr, i + 1))
+          this.match1.schuetzen[i].push(new PasseDO(null, this.match1.mannschaftId, this.match1.wettkampfId, this.match1.matchNr, i + 1));
+          this.match2.schuetzen[i].push(new PasseDO(null, this.match2.mannschaftId, this.match2.wettkampfId, this.match2.matchNr, i + 1));
         }
       }
 
@@ -42,51 +56,52 @@ export class SchusszettelComponent implements OnInit {
   }
 
 
-  onChange(value: any, schuetzenNr: number, satzNr: number, pfeilNr: number) {
+  onChange(value: any, matchNr: number, schuetzenNr: number, satzNr: number, pfeilNr: number) {
+    let match = this['match' + matchNr];
     if (value.indexOf('+') !== -1) {
       switch (pfeilNr) {
         case 1:
-          this.match1.schuetzen[schuetzenNr][satzNr].ringzahlPfeil1 = 10;
+          match.schuetzen[schuetzenNr][satzNr].ringzahlPfeil1 = 10;
           break;
         case 2:
-          this.match1.schuetzen[schuetzenNr][satzNr].ringzahlPfeil2 = 10;
+          match.schuetzen[schuetzenNr][satzNr].ringzahlPfeil2 = 10;
           break;
       }
     } else {
-      value = +value;
+      value = +value; //value ist string, ringzahlen sollen number sein -> value in number umwandeln
       switch (pfeilNr) {
         case 1:
-          this.match1.schuetzen[schuetzenNr][satzNr].ringzahlPfeil1 = value.length > 0 ? value : null;
+          match.schuetzen[schuetzenNr][satzNr].ringzahlPfeil1 = value > 0 ? value : null;
           break;
         case 2:
-          this.match1.schuetzen[schuetzenNr][satzNr].ringzahlPfeil2 = value.length > 0 ? value : null;
+          match.schuetzen[schuetzenNr][satzNr].ringzahlPfeil2 = value > 0 ? value : null;
           break;
       }
     }
     if (satzNr == 0) {
-      this.sum1 = this.match1.schuetzen[0][0].ringzahlPfeil1 + this.match1.schuetzen[0][0].ringzahlPfeil2
-        + this.match1.schuetzen[1][0].ringzahlPfeil1 + this.match1.schuetzen[1][0].ringzahlPfeil2
-        + this.match1.schuetzen[2][0].ringzahlPfeil1 + this.match1.schuetzen[2][0].ringzahlPfeil2;
+      match.sumSatz1 = match.schuetzen[0][0].ringzahlPfeil1 + match.schuetzen[0][0].ringzahlPfeil2
+        + match.schuetzen[1][0].ringzahlPfeil1 + match.schuetzen[1][0].ringzahlPfeil2
+        + match.schuetzen[2][0].ringzahlPfeil1 + match.schuetzen[2][0].ringzahlPfeil2;
     }
     if (satzNr == 1) {
-      this.sum2 = +this.match1.schuetzen[0][1].ringzahlPfeil1 + this.match1.schuetzen[0][1].ringzahlPfeil2
-        + this.match1.schuetzen[1][1].ringzahlPfeil1 + this.match1.schuetzen[1][1].ringzahlPfeil2
-        + this.match1.schuetzen[2][1].ringzahlPfeil1 + this.match1.schuetzen[2][1].ringzahlPfeil2;
+      match.sumSatz2 = match.schuetzen[0][1].ringzahlPfeil1 + match.schuetzen[0][1].ringzahlPfeil2
+        + match.schuetzen[1][1].ringzahlPfeil1 + match.schuetzen[1][1].ringzahlPfeil2
+        + match.schuetzen[2][1].ringzahlPfeil1 + match.schuetzen[2][1].ringzahlPfeil2;
     }
     if (satzNr == 2) {
-      this.sum3 = +this.match1.schuetzen[0][2].ringzahlPfeil1 + this.match1.schuetzen[0][2].ringzahlPfeil2
-        + this.match1.schuetzen[1][2].ringzahlPfeil1 + this.match1.schuetzen[1][2].ringzahlPfeil2
-        + this.match1.schuetzen[2][2].ringzahlPfeil1 + this.match1.schuetzen[2][2].ringzahlPfeil2;
+      match.sumSatz3 = match.schuetzen[0][2].ringzahlPfeil1 + match.schuetzen[0][2].ringzahlPfeil2
+        + match.schuetzen[1][2].ringzahlPfeil1 + match.schuetzen[1][2].ringzahlPfeil2
+        + match.schuetzen[2][2].ringzahlPfeil1 + match.schuetzen[2][2].ringzahlPfeil2;
     }
     if (satzNr == 3) {
-      this.sum4 = +this.match1.schuetzen[0][3].ringzahlPfeil1 + this.match1.schuetzen[0][3].ringzahlPfeil2
-        + this.match1.schuetzen[1][3].ringzahlPfeil1 + this.match1.schuetzen[1][3].ringzahlPfeil2
-        + this.match1.schuetzen[2][3].ringzahlPfeil1 + this.match1.schuetzen[2][3].ringzahlPfeil2;
+      match.sumSatz4 = match.schuetzen[0][3].ringzahlPfeil1 + match.schuetzen[0][3].ringzahlPfeil2
+        + match.schuetzen[1][3].ringzahlPfeil1 + match.schuetzen[1][3].ringzahlPfeil2
+        + match.schuetzen[2][3].ringzahlPfeil1 + match.schuetzen[2][3].ringzahlPfeil2;
     }
     if (satzNr == 4) {
-      this.sum5 = +this.match1.schuetzen[0][4].ringzahlPfeil1 + this.match1.schuetzen[0][4].ringzahlPfeil2
-        + this.match1.schuetzen[1][4].ringzahlPfeil1 + this.match1.schuetzen[1][4].ringzahlPfeil2
-        + this.match1.schuetzen[2][4].ringzahlPfeil1 + this.match1.schuetzen[2][4].ringzahlPfeil2;
+      match.sumSatz5 = match.schuetzen[0][4].ringzahlPfeil1 + match.schuetzen[0][4].ringzahlPfeil2
+        + match.schuetzen[1][4].ringzahlPfeil1 + match.schuetzen[1][4].ringzahlPfeil2
+        + match.schuetzen[2][4].ringzahlPfeil1 + match.schuetzen[2][4].ringzahlPfeil2;
     }
   }
 }
