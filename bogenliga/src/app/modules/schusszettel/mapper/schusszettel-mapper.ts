b292e84ef943e6233ed7ddcfdeaa4fcc73f31655
Ozan <ozan.guder@gmail.com>
2019-05-08 21:41:12 +0200

    added schuetzenNr to passe, made input for schuetzenNr in template editable

--- bogenliga/src/app/modules/schusszettel/mapper/schusszettel-mapper.ts
+++ bogenliga/src/app/modules/schusszettel/mapper/schusszettel-mapper.ts
@@ -1,4 +1,3 @@
-import {DataTransferObject} from '@shared/data-provider';
 import {MatchDTO} from '../types/datatransfer/match-dto.class';
 import {MatchDO} from '../types/match-do.class';
 import {PasseDTO} from '../types/datatransfer/passe-dto.class';
@@ -32,6 +31,7 @@ export class SchusszettelMapper {
     const passen = [];
     for (let i = 0; i < payload.schuetzen.length; i++) {
       for (let j = 0; j < payload.schuetzen[i].length; j++) {
+        payload.schuetzen[i][j].schuetzenNr = payload.schuetzen[i][0].schuetzenNr;
         passen.push(this.passeToDTO(payload.schuetzen[i][j]));
       }
     }
@@ -58,7 +58,8 @@ export class SchusszettelMapper {
       payload.ringzahl[2],
       payload.ringzahl[3],
       payload.ringzahl[4],
-      payload.ringzahl[5])
+      payload.ringzahl[5],
+      payload.schuetzenNr)
   }
 
   static passeToDTO(payload: PasseDO): PasseDTO {
@@ -77,75 +78,8 @@ export class SchusszettelMapper {
       payload.matchNr,
       payload.lfdnr,
       payload.dsbMitgliedNr,
-      ringzahl)
+      ringzahl,
+      payload.schuetzenNr)
   }
 }
 
-export function matchToDO(payload: MatchDTO): MatchDO {
-  return new MatchDO(payload.id,
-    payload.mannschaftId,
-    payload.wettkampfId,
-    payload.nr,
-    payload.begegnung,
-    payload.scheibenNummer,
-    payload.matchpunkte,
-    payload.satzpunkte);
-}
-
-// export function matchToDTO(payload: MatchDO): MatchDTO {
-//   const passen = [];
-//   for (let i = 0; i < payload.schuetzen.length; i++) {
-//     for (let j = 0; j < payload.schuetzen[i].length; j++) {
-//       passen.push(payload.schuetzen[i][j]);
-//     }
-//   }
-//   return new MatchDTO(payload.id,
-//     payload.mannschaftId,
-//     payload.wettkampfId,
-//     payload.nr,
-//     payload.begegnung,
-//     payload.scheibenNummer,
-//     payload.matchpunkte,
-//     payload.satzpunkte,
-//     passen);
-// }
-//
-// export function passeToDO(payload: PasseDTO): PasseDO {
-//   return new PasseDO(payload.id,
-//     payload.mannschaftId,
-//     payload.wettkampfId,
-//     payload.matchNr,
-//     payload.lfdnr,
-//     payload.dsbMitgliedNr,
-//     payload.ringzahl[0],
-//     payload.ringzahl[1],
-//     payload.ringzahl[2],
-//     payload.ringzahl[3],
-//     payload.ringzahl[4],
-//     payload.ringzahl[5])
-// }
-//
-// export function passeToDTO(payload: PasseDO): PasseDTO {
-//   const ringzahl = [
-//       payload.ringzahlPfeil1,
-//       payload.ringzahlPfeil2,
-//       payload.ringzahlPfeil3,
-//       payload.ringzahlPfeil4,
-//       payload.ringzahlPfeil5,
-//       payload.ringzahlPfeil6
-//     ]
-//   ;
-//   return new PasseDTO(payload.id,
-//     payload.mannschaftId,
-//     payload.wettkampfId,
-//     payload.matchNr,
-//     payload.lfdnr,
-//     payload.dsbMitgliedNr,
-//     ringzahl)
-// }
-
-// export function fromPayloadArray(payload: DataTransferObject[]): Array<MatchDTO> {
-//   const list: Array<MatchDTO> = [];
-//   payload.forEach((single) => list.push(fromPayload(single)));
-//   return list;
-// }
