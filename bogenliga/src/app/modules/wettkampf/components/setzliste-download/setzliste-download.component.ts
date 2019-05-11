f01911e52c27941645244c4f5a3c1838bc688ae6
Robin Müller <robin.mueller@Student.Reutlingen-University.de>
2019-05-11 11:55:25 +0200

    Delete Pop-Up after downloading the setzliste document

--- bogenliga/src/app/modules/wettkampf/components/setzliste-download/setzliste-download.component.ts
+++ bogenliga/src/app/modules/wettkampf/components/setzliste-download/setzliste-download.component.ts
@@ -1,26 +1,12 @@
 import {Component, ElementRef, OnInit, ViewChild, Input} from '@angular/core';
 import {environment} from '../../../../../environments/environment';
-import {BogenligaResponse, UriBuilder} from '../../../shared/data-provider';
-import {NotificationService} from '../../../shared/services/notification';
-import {
-  Notification,
-  NotificationOrigin,
-  NotificationSeverity,
-  NotificationType,
-  NotificationUserAction
-} from '../../../shared/services/notification/types';
-import {SetzlisteProviderService} from '../../services/setzliste-provider.service';
-
-const NOTIFICATION_DOWNLOAD_SUCCESS = 'download_example_success';
-const NOTIFICATION_DOWNLOAD_FAILURE = 'download_example_failure';
+import {UriBuilder} from '../../../shared/data-provider';
 
 @Component({
   selector: 'bla-setzliste-download',
   templateUrl: './setzliste-download.component.html'
 })
 export class SetzlisteDownloadComponent implements OnInit {
-
-  public downloadingFile = false;
 
   // Get the value of the attribute from the html tag
   @Input()
@@ -29,12 +15,9 @@ export class SetzlisteDownloadComponent implements OnInit {
   @ViewChild('downloadLink')
   private aElementRef: ElementRef;
 
-  constructor(private setzlisteService: SetzlisteProviderService,
-              private notificationService: NotificationService) {
-  }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   public getDownloadUrl(path: string): string {
     return new UriBuilder()
@@ -43,65 +26,5 @@ export class SetzlisteDownloadComponent implements OnInit {
       .path(path)
       .path('?wettkampfid=' + this.wettkampfid)
       .build();
-  }
-
-  public download(fileType: string) {
-    this.downloadingFile = true;
-
-    const fileEnding = fileType.toLowerCase();
-    const fileName = fileEnding + ' -example.' + fileEnding;
-
-    console.log('Invoke ' + fileType + ' download');
-
-    this.setzlisteService.downloadExamplePdf(fileEnding, fileName, this.aElementRef)
-        .then((response) => this.handleSuccess(response, fileType))
-        .catch((response) => this.handleFailure(response, fileType));
-
-  }
-
-  private handleSuccess(response: BogenligaResponse<string>, fileType: string): void {
-
-    const notification: Notification = {
-      id: NOTIFICATION_DOWNLOAD_SUCCESS,
-      title: fileType,
-      description: 'Download completed: ' + response.payload,
-      severity: NotificationSeverity.INFO,
-      origin: NotificationOrigin.USER,
-      type: NotificationType.OK,
-      userAction: NotificationUserAction.PENDING
-    };
-
-    this.notificationService.observeNotification(NOTIFICATION_DOWNLOAD_SUCCESS)
-        .subscribe((myNotification) => {
-          if (myNotification.userAction === NotificationUserAction.ACCEPTED) {
-            console.log('Download ' + fileType + ' from ' + response.payload + ' completed');
-            this.downloadingFile = false;
-          }
-        });
-
-    this.notificationService.showNotification(notification);
-  }
-
-  private handleFailure(response: BogenligaResponse<void>, fileType: string): void {
-
-    const notification: Notification = {
-      id: NOTIFICATION_DOWNLOAD_FAILURE,
-      title: fileType,
-      description: 'Download failed',
-      severity: NotificationSeverity.ERROR,
-      origin: NotificationOrigin.USER,
-      type: NotificationType.OK,
-      userAction: NotificationUserAction.PENDING
-    };
-
-    this.notificationService.observeNotification(NOTIFICATION_DOWNLOAD_FAILURE)
-        .subscribe((myNotification) => {
-          if (myNotification.userAction === NotificationUserAction.ACCEPTED) {
-            console.log('Download ' + fileType + ' failed');
-            this.downloadingFile = false;
-          }
-        });
-
-    this.notificationService.showNotification(notification);
   }
 }
