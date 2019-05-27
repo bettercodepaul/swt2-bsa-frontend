1587601857e21ee8c8f869a065ded411797274ab
Ozan <ozan.guder@gmail.com>
2019-05-27 15:23:50 +0200

    added 2FA flag to CredentialsDTO

--- bogenliga/src/app/modules/user/types/model/credentials-dto.class.ts
+++ bogenliga/src/app/modules/user/types/model/credentials-dto.class.ts
@@ -3,11 +3,13 @@ import {DataTransferObject} from '../../../shared/data-provider';
 export class CredentialsDTO implements DataTransferObject {
   username: string;
   password: string;
+  twoFAenabled = false;
   code: string;
 
-  constructor(username: string, password: string, code?: string) {
+  constructor(username: string, password: string, twoFAenabled: boolean, code?: string) {
     this.username = username;
     this.password = password;
+    this.twoFAenabled = twoFAenabled;
     this.code = !!code ? code : null;
   }
 
