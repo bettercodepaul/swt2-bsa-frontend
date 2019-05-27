050b0c27f5f12f487c02ccb6a5985cea90b6d33b
Ozan <ozan.guder@gmail.com>
2019-05-27 15:15:23 +0200

    added checkbox for 2FA enabling to verwaltung.benutzer.benutzer_neu component

--- bogenliga/src/app/modules/user/types/credentials-do.class.ts
+++ bogenliga/src/app/modules/user/types/credentials-do.class.ts
@@ -5,10 +5,12 @@ export class CredentialsDO implements DataObject {
   password: string;
   rememberMe = false;
   code: string;
+  twoFAenabled = false;
 
-  constructor(username?: string, password?: string, code?: string) {
+  constructor(username?: string, password?: string, code?: string, twoFAenabled?: boolean) {
     this.username = !!username ? username : '';
     this.password = !!password ? password : '';
     this.code = !!code ? code : '';
+    this.twoFAenabled = twoFAenabled;
   }
 }
