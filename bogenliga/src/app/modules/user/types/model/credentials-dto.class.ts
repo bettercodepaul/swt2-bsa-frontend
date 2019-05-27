473f7f357c4cb41562da9f313b49064ea552e3a9
Ozan <ozan.guder@gmail.com>
2019-05-27 16:43:40 +0200

    refactoring: renamed twoFAenabled to isUsing2FA

--- bogenliga/src/app/modules/user/types/model/credentials-dto.class.ts
+++ bogenliga/src/app/modules/user/types/model/credentials-dto.class.ts
@@ -3,13 +3,13 @@ import {DataTransferObject} from '../../../shared/data-provider';
 export class CredentialsDTO implements DataTransferObject {
   username: string;
   password: string;
-  twoFAenabled = false;
+  isUsing2FA = false;
   code: string;
 
-  constructor(username: string, password: string, twoFAenabled: boolean, code?: string) {
+  constructor(username: string, password: string, isUsing2FA: boolean, code?: string) {
     this.username = username;
     this.password = password;
-    this.twoFAenabled = twoFAenabled;
+    this.isUsing2FA = isUsing2FA;
     this.code = !!code ? code : null;
   }
 
