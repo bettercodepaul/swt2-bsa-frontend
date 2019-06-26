0f6fdf7fbec224bab4efd3b3ee3b750793df87aa
Dominik Halle <dominik_jonathan.halle@student.reutlingen-university.de>
2019-06-26 11:19:53 +0200

    re-add accidentially removed schusszettel-route

--- bogenliga/src/app/app.routing.ts
+++ bogenliga/src/app/app.routing.ts
@@ -15,4 +15,5 @@ export const ROUTES: Routes = [
   {path: 'regionen', loadChildren: 'src/app/modules/regionen/regionen.module#RegionenModule'},
   {path: 'vereine', loadChildren: 'src/app/modules/vereine/vereine.module#VereineModule'},
   {path: 'playground', loadChildren: 'src/app/modules/playground/playground.module#PlaygroundModule'},
+  {path: 'schusszettel/:match1id/:match2id', loadChildren: 'src/app/modules/schusszettel/schusszettel.module#SchusszettelModule'}
 ];
