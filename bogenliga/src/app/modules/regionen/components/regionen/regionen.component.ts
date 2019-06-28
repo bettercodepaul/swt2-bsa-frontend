b6d74b86cd5b0bff122d94b1610077ccc285bfd4
LarsMaronde <Lars.Maronde@student.reutlingen-university.de>
2019-06-28 16:05:41 +0200

    BSAPP-251: sunburst demo working

--- bogenliga/src/app/modules/regionen/components/regionen/regionen.component.ts
+++ bogenliga/src/app/modules/regionen/components/regionen/regionen.component.ts
@@ -1,5 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {REGIONEN_CONFIG} from './regionen.config';
+import Sunburst from 'sunburst-chart';
 
 @Component({
   selector: 'bla-regionen',
@@ -9,10 +10,50 @@ export class RegionenComponent implements OnInit {
 
   public config = REGIONEN_CONFIG;
 
-  constructor() {
-  }
+  @ViewChild('chart') myDiv: ElementRef;
+
+  constructor() {}
 
   ngOnInit() {
+    this.loadSunburst();
   }
 
+  loadSunburst() {
+    const data = {
+      name: 'main',
+      color: 'magenta',
+      children: [{
+        name: 'a',
+        color: 'yellow',
+        size: 1
+      }, {
+        name: 'b',
+        color: 'red',
+        children: [{
+          name: 'ba',
+          color: 'orange',
+          size: 1
+        }, {
+          name: 'bb',
+          color: 'blue',
+          children: [{
+            name: 'bba',
+            color: 'green',
+            size: 1
+          }, {
+            name: 'bbb',
+            color: 'pink',
+            size: 1
+          }]
+        }]
+      }]
+    };
+
+
+    Sunburst()
+      .data(data)
+      .size('size')
+      .color('color')
+      (this.myDiv.nativeElement);
+  }
 }
