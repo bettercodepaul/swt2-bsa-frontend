c05ace3218b6b09ae291d61cebc66bf20d06c8de
Dominik Halle <dominik_jonathan.halle@student.reutlingen-university.de>
2019-07-02 12:52:53 +0200

    WIP: add basic tableteingabe implementation add wettkampftag field to match

--- bogenliga/src/app/modules/schusszettel/components/tableteingabe/tableteingabe.component.ts
+++ bogenliga/src/app/modules/schusszettel/components/tableteingabe/tableteingabe.component.ts
@@ -14,6 +14,36 @@ import {
 } from '@shared/services';
 import {WettkampfDO} from '@vereine/types/wettkampf-do.class';
 
+class SchuetzeErgebnisse {
+  schuetzeNr: Number;
+  passen: Array<PasseDO>;
+
+  constructor(schuetzeNr: number) {
+    this.passen = [];
+    this.schuetzeNr = Number.isNaN(schuetzeNr) ? null : schuetzeNr;
+  }
+}
+
+const dummyMatch = new MatchDO(
+  null,
+  null,
+  null,
+  1,
+  1,
+  1,
+  1,
+  [],
+  1,
+  1,
+  null,
+  null
+);
+
+const NUM_SCHUETZEN: number = 3;
+const STORAGE_KEY_SATZ_NR: string = 'satzNr';
+const STORAGE_KEY_SCHEIBE_NR: string = 'scheibenNummer';
+const STORAGE_KEY_SCHUETZE_PREFIX: string = 'schuetze';
+
 @Component({
   selector:    'bla-tableteingabe',
   templateUrl: './tableteingabe.component.html',
@@ -23,22 +53,27 @@ export class TabletEingabeComponent implements OnInit {
 
   match1: MatchDO;
   match2: MatchDO;
+  currentMatch: MatchDO;
   wettkampf: WettkampfDO;
+  scheibenNummer: Number;
+  satzNr: Number;
+  schuetzen: Array<SchuetzeErgebnisse>;
 
 
   constructor(private schusszettelService: SchusszettelProviderService,
-              private route: ActivatedRoute,
-              private notificationService: NotificationService) {
+    private route: ActivatedRoute,
+    private notificationService: NotificationService) {
   }
 
   /**
    * Called when component is initialized.
    */
   ngOnInit() {
-    // initialwert schützen inputs
+    this.initStorageData();
 
-    this.match1 = new MatchDO(null, null, null, 1, 1, 1, 1, [], 1, 1, null, null);
-    this.match2 = new MatchDO(null, null, null, 1, 1, 1, 1, [], 1, 1, null, null);
+    this.match1 = dummyMatch;
+    this.match2 = dummyMatch;
+    this.currentMatch = dummyMatch;
 
     this.route.params.subscribe((params) => {
       if (!isUndefined(params['match1id']) && !isUndefined(params['match2id'])) {
@@ -48,11 +83,69 @@ export class TabletEingabeComponent implements OnInit {
             .then((data: BogenligaResponse<Array<MatchDO>>) => {
               this.match1 = data.payload[0];
               this.match2 = data.payload[1];
+              this.currentMatch = this.match2.scheibenNummer === this.scheibenNummer ? this.match2 : this.match1;
+              this.dumpStorageData();
             }, (error) => {
               console.error(error);
             });
-
       }
+    });
+  }
+
+  /**
+   * Initializes component data using previously stored data in the local storage of the tablet.
+   */
+  initStorageData() {
+    // scheibenNummer has to be set in the tablet administration of wettkampf
+    let sNr = Number.parseInt(localStorage.getItem(STORAGE_KEY_SCHEIBE_NR));
+    let satzNr = Number.parseInt((localStorage.getItem(STORAGE_KEY_SATZ_NR)));
+    if (!isNaN(sNr)) {
+      this.scheibenNummer = sNr;
+    } else {
+      this.showMissingScheibenNummerNotification();
+    }
+    this.satzNr = (satzNr !== null && !isNaN(satzNr)) ? satzNr : 1;
+    this.schuetzen = [];
+    for (let i of Array(NUM_SCHUETZEN)) {
+      this.schuetzen.push(new SchuetzeErgebnisse(Number.parseInt((localStorage.getItem(STORAGE_KEY_SCHUETZE_PREFIX + i + 1)))));
+    }
+  }
+
+  dumpStorageData() {
+    localStorage.setItem(STORAGE_KEY_SATZ_NR, this.satzNr.toString());
+    if (this.hasSchuetzenNummern()) {
+      for (let i of Array(NUM_SCHUETZEN)) {
+        localStorage.setItem(STORAGE_KEY_SCHUETZE_PREFIX + i + 1, this.schuetzen[i].schuetzeNr.toString());
+      }
+    }
+  }
+
+  resetStorageData() {
+    localStorage.setItem(STORAGE_KEY_SATZ_NR, '');
+    for (let i of Array(NUM_SCHUETZEN)) {
+      localStorage.setItem(STORAGE_KEY_SCHUETZE_PREFIX + i + 1, '');
+    }
+  }
+
+  hasSchuetzenNummern() {
+    return (Boolean(this.schuetzen) &&
+      this.schuetzen[0].schuetzeNr !== null &&
+      this.schuetzen[1].schuetzeNr !== null &&
+      this.schuetzen[2].schuetzeNr !== null &&
+      this.schuetzen[0].schuetzeNr > 0 &&
+      this.schuetzen[1].schuetzeNr > 0 &&
+      this.schuetzen[2].schuetzeNr > 0);
+  }
+
+  showMissingScheibenNummerNotification() {
+    this.notificationService.showNotification({
+      id:          'tabletScheibenNummerMissing',
+      title:       'Konfigurationsfehler',
+      description: 'Für dieses Gerät wurde noch keine Scheibennummer zugewiesen. Bitte holen Sie dies in der Wettkampf-Administration nach.',
+      severity:    NotificationSeverity.ERROR,
+      origin:      NotificationOrigin.SYSTEM,
+      type:        NotificationType.OK,
+      userAction:  NotificationUserAction.PENDING
     });
   }
 
@@ -71,6 +164,7 @@ export class TabletEingabeComponent implements OnInit {
           this.match1 = data.payload[0];
           this.match2 = data.payload[1];
           this.notificationService.discardNotification();
+          this.dumpStorageData();
         }, (error) => {
           console.error(error);
           this.notificationService.discardNotification();
--- bogenliga/src/app/modules/schusszettel/schusszettel.routing.ts
+++ bogenliga/src/app/modules/schusszettel/schusszettel.routing.ts
@@ -1,7 +1,9 @@
 import {Routes} from '@angular/router';
 import {SchusszettelComponent} from './components/schusszettel/schusszettel.component';
+import {TabletEingabeComponent} from './components/tableteingabe/tableteingabe.component';
 
 
 export const SCHUSSZETTEL_ROUTES: Routes = [
   {path: '', pathMatch: 'full', component: SchusszettelComponent},
+  {path: 'tablet', pathMatch: 'full', component: TabletEingabeComponent},
 ];
