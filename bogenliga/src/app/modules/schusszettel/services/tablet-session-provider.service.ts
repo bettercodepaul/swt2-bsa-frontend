8e091f32954371120d53b7c6ef1925ec2466f8ad
Ozan <ozan.guder@gmail.com>
2019-07-03 18:48:08 +0200

    added update function to component and service

--- bogenliga/src/app/modules/schusszettel/services/tablet-session-provider.service.ts
+++ bogenliga/src/app/modules/schusszettel/services/tablet-session-provider.service.ts
@@ -71,6 +71,21 @@ export class TabletSessionProviderService extends DataProviderService {
     }));
   }
 
+  public update(session: TabletSessionDO): Promise<BogenligaResponse<TabletSessionDO>> {
+    return new Promise(((resolve, reject) => {
+      this.restClient.PUT(this.getUrl(), TabletMapper.sessionToDTO(session))
+          .then((data: TabletSessionDTO) => {
+            resolve({result: RequestResult.SUCCESS, payload: TabletMapper.sessionToDO(data)});
+          }, (error: HttpErrorResponse) => {
+            if (error.status === 0) {
+              reject({result: RequestResult.CONNECTION_PROBLEM});
+            } else {
+              reject({result: RequestResult.FAILURE});
+            }
+          });
+    }));
+  }
+
   public delete(wettkampfId: string, scheibenNr: string): Promise<BogenligaResponse<any>> {
     return new Promise((resolve, reject) => {
       this.restClient.DELETE<any>(new UriBuilder().fromPath(this.getUrl()).path(wettkampfId + '/' + scheibenNr).build())
