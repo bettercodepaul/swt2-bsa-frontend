f9a46e5b1094206811b2fa7616b3bd9b42426e5e
Ozan <ozan.guder@gmail.com>
2019-07-03 19:31:28 +0200

    added isActive flag to tabletsession DO/DTO

--- bogenliga/src/app/modules/schusszettel/types/datatransfer/tablet-session-dto.class.ts
+++ bogenliga/src/app/modules/schusszettel/types/datatransfer/tablet-session-dto.class.ts
@@ -4,15 +4,18 @@ export class TabletSessionDTO implements DataTransferObject {
 
   scheibenNr: number;
   wettkampfID: number;
+  isActive: boolean;
   satzNr: number;
   matchID: number;
 
   constructor(scheibenNr?: number,
     wettkampfID?: number,
+    isActive?: boolean,
     satzNr?: number,
     matchID?: number) {
     this.scheibenNr = !!scheibenNr ? scheibenNr : null;
     this.wettkampfID = !!wettkampfID ? wettkampfID : null;
+    this.isActive = !!isActive;
     this.satzNr = !!satzNr ? satzNr : null;
     this.matchID = !!matchID ? matchID : null;
   }
