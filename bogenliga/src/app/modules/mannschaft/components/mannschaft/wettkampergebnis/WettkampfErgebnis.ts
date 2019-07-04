690c2e386d3bc3c5ef2b16e83abe73ac7a6bfde9
pietscmi <michael.pietschmann@student.reutlingen-university.de>
2019-07-04 09:17:00 +0200

    BSAPP-145: Zu der Tabelle die Spalten hinzugefügt. Ein passendes Object dazu definiert. Services dazu angefangen.

---
+++ bogenliga/src/app/modules/mannschaft/components/mannschaft/wettkampergebnis/WettkampfErgebnis.ts
@@ -0,0 +1,53 @@
+import {VersionedDataObject} from '@shared/data-provider/models/versioned-data-object.interface';
+
+export class WettkampfErgebnis implements VersionedDataObject {
+  id: number;
+  version: number;
+
+  mannschaftName: string;
+  mannschaftSatz1: number;
+  mannschaftSatz2: number;
+  mannschaftSatz3: number;
+  mannschaftSatz4: number;
+  mannschaftSatz5: number;
+  opponentName: string;
+  opponentSatz1: number;
+  opponentSatz2: number;
+  opponentSatz3: number;
+  opponentSatz4: number;
+  opponentSatz5: number;
+  satzpunkte: string;
+  matchpunkte: string;
+
+  constructor(mannschaftName?: string,
+              mannschaftSatz1?: number,
+              mannschaftSatz2?: number,
+              mannschaftSatz3?: number,
+              mannschaftSatz4?: number,
+              mannschaftSatz5?: number,
+              opponentName?: string,
+              opponentSatz1?: number,
+              opponentSatz2?: number,
+              opponentSatz3?: number,
+              opponentSatz4?: number,
+              opponentSatz5?: number,
+              satzpunkte?: string,
+              matchpunkte?: string) {
+    this.mannschaftName = mannschaftName;
+    this.mannschaftSatz1 = mannschaftSatz1;
+    this.mannschaftSatz2 = mannschaftSatz2;
+    this.mannschaftSatz3 = mannschaftSatz3;
+    this.mannschaftSatz4 = mannschaftSatz4;
+    this.mannschaftSatz5 = mannschaftSatz5;
+    this.opponentName = opponentName;
+    this.opponentSatz1 = opponentSatz1;
+    this.opponentSatz2 = opponentSatz2;
+    this.opponentSatz3 = opponentSatz3;
+    this.opponentSatz4 = opponentSatz4;
+    this.opponentSatz5 = opponentSatz5;
+    this.satzpunkte = satzpunkte;
+    this.matchpunkte = matchpunkte;
+
+
+  }
+}
