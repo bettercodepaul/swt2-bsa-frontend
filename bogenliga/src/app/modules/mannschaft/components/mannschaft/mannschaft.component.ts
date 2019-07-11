cd8c037e88422c98126d0b8728e60f3229e4b374
Michael Dirksmöller <michael.dirksmoeller@exxcellent.de>
2019-07-11 08:17:33 +0200

    Zwischenstand ligatabelle

--- bogenliga/src/app/modules/mannschaft/components/mannschaft/mannschaft.component.ts
+++ bogenliga/src/app/modules/mannschaft/components/mannschaft/mannschaft.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {MANNSCHAFT_CONFIG} from './mannschaft.config';
 import {VeranstaltungDataProviderService} from '@vereine/services/veranstaltung-data-provider.service';
-import {VereinDataProviderService} from '@vereine/services/verein-data-provider.service';
+import {VereinDataProviderService} from '@verwaltung/services/verein-data-provider.service';
 import {CommonComponent, toTableRows} from '@shared/components';
 import {VereinDO} from '@vereine/types/verein-do.class';
 import {BogenligaResponse} from '@shared/data-provider';
