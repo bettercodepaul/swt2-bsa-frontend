9199160303164e74d6ad07d75d7993842af899f4
mpietschmann <michael.pietschmann@exxcellent.de>
2019-09-05 16:57:50 +0200

    Leere Manschaftstabellen-Component unter Veranstaltung-Details angelegt.

---
+++ bogenliga/src/app/modules/verwaltung/components/veranstaltung/veranstaltung-detail/mannschaftTabelle/mannschaftTabelle.component.spec.ts
@@ -0,0 +1,25 @@
+// import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+//
+// import {MannschaftTabelleComponent} from './mannschaftTabelle.component';
+//
+// describe('LigaDetailComponent', () => {
+//   let component: MannschaftTabelleComponent;
+//   let fixture: ComponentFixture<MannschaftTabelleComponent>;
+//
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [MannschaftTabelleComponent]
+//     })
+//            .compileComponents();
+//   }));
+//
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(MannschaftTabelleComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+//
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
