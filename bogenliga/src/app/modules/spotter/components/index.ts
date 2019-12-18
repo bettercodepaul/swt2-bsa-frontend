8513a9d5c9128e0f0ce80755020c45d380414263
Tobias Schuster <tobias.schuster@student.reutlingen-university.de>
2019-12-18 13:25:21 +0100

    Added Spotter Modules and Components with Routing
    Folder and File Structure was created. You now can navigate to the spotter dialogs via /spotter.

---
+++ bogenliga/src/app/modules/spotter/components/index.ts
@@ -0,0 +1,4 @@
+export * from './bahnen/bahnen.component';
+export * from './matches/matches.component';
+export * from './schuetzen/schuetzen.component';
+export * from './wettkaempfe/wettkaempfe.component';
---
+++ bogenliga/src/app/modules/spotter/components/wettkaempfe/wettkaempfe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bla-wettkaempfe',
+  templateUrl: './wettkaempfe.component.html',
+  styleUrls: ['./wettkaempfe.component.scss']
+})
+export class WettkaempfeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
