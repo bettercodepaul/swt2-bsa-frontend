2c224ed3abada62c63597a7397cdb939897a276f
GithubDomenico <domenico_alessandro.capano@student.reutlingen-university.de>
2020-01-02 15:09:40 +0100

    Fixed error messages in the beginning. (Still requires testing) (JWT TOKEN ISSUES)

--- bogenliga/src/app/modules/user/services/login-data-provider.service.ts
+++ bogenliga/src/app/modules/user/services/login-data-provider.service.ts
@@ -47,7 +47,7 @@ export class LoginDataProviderService extends DataProviderService {
   public signIn(credentialsDO: CredentialsDO): Promise<LoginResult> {
     // check remember me flag
     if(credentialsDO.username == 'ligadefault') {
-      // do nothing, fixes jwt token issue for default user
+      // do nothing, fixed jwt token issue for default user
     }
     if (credentialsDO.rememberMe) {
       this.currentUserService.rememberUsername(credentialsDO.username);
