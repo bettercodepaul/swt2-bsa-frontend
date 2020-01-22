4d654ea5692c020004e340aaa882591b3590093c
Tobias Schuster <tobias.schuster@student.reutlingen-university.de>
2020-01-22 16:25:01 +0100

    Added Types and basic functionality

--- bogenliga/src/app/modules/spotter/components/interface/interface.component.ts
+++ bogenliga/src/app/modules/spotter/components/interface/interface.component.ts
@@ -1,3 +1,4 @@
+import { Match } from './../../types/match';
 import { FullscreenService } from './../../../../services/fullscreen.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 
@@ -8,16 +9,78 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 })
 export class InterfaceComponent implements OnInit, OnDestroy {
 
+  selectedPlayNumber = 1;
+
+  match: Match;
+
   spotting = true;
+
+  selectedValue = -1;
 
   constructor(private fullscreenService: FullscreenService) { }
 
   ngOnInit() {
     this.fullscreenService.emitChange(true);
+
+    if (localStorage.getItem('match')) {
+      this.match = JSON.parse(localStorage.getItem('match'));
+    } else {
+      this.match = new Match('Nürtingen', 1);
+    }
   }
 
   ngOnDestroy(): void {
     this.fullscreenService.emitChange(false);
   }
+
+  onSave() {
+    if (this.selectedValue >= 0 && this.selectedValue <= 10) {
+      this.match.currentSet.currentPlay.number = this.selectedValue;
+
+      // send current play() then {
+      if (!this.match.addPlay()) {
+        this.match.addSet();
+      }
+
+      this.selectedValue = -1;
+
+      // }
+
+    }
+  }
+
+  onNextSet() {
+    if (!this.match.addSet()) {
+      // Send set to Server to confirm
+
+      // Create new Set
+    }
+  }
+
+  onFinishMatch() {
+    if (this.match.canFinish()) {
+      // Send match to Server to confirm
+      // -> when successful:
+      localStorage.removeItem('match');
+      this.match = new Match('Frickenhausen', 0);
+      // Send information to server that match is finished
+    }
+  }
+
+  onBack() {
+    if (this.match.lastSet() && this.match.currentSet.currentPlayNumber === 1) {
+      this.spotting = false;
+    }
+
+  }
+
+  selectResult(selected: any) {
+    this.selectedValue = selected;
+  }
+
+  changeFinal() {
+    this.match.currentSet.currentPlay.final = !this.match.currentSet.currentPlay.final;
+  }
+
 
 }
---
+++ bogenliga/src/app/modules/spotter/types/match.ts
@@ -0,0 +1,65 @@
+import { Set } from './set';
+
+export class Match {
+
+    sets: Array<Set>;
+
+    selectedPlayNumber: number;
+
+    currentSetNumber: number;
+    currentSet: Set;
+
+    mannschaft: string;
+    bahn: number;
+    satzpunkte: number;
+
+    constructor(mannschaft: string, bahn: number) {
+        this.mannschaft = mannschaft;
+        this.bahn = bahn;
+        this.satzpunkte = 0;
+
+        this.selectedPlayNumber = 1;
+        this.currentSetNumber = 1;
+        this.currentSet = new Set(this.currentSetNumber);
+
+        this.sets = new Array<Set>();
+        this.sets.push(this.currentSet);
+    }
+
+    addSet(): boolean {
+        if (this.currentSet.canFinish()) {
+            this.currentSetNumber++;
+            this.currentSet = new Set(this.currentSetNumber);
+            this.sets.push(this.currentSet);
+            return true;
+        }
+        return false;
+    }
+
+    addPlay(): boolean {
+        if (!this.currentSet.addPlay()) {
+            this.currentSetNumber++;
+            this.currentSet = new Set(this.currentSetNumber);
+            this.sets.push(this.currentSet);
+            return true;
+        }
+        return false;
+    }
+
+    canFinish(): boolean {
+        this.sets.forEach((set: Set) => {
+            if (!set.canFinish()) {
+                return false;
+            }
+        });
+        return true;
+    }
+
+    lastSet() {
+        if (this.currentSetNumber > 1) {
+            return this.sets[this.sets.length - 2];
+        }
+        return null;
+    }
+
+}
