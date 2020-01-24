da1c4a41990c9618c82971fe5cbe76d213895295
AnGsmn <annika.gessmann@student.reutlingen-university.de>
2020-01-24 14:29:21 +0100

    BSAPP-572 "Einbinden der Eingabemöglichkeit für den Authentifizierungscode" abgeschlossen

--- bogenliga/src/app/modules/spotter/components/authentication/authentication.component.ts
+++ bogenliga/src/app/modules/spotter/components/authentication/authentication.component.ts
@@ -1,12 +1,15 @@
 import {Component, OnInit} from '@angular/core';
+import {ButtonSize} from '../../../shared/components/buttons';
 
 @Component({
   selector: 'bla-authentication',
   templateUrl: './authentication.component.html',
-  //styleUrls: ['./authentication.component.scss']
+  styleUrls: ['./authentication.component.scss']
 })
 
 export class AuthenticationComponent implements OnInit {
+
+  public ButtonSize = ButtonSize;
 
   constructor() { }
 
