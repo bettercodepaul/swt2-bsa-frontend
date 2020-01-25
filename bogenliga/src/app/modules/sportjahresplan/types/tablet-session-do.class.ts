570a64263db2950686463852b4ee30e20f9d818d
Christian Metzger <metzger-christian@hotmail.de>
2020-01-25 21:04:26 +0100

    Created function for creating random six digit token

--- bogenliga/src/app/modules/sportjahresplan/types/tablet-session-do.class.ts
+++ bogenliga/src/app/modules/sportjahresplan/types/tablet-session-do.class.ts
@@ -8,18 +8,21 @@ export class TabletSessionDO implements DataObject {
   satzNr: number;
   matchID: number;
   otherMatchId: number;
+  accessToken: number;
 
   constructor(scheibenNr?: number,
               wettkampfID?: number,
               isActive?: boolean,
               satzNr?: number,
               matchID?: number,
-              otherMatchID?: number) {
+              otherMatchID?: number,
+              accessToken?: number) {
     this.scheibenNr = !!scheibenNr ? scheibenNr : null;
     this.wettkampfID = !!wettkampfID ? wettkampfID : null;
     this.isActive = !!isActive;
     this.satzNr = !!satzNr ? satzNr : null;
     this.matchID = !!matchID ? matchID : null;
     this.otherMatchId = !!otherMatchID ? otherMatchID : null;
+    this.accessToken = !!accessToken ? accessToken : accessToken;
   }
 }
