8c741b6a69ef7b5abe8f3f8c237e1908b61773de
bartu26 <bartu26@gmx.de>
2020-01-29 20:39:37 +0100

    + Änderung im Code: BSAPP-550; Benachrichtigungstext in de.json ausgelagert

--- bogenliga/src/app/modules/sportjahresplan/components/schusszettel/schusszettel.component.ts
+++ bogenliga/src/app/modules/sportjahresplan/components/schusszettel/schusszettel.component.ts
@@ -21,6 +21,7 @@ const NOTIFICATION_WEITER_SCHALTEN = 'schusszettel_weiter';
 const NOTIFICATION_SCHUSSZETTEL_EINGABEFEHLER = 'schusszettelEingabefehler';
 const NOTIFICATION_SCHUSSZETTEL_ENTSCHIEDEN = 'schusszettelEntschieden';
 const NOTIFICATION_SCHUSSZETTEL_SPEICHERN = 'schusszettelSave';
+const NOTIFICATION_SCHUSSZETTEL_SCHUETZENNUMMER = 'schusszettelEntschieden';
 
 
 @Component({
@@ -150,11 +151,11 @@ export class SchusszettelComponent implements OnInit {
         userAction:  NotificationUserAction.ACCEPTED
       });
     } else if (this.match1.schuetzen[0][0].schuetzeNr == null || this.match1.schuetzen[1][0].schuetzeNr == null || this.match1.schuetzen[2][0].schuetzeNr == null
-    || this.match2.schuetzen[0][0].schuetzeNr == null || this.match2.schuetzen[1][0].schuetzeNr == null || this.match2.schuetzen[2][0].schuetzeNr == null) {
+      || this.match2.schuetzen[0][0].schuetzeNr == null || this.match2.schuetzen[1][0].schuetzeNr == null || this.match2.schuetzen[2][0].schuetzeNr == null) {
       this.notificationService.showNotification({
-        id:          'NOTIFICATION_SCHUSSZETTEL_ENTSCHIEDEN',
-        title:       'SPORTJAHRESPLAN.SCHUSSZETTEL.NOTIFICATION.ENTSCHIEDEN.TITLE',
-        description: 'Bitte unter Schuetze in alle Felder eine Schuetzennummer eintragen!', // hier ist die Meldung direkt eingefügt
+        id:          'NOTIFICATION_SCHUSSZETTEL_SCHUETZENNUMMER',
+        title:       'SPORTJAHRESPLAN.SCHUSSZETTEL.NOTIFICATION.SCHUETZENNUMMER.TITLE',
+        description: 'SPORTJAHRESPLAN.SCHUSSZETTEL.NOTIFICATION.SCHUETZENNUMMER.DESCRIPTION',
         severity:    NotificationSeverity.ERROR,
         origin:      NotificationOrigin.SYSTEM,
         type:        NotificationType.OK,
