487319bb93505e5a3fc7d917bf0b704ccf3d6653
lxvongobsthndlE <alexander.schreiner@student.reutlingen-university.de>
2020-05-27 19:19:59 +0200

    Routing works

--- bogenliga/src/app/modules/ligatabelle/ligatabelle.routing.ts
+++ bogenliga/src/app/modules/ligatabelle/ligatabelle.routing.ts
@@ -4,5 +4,6 @@ import {LigatabelleGuard} from './guards/ligatabelle.guard';
 
 
 export const LIGATABELLE_ROUTES: Routes = [
-  {path: '', component: LigatabelleComponent, canActivate: [LigatabelleGuard]}
+  {path: '', component: LigatabelleComponent, canActivate: [LigatabelleGuard], pathMatch: 'full'},
+  {path: '/:id', component: LigatabelleComponent, canActivate: [LigatabelleGuard], pathMatch: 'full'}
 ];
