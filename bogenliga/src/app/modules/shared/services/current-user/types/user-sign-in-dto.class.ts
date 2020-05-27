de12b13287e2bc50bd50dad83cac945d4c330027
maudeees <f.maute@gmail.com>
2020-05-27 14:53:32 +0200

    ~user-sign-in-dto: switched permissions into string format, so it corresponds to the payload to the redux-store created after Login. The "qrCode"-Key-Value-Pair was also added because of the same reason above ~current-user.service: Added atrribute "currentUserPermissions"; within the method "loadCurrentUser" this array is filled with the values of the permissions which are set; Now the permissions are correctly set and refreshed on each "loadCurrentUser" and it fits to the information into the redux store

--- bogenliga/src/app/modules/shared/services/current-user/types/user-sign-in-dto.class.ts
+++ bogenliga/src/app/modules/shared/services/current-user/types/user-sign-in-dto.class.ts
@@ -6,13 +6,16 @@ export class UserSignInDTO implements DataTransferObject {
   id: number;
   version: number;
   email: string;
+  qrCode: string;
   jwt: string;
-  permissions: UserPermission[];
+  permissions: string[];
+  //permissionsID: UserPermission[];
 
   static copyFromJson(json: {
     id: number,
     version: number,
     email: string,
+    qrCode: string,
     jwt: string,
     permissions?: string[]
   }): UserSignInDTO {
@@ -20,15 +23,18 @@ export class UserSignInDTO implements DataTransferObject {
     userSign.id = json.id;
     userSign.version = json.version;
     userSign.email = json.email;
+    userSign.qrCode = json.qrCode;
     userSign.jwt = json.jwt;
-
     userSign.permissions = [];
-
-    // map string list to enum list
+    //userSign.permissionsID = [];
+    // do not map permissions to enum-values here because the mapping is done in
+    // the current-user-service from the String to the value (e.g. "CAN_READ_DEFAULT" -> 0).
+    // Therefore the refresh by pressing "F5" will not set the permissions right away
+    // in the frontend an will not show the icons within the sidebar to navigate through
+    // the options you should be able to navigate.
     if (json.permissions) {
       json.permissions.forEach((permission) => {
-        const userPermission = UserPermission[permission];
-        userSign.permissions.push(userPermission);
+        userSign.permissions.push(permission);
       });
     }
     return userSign;
@@ -38,8 +44,10 @@ export class UserSignInDTO implements DataTransferObject {
     this.id = 0;
     this.version = 0;
     this.email = null;
+    this.qrCode = null;
     this.jwt = null;
     this.permissions = null;
+    //this.permissionsID = null;
   }
 
 }
