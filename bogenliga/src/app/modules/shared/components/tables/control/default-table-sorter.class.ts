a24e844ff8393db163de6ecf747665cbc35b3168
Dennis Zeller <dennis_rolf.zeller@student.reutlingen-university.de>
2020-06-10 13:09:34 +0200

    ~fixed some misspellings.

--- bogenliga/src/app/modules/shared/components/tables/control/default-table-sorter.class.ts
+++ bogenliga/src/app/modules/shared/components/tables/control/default-table-sorter.class.ts
@@ -16,7 +16,7 @@ export class DefaultTableSorter extends BaseTableSorter {
           return this.sortNumber(currentlySortedColumn);
         case TableColumnType.DATE:
           return this.sortOnDate(currentlySortedColumn);
-        // If its possible that null values appear in a collumn, use this if you want to make sure that null is
+        // If its possible that null values appear in a column, use this if you want to make sure that null is
         // smaller than numbers if you use a sort algorithm.
         case TableColumnType.NULLORNUMBER:
           return this.sortByNull(currentlySortedColumn);
