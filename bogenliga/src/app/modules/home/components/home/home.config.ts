8f89bfc046c54b6224232e34465ff1271af8eb97
Philipp Lohmann <philipp_franz.lohmann@student.reutlingen-university.de>
2020-06-12 13:42:50 +0200

    BSAPP-534 Frontpage (Home) and Vereine added detailed address, aswell as action for a hyperlink towards GoogleMaps.

--- bogenliga/src/app/modules/home/components/home/home.config.ts
+++ bogenliga/src/app/modules/home/components/home/home.config.ts
@@ -1,8 +1,11 @@
 import {CommonDialogConfig} from '../../../shared/components/dialogs';
+import {TableConfig} from '@shared/components/tables/types/table-config.interface';
+import {TableActionType} from '@shared/components/tables/types/table-action-type.enum';
 
 
 export const HOME_CONFIG: CommonDialogConfig = {
   moduleTranslationKey:    'HOME',
   pageTitleTranslationKey: 'HOME.HOME.TITLE'
 };
+
 
