57f10c3b0dc5e2324b20d3eb5db5472953b31df2
Jonas Liedel <jonas_matthias.liedel@student.reutlingen-university.de>
2020-06-15 15:35:10 +0200

    Zweiter Download-Button für Rückennummern erstellt

--- bogenliga/src/app/modules/verwaltung/components/verein/verein-detail/mannschafts-detail/mannschaft-detail.config.ts
+++ bogenliga/src/app/modules/verwaltung/components/verein/verein-detail/mannschafts-detail/mannschaft-detail.config.ts
@@ -32,7 +32,7 @@ export const MANNSCHAFT_DETAIL_TABLE_CONFIG: TableConfig = {
     },
   ],
   actions: {
-    actionTypes: [TableActionType.DELETE, TableActionType.DOWNLOAD],
+    actionTypes: [TableActionType.DELETE, TableActionType.DOWNLOAD, TableActionType.DOWNLOADRUECKENNUMMER],
     width:       6
   },
 
