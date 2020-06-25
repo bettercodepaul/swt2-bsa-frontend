938534c1c78494afb3f4e173c817447e2a21abab
Maike Knauer <maike_jasmin.knauer@student.reutlingen-university.de>
2020-06-25 13:40:27 +0200

    modified permissions

--- bogenliga/src/app/modules/verwaltung/guards/region-detail.guard.ts
+++ bogenliga/src/app/modules/verwaltung/guards/region-detail.guard.ts
@@ -9,6 +9,6 @@ export class RegionDetailGuard implements CanActivate {
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     return this.currentUserService.hasAnyPermisson(
-      [UserPermission.CAN_MODIFY_STAMMDATEN]);
+      [UserPermission.CAN_MODIFY_SYSTEMDATEN]);
   }
 }
