6f02103bc61cec51a64961841bd311bc4856c375
maudeees <f.maute@gmail.com>
2020-07-05 21:39:51 +0200

    ~de.json/en.json: Texte für CONNECTION_ERROR. Connection error wird benützt um ein abgelaufenens Session Token zu erkennen/behandeln. Dies soll kein Dauerzustand sein, jedoch wird maskiert dieser Fehlercode den eigentlichen. ~Ablaufzeit der Tokens für Produktiv- und Localumgebung wieder heraufgesetzt auf 1 Stunde

--- bogenliga/src/app/modules/shared/data-provider/interceptors/error-interceptor.class.ts
+++ bogenliga/src/app/modules/shared/data-provider/interceptors/error-interceptor.class.ts
@@ -31,13 +31,13 @@ export class ErrorInterceptor implements HttpInterceptor {
 
                      // handle connection (0), client (4xx), server (5xx) and custom error codes (9xx)
                      if (error.status === 0) {
-                       console.log("Exipred Token", error);
+                       console.log('Exipred Token', error);
                        this.router.navigateByUrl('user/login');
                        this.currentUserService.logout();
                        return this.errorHandlingService.handleHttpError(error);
                        // caught and handle the error
                        // return of(error);
-                     } else if(error.status >= 400){
+                     } else if (error.status >= 400) {
                        return this.errorHandlingService.handleHttpError(error);
                      }
 
