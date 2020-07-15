47a77782e75929065926279bac6485ef3e794230
fipsi.schmidt@gmail.com <fdsawertP1@>
2020-07-15 16:40:45 +0200

    BSAPP-610: dialog added for veranstaltungen with selectionlist

--- bogenliga/src/app/modules/verwaltung/components/veranstaltung/veranstaltung-overview/veranstaltung-overview.component.ts
+++ bogenliga/src/app/modules/verwaltung/components/veranstaltung/veranstaltung-overview/veranstaltung-overview.component.ts
@@ -1,4 +1,3 @@
-
 import {Component, EventEmitter, Input, OnInit, Output, SimpleChanges} from '@angular/core';
 import {Router} from '@angular/router';
 import {CommonComponent} from '../../../../shared/components/common';
@@ -6,7 +5,6 @@ import {hideLoadingIndicator, showDeleteLoadingIndicatorIcon, toTableRows} from 
 import {TableRow} from '../../../../shared/components/tables/types/table-row.class';
 import {BogenligaResponse} from '../../../../shared/data-provider';
 import {VersionedDataObject} from '../../../../shared/data-provider/models/versioned-data-object.interface';
-import {isNullOrUndefined} from '@shared/functions';
 import {
   Notification,
   NotificationOrigin,
@@ -18,19 +16,12 @@ import {
 import {VeranstaltungDataProviderService} from '../../../services/veranstaltung-data-provider.service';
 import {VeranstaltungDTO} from '../../../types/datatransfer/veranstaltung-dto.class';
 import {VeranstaltungDO} from '../../../types/veranstaltung-do.class';
-import {VERANSTALTUNG_OVERVIEW_CONFIG} from './veranstaltung-overview.config';
+import {VERANSTALTUNG_OVERVIEW_TABLE_CONFIG} from './veranstaltung-overview.config';
 import {NOTIFICATION_DELETE_LIGA} from '@verwaltung/components/liga/liga-overview/liga-overview.component';
 import {SportjahrVeranstaltungDTO} from '@verwaltung/types/datatransfer/sportjahr-veranstaltung-dto';
 import {SportjahrVeranstaltungDO} from '@verwaltung/types/sportjahr-veranstaltung-do';
-import {PlaygroundVersionedDataObject} from '../../../../playground/components/playground/types/playground-versioned-data-object.class';
-import {of} from 'rxjs';
-import {delay} from 'rxjs/operators';
+
 import {CurrentUserService, UserPermission} from '@shared/services';
-import {SportjahrVeranstaltungDTO} from '@verwaltung/types/datatransfer/sportjahr-veranstaltung-dto';
-import {SportjahrVeranstaltungDO} from '@verwaltung/types/sportjahr-veranstaltung-do';
-import {PlaygroundVersionedDataObject} from '../../../../playground/components/playground/types/playground-versioned-data-object.class';
-import {of} from 'rxjs';
-import {delay} from 'rxjs/operators';
 
 export const NOTIFICATION_DELETE_VERANSTALTUNG = 'veranstaltung_overview_delete';
 
@@ -41,17 +32,16 @@ export const NOTIFICATION_DELETE_VERANSTALTUNG = 'veranstaltung_overview_delete'
 })
 export class VeranstaltungOverviewComponent extends CommonComponent implements OnInit {
 
-  public config = VERANSTALTUNG_OVERVIEW_CONFIG;
+  public table_config = VERANSTALTUNG_OVERVIEW_TABLE_CONFIG;
+
   public rows: TableRow[];
 
   public loading = true;
   public loadingYear = true;
   public loadingSearch = true;
-  public selectDTO: SportjahrVeranstaltungDO[];
-  public selecetedYear = 2020;
+  public selecetedYear = this.getCurrentYear();
   public selectedDTOs: SportjahrVeranstaltungDO[];
   public multipleSelections = true;
-
 
   constructor(private veranstaltungDataProvider: VeranstaltungDataProviderService,
               private router: Router, private notificationService: NotificationService,
@@ -60,9 +50,16 @@ export class VeranstaltungOverviewComponent extends CommonComponent implements O
   }
 
   ngOnInit() {
-    this.loadTableRows();
-    this.loadDistinctSporjahr();
+   // this.loadTableRows();
+
     this.loadBySportjahr();
+    this.loadDistinctSporjahr();
+
+  }
+
+  private getCurrentYear(): number {
+    this.selecetedYear = new Date().getFullYear();
+    return this.selecetedYear;
   }
 
 
@@ -71,6 +68,7 @@ export class VeranstaltungOverviewComponent extends CommonComponent implements O
   }
 
   public onEdit(versionedDataObject: VersionedDataObject): void {
+    console.log('veranstaltung-overview.component.ts');
     this.navigateToDetailDialog(versionedDataObject);
   }
 
@@ -94,32 +92,20 @@ export class VeranstaltungOverviewComponent extends CommonComponent implements O
         .subscribe((myNotification) => {
           if (myNotification.userAction === NotificationUserAction.ACCEPTED) {
             this.veranstaltungDataProvider.deleteById(id)
-                .then((response) => this.loadTableRows())
+                .then((response) => this.loadBySportjahr())
                 .catch((response) => this.rows = hideLoadingIndicator(this.rows, id));
           } else if (myNotification.userAction === NotificationUserAction.DECLINED) {
             this.rows = hideLoadingIndicator(this.rows, id);
           }
         });
-
     this.notificationService.showNotification(notification);
-  }
-
-  private loadTableRows() {
-    this.loading = true;
-    this.veranstaltungDataProvider.findAll()
-        .then((response: BogenligaResponse<VeranstaltungDO[]>) => this.handleLoadTableRowsSuccess(response))
-        .catch((response: BogenligaResponse<VeranstaltungDTO[]>) => this.handleLoadTableRowsFailure(response));
-  }
-
-  private handleLoadTableRowsFailure(response: BogenligaResponse<VeranstaltungDTO[]>): void {
-    this.rows = [];
-    this.loading = false;
   }
 
   private handleLoadTableRowsSuccess(response: BogenligaResponse<VeranstaltungDO[]>): void {
     if (this.currentUserService.hasPermission(UserPermission.CAN_MODIFY_MY_VERANSTALTUNG)) {
       response.payload = response.payload.filter((entry) => this.currentUserService.hasVeranstaltung(entry.id));
-      console.log('detected'); }
+      console.log('detected');
+    }
     this.rows = []; // reset array to ensure change detection
     this.rows = toTableRows(response.payload);
     this.loading = false;
@@ -133,17 +119,10 @@ export class VeranstaltungOverviewComponent extends CommonComponent implements O
 
   private loadBySportjahr(): void {
     this.loadingSearch = true;
+    console.log(this.selecetedYear);
     this.veranstaltungDataProvider.findBySportyear(this.selecetedYear)
-        .then((newList: BogenligaResponse<VeranstaltungDO[]>) => this.handleLoadBySportjahrSecess(newList))
+        .then((newList: BogenligaResponse<VeranstaltungDO[]>) => this.handleLoadTableRowsSuccess(newList))
         .catch((newList: BogenligaResponse<VeranstaltungDTO[]>) => this.handleLoadBySportjahrfailure(newList));
-
-  }
-
-  private handleLoadBySportjahrSecess(response: BogenligaResponse<VeranstaltungDO[]>): void {
-    this.rows = [];
-    this.rows = toTableRows(response.payload);
-    this.loadingSearch = false;
-
 
   }
 
@@ -166,7 +145,7 @@ export class VeranstaltungOverviewComponent extends CommonComponent implements O
     this.selectedDTOs = [];
     this.selectedDTOs = response.payload;
     this.loadingYear = false;
-
+    console.log(this.selectedDTOs);
 
   }
 
@@ -175,5 +154,10 @@ export class VeranstaltungOverviewComponent extends CommonComponent implements O
     this.loadingYear = false;
   }
 
+  public onSelect($event: SportjahrVeranstaltungDO[]): void {
+    this.selecetedYear = null;
+    this.selecetedYear = $event[0].sportjahr;
+    this.loadBySportjahr();
+  }
 
 }
