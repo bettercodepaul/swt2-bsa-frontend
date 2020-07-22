85e4bbd0a5c237571f0eaeecc1c712f79b3f4bf8
Jonas Liedel <jonas_matthias.liedel@student.reutlingen-university.de>
2020-07-22 20:13:18 +0200

    Ausrichter einem WettkampfTag zuweisen

--- bogenliga/src/app/modules/verwaltung/types/datatransfer/wettkampf-dto.class.ts
+++ bogenliga/src/app/modules/verwaltung/types/datatransfer/wettkampf-dto.class.ts
@@ -12,6 +12,7 @@ export class WettkampfDTO implements DataTransferObject {
   wettkampfDisziplinId: number;
   wettkampfTypId: number;
   version: number;
+  wettkampfAusrichter: number;
 
   constructor(id?: number,
               wettkampfVeranstaltungsId?: number,
@@ -21,7 +22,8 @@ export class WettkampfDTO implements DataTransferObject {
               wettkampfTag?: number,
               wettkampfDisziplinId?: number,
               wettkampfTypId?: number,
-              version?: number) {
+              version?: number,
+              wettkampfAusrichter ?: number) {
     this.id = !!id ? id : null;
     this.wettkampfVeranstaltungsId = !!wettkampfVeranstaltungsId ? wettkampfVeranstaltungsId : null;
     this.wettkampfDatum = !!wettkampfDatum ? wettkampfDatum : '';
@@ -31,6 +33,7 @@ export class WettkampfDTO implements DataTransferObject {
     this.wettkampfDisziplinId = !!wettkampfDisziplinId ? wettkampfDisziplinId : null;
     this.wettkampfTypId = !!wettkampfTypId ? wettkampfTypId : null;
     this.version = !!version ? version : null;
+    this.wettkampfAusrichter = !!wettkampfAusrichter ? wettkampfAusrichter : null;
   }
 
 
@@ -44,6 +47,7 @@ export class WettkampfDTO implements DataTransferObject {
     wettkampfDisziplinId?: number;
     wettkampfTypId?: number;
     version?: number;
+    wettkampfAusrichter?: number;
   } = {}): WettkampfDTO {
     const copy = new WettkampfDTO();
 
@@ -78,6 +82,11 @@ export class WettkampfDTO implements DataTransferObject {
       copy.wettkampfTypId = null;
     }
     copy.version = optional.version || null;
+    if(optional.wettkampfAusrichter >= 0){
+      copy.wettkampfAusrichter = optional.wettkampfAusrichter;
+    }else{
+      copy.wettkampfAusrichter = null;
+    }
 
     return copy;
   }
