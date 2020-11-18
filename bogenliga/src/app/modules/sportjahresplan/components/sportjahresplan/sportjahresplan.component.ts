17275c27245fcb18a52c3d26d54cc8d71f7ead07
DavinciLeonardo <davinci98@online.de>
2020-11-18 15:55:03 +0100

    BSAPP-712: Added '+' between splitted location string + commented the method

--- bogenliga/src/app/modules/sportjahresplan/components/sportjahresplan/sportjahresplan.component.ts
+++ bogenliga/src/app/modules/sportjahresplan/components/sportjahresplan/sportjahresplan.component.ts
@@ -116,15 +116,21 @@ export class SportjahresplanComponent extends CommonComponentDirective implement
 // TODO URL-Sprung bei TabletButtonClick
   }
 
-  // when a Action gets selected from the list --> Link to Google Maps
-
+  /** DUPLICATE
+   * Creates Link to Google Maps
+   * Splits given Location at every comma and passes it to Google Maps
+   * @param $event
+   */
   public onMap($event: WettkampfDO): void {
 
     const str = $event.wettkampfOrt;
     let splits: string[];
-    splits = str.split(',', 5);
+    splits = str.split(', ', 5);
     let locationUrl = 'https://www.google.de/maps/place/';
     for(let i = 0; i < splits.length; i++){
+      if (i!=0){
+        locationUrl += '+';
+      }
       locationUrl += splits[i];
     }
     window.open(locationUrl);
