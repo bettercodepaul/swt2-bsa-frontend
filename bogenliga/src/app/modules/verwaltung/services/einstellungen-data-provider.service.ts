0c6ee69a97437b5cd264e16e28d4669bcb729c83
LarsBahnmueller <Lars_Herbert.Bahnmueller@Student.Reutlingen-University.DE>
2020-11-27 13:20:46 +0100

    BSAPP-722 -> base structur for communication

---
+++ bogenliga/src/app/modules/verwaltung/services/einstellungen-data-provider.service.ts
@@ -0,0 +1,49 @@
+import {Injectable} from '@angular/core';
+import {
+  BogenligaResponse,
+  DataProviderService,
+  RequestResult,
+  RestClient,
+  UriBuilder,
+  VersionedDataTransferObject
+} from '../../shared/data-provider';
+import {CurrentUserService} from '../../shared/services/current-user';
+import {EinstellungenDO} from '@verwaltung/types/einstellungen-do.class';
+import {fromPayloadArray} from '@verwaltung/mapper/einstellungen-mapper';
+import {HttpErrorResponse} from '@angular/common/http';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EinstellungenProviderService  extends DataProviderService {
+  serviceSubUrl = 'v1/einstellungen';
+
+
+  constructor(private restClient: RestClient, private currentUserService: CurrentUserService) {
+    super();
+  }
+
+  public findAll(): Promise<BogenligaResponse<EinstellungenDO[]>> {
+    // return promise
+    // sign in success -> resolve promise
+    // sign in failure -> reject promise with result
+    return new Promise((resolve, reject) => {
+      this.restClient.GET<Array<VersionedDataTransferObject>>(this.getUrl())
+          .then((data: VersionedDataTransferObject[]) => {
+
+            resolve({result: RequestResult.SUCCESS, payload: fromPayloadArray(data)});
+
+          }, (error: HttpErrorResponse) => {
+
+            if (error.status === 0) {
+              reject({result: RequestResult.CONNECTION_PROBLEM});
+            } else {
+              reject({result: RequestResult.FAILURE});
+            }
+          });
+    });
+  }
+
+}
