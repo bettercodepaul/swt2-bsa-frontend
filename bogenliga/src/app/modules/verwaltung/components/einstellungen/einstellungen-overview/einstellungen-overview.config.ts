c51d7e3dd768cd57bf2e716922998b5eea2f6f08
LarsBahnmueller <Lars_Herbert.Bahnmueller@Student.Reutlingen-University.DE>
2020-12-07 21:51:11 +0100

    BSAPP-722 -> 21:50 Uhr

--- bogenliga/src/app/modules/verwaltung/components/einstellungen/einstellungen-overview/einstellungen-overview.config.ts
+++ bogenliga/src/app/modules/verwaltung/components/einstellungen/einstellungen-overview/einstellungen-overview.config.ts
@@ -1,6 +1,5 @@
 import {OverviewDialogConfig} from '@shared/components';
 import {TableActionType} from '@shared/components/tables/types/table-action-type.enum';
-import {TableColumnType} from '@shared/components/tables/types/table-column-type.enum';
 import {UserPermission} from '@shared/services';
 
 export const EINSTELLUNGEN_OVERVIEW_CONFIG: OverviewDialogConfig = {
@@ -8,15 +7,40 @@ export const EINSTELLUNGEN_OVERVIEW_CONFIG: OverviewDialogConfig = {
   pageTitleTranslationKey: 'MANAGEMENT.EINSTELLUNGEN.TITLE',
 
   tableConfig: {
+
     columns: [
+
+      {
+        translationKey:'MANAGEMENT.EINSTELLUNGEN_DETAIL.KEY',
+        propertyName: 'key',
+        width: 20,
+      },
+
+      {
+        translationKey:'MANAGEMENT.EINSTELLUNGEN_DETAIL.VALUE',
+        propertyName: 'value',
+        width: 20,
+      },
+
+
+
     ],
     actions: {
-      actionTypes: [TableActionType.EDIT],
-      width: 6
+      actionTypes: [TableActionType.EDIT,TableActionType.DELETE],
+      width:       6
     },
     editPermission : [UserPermission.CAN_MODIFY_SYSTEMDATEN]
 
   },
   // TODO exchange modify for create
   createPermission : [UserPermission.CAN_CREATE_SYSTEMDATEN]
+
+
+
+
+
+
+
+
+
 };
