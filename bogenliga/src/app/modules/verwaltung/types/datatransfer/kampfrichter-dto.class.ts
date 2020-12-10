cb91c667941f35f8dd81d8a6bc1081d7d93fcc90
Justin-Burg <64771881+Justin-Burg@users.noreply.github.com>
2020-12-10 18:25:27 +0100

    BSAPP-737: Managed to fix the Kampfrichter saving and reading so that it partially works.

--- bogenliga/src/app/modules/verwaltung/types/datatransfer/kampfrichter-dto.class.ts
+++ bogenliga/src/app/modules/verwaltung/types/datatransfer/kampfrichter-dto.class.ts
@@ -8,24 +8,24 @@ export class KampfrichterDTO implements DataTransferObject {
 
   //TODO: Fix the userid to wettkampfID assignment so that name name, order and function are all correct
 
-  // Apparently you can't change the parameter names here without consequences
+  // The attribute names of the optional have to be exactly the same as the getters in KampfrichterDTO.java
   static copyFrom(optional: {
-    id?: number;
-    userid?: number;
+    userID?: number;
+    wettkampfID?: number;
     leitend?: boolean;
     version?: number;
   } = {}): KampfrichterDTO {
     const copy = new KampfrichterDTO();
 
     // show '0' value
-    if (optional.id >= 0) {
-      copy.id = optional.id;
+    if (optional.userID >= 0) {
+      copy.id = optional.userID;
     } else {
       copy.id = null;
     }
 
-    if (optional.userid >= 0) {
-      copy.wettkampfID = optional.userid;
+    if (optional.wettkampfID >= 0) {
+      copy.wettkampfID = optional.wettkampfID;
     } else {
       copy.wettkampfID = null;
     }
