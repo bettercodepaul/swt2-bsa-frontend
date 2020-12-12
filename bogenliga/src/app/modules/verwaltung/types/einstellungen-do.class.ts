5d7e97dda958f49f37cc2cd9e745fca6172c2097
brendli <Timteimer123>
2020-12-12 17:35:42 +0100

    Implementierungen in der einstellungen data provider datei. BSAPP-725

--- bogenliga/src/app/modules/verwaltung/types/einstellungen-do.class.ts
+++ bogenliga/src/app/modules/verwaltung/types/einstellungen-do.class.ts
@@ -9,7 +9,7 @@ export class EinstellungenDO implements VersionedDataObject {
   id: number;
   version: number;
 
-  einstellungenValue: string;
-  einstellungenKey: string;
+  value: string;
+  key: string;
 
 }
