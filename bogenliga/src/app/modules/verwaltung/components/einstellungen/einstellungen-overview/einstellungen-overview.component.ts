941ca1ea1e98a84f2e857f051d2e8e5174ea0805
mki4 <Robert.Reiser@hotmail.de>
2020-12-12 12:04:33 +0100

    BSAPP 723 - Robert Reiser

--- bogenliga/src/app/modules/verwaltung/components/einstellungen/einstellungen-overview/einstellungen-overview.component.ts
+++ bogenliga/src/app/modules/verwaltung/components/einstellungen/einstellungen-overview/einstellungen-overview.component.ts
@@ -3,11 +3,16 @@ import {EinstellungenProviderService} from '@verwaltung/services/einstellungen-d
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {CommonComponentDirective} from '../../../../shared/components/common';
-import {toTableRows} from '../../../../shared/components/tables';
+import {hideLoadingIndicator, showDeleteLoadingIndicatorIcon, toTableRows} from '../../../../shared/components/tables';
 import {TableRow} from '../../../../shared/components/tables/types/table-row.class';
 import {BogenligaResponse} from '../../../../shared/data-provider';
 import {VersionedDataObject} from '../../../../shared/data-provider/models/versioned-data-object.interface';
-import {NotificationService} from '../../../../shared/services/notification';
+import {
+  Notification,
+  NotificationOrigin,
+  NotificationService,
+  NotificationSeverity, NotificationType, NotificationUserAction
+} from '../../../../shared/services/notification';
 import {EINSTELLUNGEN_OVERVIEW_CONFIG} from '@verwaltung/components/einstellungen/einstellungen-overview/einstellungen-overview.config';
 import {BenutzerRolleDO} from '@verwaltung/types/benutzer-rolle-do.class';
 import {BenutzerDO} from '@verwaltung/types/benutzer-do.class';
@@ -16,6 +21,9 @@ import {DsbMitgliedDTO} from '@verwaltung/types/datatransfer/dsb-mitglied-dto.cl
 import {CurrentUserService, UserPermission} from '@shared/services';
 import {EinstellungenDTO} from '@verwaltung/types/datatransfer/einstellungen-dto.class';
 import {EinstellungenDO} from '@verwaltung/types/einstellungen-do.class';
+import {NOTIFICATION_DELETE_DSB_MITGLIED} from '@verwaltung/components';
+
+
 
 @Component({
   selector: 'bla-einstellungen-overview',
@@ -32,7 +40,8 @@ export class EinstellungenOverviewComponent extends CommonComponentDirective imp
   public saveLoading = false
 
   //private einstellungenDataProvider: EinstellungenDataProviderService
-  constructor(private einstellungenDataProvider: EinstellungenProviderService, private router: Router,private currentUserService: CurrentUserService) {
+
+  constructor(private einstellungenDataProvider: EinstellungenProviderService, private router: Router,private currentUserService: CurrentUserService,private notificationService: NotificationService,) {
     super();
   }
 
@@ -49,7 +58,37 @@ export class EinstellungenOverviewComponent extends CommonComponentDirective imp
   }
 
 
+  public onDelete(versionedDataObject: VersionedDataObject): void {
+    // show loading icon
+    const id = versionedDataObject.id;
 
+    this.rows = showDeleteLoadingIndicatorIcon(this.rows, id);
+
+    const notification: Notification = {
+      id: NOTIFICATION_DELETE_DSB_MITGLIED + id,
+      title: 'MANAGEMENT.DSBMITGLIEDER.NOTIFICATION.DELETE.TITLE',
+      description: 'MANAGEMENT.DSBMITGLIEDER.NOTIFICATION.DELETE.DESCRIPTION',
+      descriptionParam: '' + id,
+      severity: NotificationSeverity.QUESTION,
+      origin: NotificationOrigin.USER,
+      type: NotificationType.YES_NO,
+      userAction: NotificationUserAction.PENDING
+    };
+
+    this.notificationService.observeNotification(NOTIFICATION_DELETE_DSB_MITGLIED + id)
+        .subscribe((myNotification) => {
+          if (myNotification.userAction === NotificationUserAction.ACCEPTED) {
+            this.einstellungenDataProvider.deleteById(id)
+                .then((response) => this.loadTableRows())
+                .catch((response) => this.rows = hideLoadingIndicator(this.rows, id));
+          } else if (myNotification.userAction === NotificationUserAction.DECLINED) {
+            this.rows = hideLoadingIndicator(this.rows, id);
+          }
+        });
+
+    this.notificationService.showNotification(notification);
+
+  }
 
 
   private loadTableRows() {
