c9d15ee42ee68870c369f684271fb8c2f73f1f0d
yannikma <may.yan@web.de>
2020-12-15 21:45:51 +0100

    BSAPP-738: deleted variable wettkampfort in wettkampfTable-do.class.ts

--- bogenliga/src/app/modules/home/components/home/wettkampf/wettkampfTable-do.class.ts
+++ bogenliga/src/app/modules/home/components/home/wettkampf/wettkampfTable-do.class.ts
@@ -3,7 +3,7 @@ import {DataObject} from '@shared/data-provider/models/data-object.interface';
 export class WettkampfTableDo implements DataObject {
 
   // gets used in Ligatabelle Table to show the Ligatabelle Information
-  constructor(wettkampfTag: string, wettkampfUhrzeit: string, wettkampfLiga: string, wettkampfOrt: string, wettkampfStrasse: string, wettkampfPlz: string, wettkampfOrtsname: string, wettkampfOrtsinfo: string) {
+  constructor(wettkampfTag: string, wettkampfUhrzeit: string, wettkampfLiga: string, wettkampfStrasse: string, wettkampfPlz: string, wettkampfOrtsname: string, wettkampfOrtsinfo: string) {
     this.wettkampfTag = wettkampfTag;
     this.wettkampfUhrzeit = wettkampfUhrzeit;
     this.wettkampfLiga = wettkampfLiga;
