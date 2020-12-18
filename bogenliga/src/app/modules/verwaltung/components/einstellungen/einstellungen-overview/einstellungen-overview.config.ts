365db092ebf172c1d85ce9f1d54d339613f17d1d
brendli <Timteimer123>
2020-12-18 17:02:55 +0100

    Auskommentierung von Neu und Delete Button

--- bogenliga/src/app/modules/verwaltung/components/einstellungen/einstellungen-overview/einstellungen-overview.config.ts
+++ bogenliga/src/app/modules/verwaltung/components/einstellungen/einstellungen-overview/einstellungen-overview.config.ts
@@ -39,7 +39,6 @@ export const EINSTELLUNGEN_OVERVIEW_CONFIG: OverviewDialogConfig = {
 
   createPermission : [UserPermission.CAN_CREATE_EINSTELLUNGEN  /*, UserPermission.CAN_CREATE_SYSTEMDATEN, UserPermission.CAN_MODIFY_SYSTEMDATEN */ ]
 
-
  // Auskommentierung für eventuell späteren Verwendung von der Delete und Create Funktion
 
 
