5c281b46b6934fa3009d0a0fdfc890c62984655a
brendli <Timteimer123>
2021-01-06 17:20:24 +0100

    Rechtesortierung Neu und Entfernen von Neu und Delete Button

--- bogenliga/src/app/modules/verwaltung/components/einstellungen/einstellungen-overview/einstellungen-overview.config.ts
+++ bogenliga/src/app/modules/verwaltung/components/einstellungen/einstellungen-overview/einstellungen-overview.config.ts
@@ -41,13 +41,4 @@ export const EINSTELLUNGEN_OVERVIEW_CONFIG: OverviewDialogConfig = {
  // Auskommentierung für eventuell späteren Verwendung von der Delete und Create Funktion
 
 
-
-
-
-
-
-
-
-
-
-};
+}
