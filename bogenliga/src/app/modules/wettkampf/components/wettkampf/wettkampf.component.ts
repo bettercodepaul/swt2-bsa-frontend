922ad174a2bf523227f1f5a0e781e878698ce9ee
LarsBahnmueller <Lars_Herbert.Bahnmueller@Student.Reutlingen-University.DE>
2021-01-08 18:46:57 +0100

    BSAPP-769

--- bogenliga/src/app/modules/wettkampf/components/wettkampf/wettkampf.component.ts
+++ bogenliga/src/app/modules/wettkampf/components/wettkampf/wettkampf.component.ts
@@ -56,9 +56,10 @@ export class WettkampfComponent extends CommonComponentDirective implements OnIn
   public wettkaempfe: Array<WettkampfDO> = [];
   private passen: Array<PasseDoClass[]> = [];
 
-
   popup: boolean;
 
+
+  isTableEmpty: Array<boolean> = [].fill(false,0,this.rows.length - 1);
 
 
   constructor(private veranstaltungsDataProvider: VeranstaltungDataProviderService,
@@ -117,6 +118,13 @@ export class WettkampfComponent extends CommonComponentDirective implements OnIn
     for (let i = 0; i < this.wettkaempfe.length; i++) {
       this.rows.push((toTableRows(this.wettkampfErgebnisService.createErgebnisse(this.currentJahr, selectedMannschaft,
         this.mannschaften, this.currentVeranstaltung, this.matches[i], this.passen[i]))));
+    }
+
+    //This loop saves that the table is either empty or not. If table empty -> don't show on frontend
+    for(let i = 0; i < this.rows.length - 1; i++) {
+      if(this.rows[i].length > 0) {
+        this.isTableEmpty[i] = true;
+      }
     }
 
     document.getElementById('druckButton').classList.add('hidden');
