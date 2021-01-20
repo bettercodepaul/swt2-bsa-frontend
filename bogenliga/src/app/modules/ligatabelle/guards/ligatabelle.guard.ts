03145c250ef0d4536146fca1b3aa924f1b346cac
Sebastian Brenner <brenner.sebi@gmail.com>
2021-01-20 15:56:38 +0100

    fix conflicts

--- bogenliga/src/app/modules/ligatabelle/guards/ligatabelle.guard.ts
+++ bogenliga/src/app/modules/ligatabelle/guards/ligatabelle.guard.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {CanActivate} from '@angular/router';
-import {CurrentUserService, UserPermission} from '../../shared/services/current-user';
+import {CurrentUserService} from '../../shared/services/current-user';
 
 @Injectable()
 export class LigatabelleGuard implements CanActivate {
@@ -8,8 +8,7 @@ export class LigatabelleGuard implements CanActivate {
   }
 
   canActivate() {
-    // CAN_READ_DEFAULT required to activate Ligatabelle
-    return this.currentUserService.hasPermission(
-      UserPermission.CAN_READ_DEFAULT);
+    // no permission required to activate Ligatabelle
+    return this.currentUserService.hasAnyPermisson([]);
   }
 }
