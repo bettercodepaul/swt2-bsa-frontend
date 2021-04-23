46fcdb4bca198948a209069ef73ffbdce8056f0f
kernth <Thilo.Kern@web.de>
2021-04-23 16:33:39 +0200

    Renamed and changed Benutzer to User in guards/

--- bogenliga/src/app/modules/verwaltung/guards/user-overview.guard.ts
+++ bogenliga/src/app/modules/verwaltung/guards/user-overview.guard.ts
@@ -3,12 +3,12 @@ import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot} from '@angular
 import {CurrentUserService, UserPermission} from '@shared/services';
 
 @Injectable()
-export class BenutzerOverviewGuard implements CanActivate {
+export class UserOverviewGuard implements CanActivate {
   constructor(private currentUserService: CurrentUserService) {
   }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    // CAN_MODIFY_DSBMITGLIEDER or CAN_MODIFY_VEREIN_DSBMITGLIEDER required to activate Benutzer Overview
+    // CAN_MODIFY_DSBMITGLIEDER or CAN_MODIFY_VEREIN_DSBMITGLIEDER required to activate User Overview
     return this.currentUserService.hasAnyPermisson(
       [UserPermission.CAN_READ_SYSTEMDATEN]);
   }
