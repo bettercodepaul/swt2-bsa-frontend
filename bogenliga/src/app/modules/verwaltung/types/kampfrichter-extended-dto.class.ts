e367a709216e95f24e9c0edf0024489496ed7508
jschaenz <2000jojos@googlemail.com>
2021-07-02 16:01:03 +0200

    add extended Kampfrichter Files

---
+++ bogenliga/src/app/modules/verwaltung/types/kampfrichter-extended-dto.class.ts
@@ -0,0 +1,12 @@
+import {VersionedDataObject} from '@shared/data-provider/models/versioned-data-object.interface';
+
+
+export class KampfrichterExtendedDO implements VersionedDataObject {
+  id: number;
+  wettkampfID: number;
+  leitend: boolean;
+  version: number;
+  vorname: string;
+  nachname: string;
+  email:string;
+}
