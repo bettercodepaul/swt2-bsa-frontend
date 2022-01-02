8fa7497317383f9da9774f4f71ee01120096d9f8
Michael Dirksmöller <michael.dirksmoeller@exxcellent.de>
2022-01-02 12:04:42 +0100

    erste Datentypen für eine Sync Funktion und eine Offline Fähigkeit.

--- bogenliga/src/app/components/offlinedb/services/sync-provider.service.ts
+++ bogenliga/src/app/components/offlinedb/services/sync-provider.service.ts
@@ -10,8 +10,9 @@ import {
 } from '@shared/data-provider';
 import {CurrentUserService} from '@shared/services/current-user';
 import {fromPayload, fromPayloadArray, fromVeranstaltungsPayload} from '@verwaltung/mapper/match-mapper';
-import {LigatabelleErgebnisDO} from '@wettkampf/types/wettkampf-ergebnis-do.class';
+import {Oligatabelle} from '../types/oligatabelle.interface';
 import {fromPayloadLigatabelleErgebnisArray} from '@wettkampf/mapper/wettkampf-ergebnis-mapper';
+import {fromPayloadOligatabelleArray} from '../mapper/oligatabelle-mapper';
 
 
 @Injectable({
@@ -26,14 +27,14 @@ export class SyncDataProviderService extends DataProviderService {
     super();
   }
 
-  public loadLigatabelleVeranstaltung(id: string | number): Promise<BogenligaResponse<LigatabelleErgebnisDO[]>> {
+  public loadLigatabelleVeranstaltung(id: string | number): Promise<BogenligaResponse<Oligatabelle[]>> {
     // return promise
     // sign in success -> resolve promise
     // sign in failure -> reject promise with result
     return new Promise((resolve, reject) => {
       this.restClient.GET<Array<VersionedDataTransferObject>>(new UriBuilder().fromPath(this.getUrl()).path('veranstaltung=' + id).build())
         .then((data: VersionedDataTransferObject[]) => {
-          resolve({result: RequestResult.SUCCESS, payload: fromPayloadLigatabelleErgebnisArray(data)});
+          resolve({result: RequestResult.SUCCESS, payload: fromPayloadOligatabelleArray(data)});
         }, (error: HttpErrorResponse) => {
 
           if (error.status === 0) {
