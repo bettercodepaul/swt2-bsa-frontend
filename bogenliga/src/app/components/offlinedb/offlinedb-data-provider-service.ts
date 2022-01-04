63b7ec1bdbf0b7a7ae0b307fa93cc7ba8c38c3c3
Michael Dirksmöller <michael.dirksmoeller@exxcellent.de>
2022-01-04 15:06:36 +0100

    weitere Lesezugriffe mit Datenfeldern

--- bogenliga/src/app/components/offlinedb/offlinedb-data-provider-service.ts
+++ bogenliga/src/app/components/offlinedb/offlinedb-data-provider-service.ts
@@ -1,10 +1,12 @@
 import {LigatabelleOdaoClass} from './ligatabelle-odao.class';
 import {db} from './offlinedb.component';
+import {MatchOdaoClass} from './match-odao.class';
 
 
 export class OfflinedbDataProviderService {
 
   public currentLigatabelle: LigatabelleOdaoClass[];
+  public matches: MatchOdaoClass[];
 
   constructor() {
     this.currentLigatabelle = [];
@@ -13,7 +15,18 @@ export class OfflinedbDataProviderService {
   public getLigatabelle(): LigatabelleOdaoClass[] {
 
     this.currentLigatabelle = [];
+
     db.ligatabelle.orderBy('tabellenplatz').each((item: LigatabelleOdaoClass ) => this.currentLigatabelle.push(item));
     return this.currentLigatabelle;
   }
+
+  public getAllMatches(): MatchOdaoClass[] {
+    this.matches = [];
+    db.match.orderBy('id').each((item: MatchOdaoClass ) => this.matches.push(item));
+    return this.matches;
+  }
+
+  // TODO: getAllPasseForMatch(matchid: number): PasseOdaoClass[]
+  // TODO: getAllPasse(): PasseOdaoClass[]
+  // TODO: getAllMemeberOfManschaft(manschaftid): ManschaftOdaoClass[]
 }
