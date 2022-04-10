cb75d687c3dca6ff38d1a5ef9ee58c5874edaa49
Artur Kremer <artur.kremer@student.reutlingen-university.de>
2022-04-10 15:16:54 +0200

    Hilfeseite angelegt

---
+++ bogenliga/src/app/modules/hilfe/components/hilfe/hilfe.config.ts
@@ -0,0 +1,9 @@
+import {NavigationDialogConfig} from '@shared/components';
+
+export const HILFE_CONFIG: NavigationDialogConfig = {
+  moduleTranslationKey: 'HILFE',
+  pageTitleTranslationKey: 'HILFE.HILFE.TITLE',
+  navigationCardsConfig: {
+    navigationCards: []
+  }
+};
