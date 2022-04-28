a85cea0163102d30b066340bd3f37ec5f0831ee5
Daniel Lambrecht <daniel.lambrecht@student.reutlingen-university.de>
2022-04-28 16:46:37 +0200

    iframe erstellt

--- bogenliga/src/app/modules/hilfe/components/hilfe/hilfe.component.ts
+++ bogenliga/src/app/modules/hilfe/components/hilfe/hilfe.component.ts
@@ -1,17 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import {HILFE_CONFIG} from './hilfe.config';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'bla-components',
   templateUrl: './hilfe.component.html',
   styleUrls: ['./hilfe.component.scss']
 })
+
 export class HilfeComponent implements OnInit {
 
   public config = HILFE_CONFIG;
-  constructor() { }
+  constructor(private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
+  }
+
+  getWikiUrl() {
+    return this.sanitizer.
+      bypassSecurityTrustResourceUrl("https://wiki.bsapp.de/doku.php")
   }
 
 }
