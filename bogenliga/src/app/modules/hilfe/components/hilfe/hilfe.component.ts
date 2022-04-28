6a3d33e0558b5e19e80c18b15123e94826a8ce86
Daniel Lambrecht <daniel.lambrecht@student.reutlingen-university.de>
2022-04-28 18:16:50 +0200

    sections (buttons) erstellt

--- bogenliga/src/app/modules/hilfe/components/hilfe/hilfe.component.ts
+++ bogenliga/src/app/modules/hilfe/components/hilfe/hilfe.component.ts
@@ -9,16 +9,27 @@ import {DomSanitizer} from '@angular/platform-browser';
 })
 
 export class HilfeComponent implements OnInit {
-
   public config = HILFE_CONFIG;
+  public wikiUrl:any;
+  public sectionSelected:any;
+
+  /* Hier können weitere Sektionen angelegt werden (schema beachten mit id und url)*/
+  public sections:Array<any> = [
+    {id: 'Startseite', url: 'https://wiki.bsapp.de/doku.php'},
+    {id: 'Neue Seiten Anlegen', url: 'https://wiki.bsapp.de/doku.php?id=liga:ligasoftware#neue-eintraege-anlegen'}
+  ]
   constructor(private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
   }
 
-  getWikiUrl() {
-    return this.sanitizer.
-      bypassSecurityTrustResourceUrl("https://wiki.bsapp.de/doku.php")
+  setUrl(url) {
+    this.wikiUrl = url
+    this.sectionSelected = true
   }
 
+  getSecureUrl() {
+    return this.sanitizer.
+      bypassSecurityTrustResourceUrl(this.wikiUrl)
+  }
 }
