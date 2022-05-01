b38135c3f747be40348a3eb7dbb2d2f1d59e7650
Dennis Bär <dennis.baer@student.reutlingen-university.de>
2022-05-01 17:58:28 +0200

    Sonnarlint fixes

--- bogenliga/src/app/modules/shared/data-provider/offlinedb/offlinedb.ts
+++ bogenliga/src/app/modules/shared/data-provider/offlinedb/offlinedb.ts
@@ -11,22 +11,8 @@ import {
 } from '@shared/data-provider/offlinedb/types/offline-mannschaftsmitglied.interface';
 import {
   OfflineDsbMitglied
-} from "@shared/data-provider/offlinedb/types/offline-dsbmitglied.interface";
+} from '@shared/data-provider/offlinedb/types/offline-dsbmitglied.interface';
 
-
-/**
- * export interface OfflineDsbMitglied {
- *   id?: number;
- *   version?: number;
- *   vorname: string;
- *   nachname: string;
- *   geburtsdatum: string;
- *   nationalitaet: string;
- *   mitgliedsnummer: string;
- *   vereinId: number;
- *   benutzerId: number;
- * }
- */
 
 export class OfflineDB extends Dexie {
 
