8c846026ceaedc4fde4eed8676716f6f7b837bad
PaulSch01 <Paul.Schmanns@Student.Reutlingen-University.DE>
2022-06-13 20:47:57 +0200

    Dsbmitglieder werden jetzt aus der alten schnittstelle in die OfflienDb geladen

--- bogenliga/src/app/modules/verwaltung/mapper/dsb-mitglied-offline.mapper.ts
+++ bogenliga/src/app/modules/verwaltung/mapper/dsb-mitglied-offline.mapper.ts
@@ -5,6 +5,7 @@ import {
 import {
   DsbMitgliedOfflineSyncDto
 } from '@verwaltung/types/datatransfer/dsb-mitglied-offline-sync-dto.class';
+import {DsbMitgliedDO} from '@verwaltung/types/dsb-mitglied-do.class';
 
 export function toDO(dsbMitgliedDTO: DsbMitgliedOfflineSyncDto): OfflineDsbMitglied {
   return {
@@ -20,8 +21,24 @@ export function toDO(dsbMitgliedDTO: DsbMitgliedOfflineSyncDto): OfflineDsbMitgl
   };
 }
 
+export function fromDOtoOfflineDsbMitgliederArray(payload: DsbMitgliedDO[]): OfflineDsbMitglied[]{
+  return payload.map(mitglied => fromDOtoOfflineDsbMitglied(mitglied));
+}
 
+export function fromDOtoOfflineDsbMitglied(payload: DsbMitgliedDO): OfflineDsbMitglied{
+  return{
+    benutzerId:      payload.userId,
+    geburtsdatum:    payload.geburtsdatum,
+    id:              payload.id,
+    mitgliedsnummer: payload.mitgliedsnummer,
+    nachname:        payload.nachname,
+    nationalitaet:   payload.nationalitaet,
+    vereinId:        payload.vereinsId,
+    version:         1,
+    vorname:         payload.vorname
 
+  }
+}
 
 export function fromOfflineDsbMitgliedPayload(payload: VersionedDataTransferObject): OfflineDsbMitglied {
   return toDO(DsbMitgliedOfflineSyncDto.copyFrom(payload));
