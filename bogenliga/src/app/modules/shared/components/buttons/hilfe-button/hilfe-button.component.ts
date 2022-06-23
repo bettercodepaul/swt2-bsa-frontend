e8f06d479df2c1d15d429f11243d62b4039d08b6
Daniel Lambrecht <daniel.lambrecht@student.reutlingen-university.de>
2022-06-23 18:30:24 +0200

    hilfe button styling

--- bogenliga/src/app/modules/shared/components/buttons/hilfe-button/hilfe-button.component.ts
+++ bogenliga/src/app/modules/shared/components/buttons/hilfe-button/hilfe-button.component.ts
@@ -1,13 +1,16 @@
 import {OnInit, Input, Component} from '@angular/core';
 
+import {faQuestion} from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector:    'bla-hilfe-button',
-  templateUrl: './hilfe-button.component.html'
+  templateUrl: './hilfe-button.component.html',
 })
 
 export class HilfeButtonComponent implements OnInit {
-
+  faQuestion = faQuestion;
   @Input() public id : string;
+  @Input() public wikiUrl : string;
 
   ngOnInit(): void {
   }
