66cd7211d1944382d771b4297afcf2a2a7e10e39
Manuel Lange <manuel.lange@student.reutlingen-university.de>
2022-11-16 17:09:32 +0100

    change onFocus() method to checkSessionExpired(). add current Time from current Date and expire Time from JSONWebToken and compare each other.

--- bogenliga/src/app/modules/shared/event-handling/session-handling.ts
+++ bogenliga/src/app/modules/shared/event-handling/session-handling.ts
@@ -2,11 +2,28 @@ import {CurrentUserService} from '@shared/services/current-user';
 
 
 export class SessionHandling {
+
+  private isExpired: boolean;
+
   constructor(private currentUserService: CurrentUserService) {
+    this.isExpired = false;
   }
 
-  public onFocus(): void {
-    console.log('test onFOcus sessionHandling');
-    this.currentUserService.isLoggedIn();
+  /* This method gets the current Time in number and gets the Time, when the session is about to expire, by using
+   the jsonWebToken from currentUserService. To get the expire session time from jsonWebToken, the jsonWebToken will
+   be split and parsed only the information about the expire session time in a variable. After that it will check if
+   the current Time is already after the session expire time. If the condition is met, then it will return true and
+   when the condition is not met, it will return a false.
+   */
+  public checkSessionExpired(): boolean {
+    let currentTime = Math.floor((new Date).getTime() / 1000);
+    const jsonWebToken = this.currentUserService.getJsonWebToken();
+    const expireTime = (JSON.parse(atob(jsonWebToken.split('.')[1]))).exp;
+    if (currentTime >= expireTime) {
+      this.isExpired = true;
+    } else {
+      this.isExpired = false;
+    }
+    return this.isExpired;
   }
 }
