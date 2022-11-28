7f10694779cf36207ab34dd32fba6adccfe0ec13
Manuel Lange <manuel.lange@student.reutlingen-university.de>
2022-11-28 20:23:13 +0100

    add new variable expiredOnceTime to handle endless reloading window. also add isOffline to handle auto logout in Offline-Modus.

--- bogenliga/src/app/modules/shared/event-handling/session-handling.ts
+++ bogenliga/src/app/modules/shared/event-handling/session-handling.ts
@@ -1,12 +1,16 @@
 import {CurrentUserService} from '@shared/services/current-user';
+import {OnOfflineService} from '@shared/services';
 
 
 export class SessionHandling {
 
   private isExpired: boolean;
+  private expiredOnceTime: number;
 
-  constructor(private currentUserService: CurrentUserService) {
+  constructor(private currentUserService: CurrentUserService,
+    private onOfflineService: OnOfflineService) {
     this.isExpired = false;
+    this.expiredOnceTime = Math.floor((new Date).getTime() / 1000);
   }
 
   /* This method gets the current Time in number and gets the Time, when the session is about to expire, by using
@@ -19,8 +23,9 @@ export class SessionHandling {
     let currentTime = Math.floor((new Date).getTime() / 1000);
     const jsonWebToken = this.currentUserService.getJsonWebToken();
     const expireTime = (JSON.parse(atob(jsonWebToken.split('.')[1]))).exp;
-    if (currentTime >= expireTime) {
+    if (currentTime >= expireTime && this.expiredOnceTime < expireTime && !this.onOfflineService.isOffline()) {
       this.isExpired = true;
+      this.expiredOnceTime = currentTime;
     } else {
       this.isExpired = false;
     }
