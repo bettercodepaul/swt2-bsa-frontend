e373242faaff0c5f77b321f48691e3a6ef8fbcee
Manuel Lange <manuel.lange@student.reutlingen-university.de>
2022-11-29 22:06:03 +0100

    edit comments for latest changes

--- bogenliga/src/app/modules/shared/event-handling/session-handling.ts
+++ bogenliga/src/app/modules/shared/event-handling/session-handling.ts
@@ -13,12 +13,15 @@ export class SessionHandling {
     this.expiredOnceTime = Math.floor((new Date).getTime() / 1000);
   }
 
-  /* This method gets the current Time in number and gets the Time, when the session is about to expire, by using
-   the jsonWebToken from currentUserService. To get the expire session time from jsonWebToken, the jsonWebToken will
-   be split and parsed only the information about the expire session time in a variable. After that it will check if
-   the current Time is already after the session expire time. If the condition is met, then it will return true and
-   when the condition is not met, it will return a false.
-   */
+  /** This method gets the current Time in number and gets the Time, when the session is about to expire, by using
+   *  the jsonWebToken from currentUserService. To get the expire session time from jsonWebToken, the jsonWebToken will
+   *  be split and parsed only the information about the expire session time in a variable. After that it will check if
+   *  the current Time is already after the session expire time. It also checks if expiredOnceTime is smaller than
+   *  expireTime and if the system is in online-modus.
+   *  If one of three condition is not met, then it will return a false.
+   *  If all three conditions are met, then it will return true. It will also write currentTime in expiredOnceTime,
+   *  so that the pages can only reload and logout once in only one session-expired case (to handle endless reloads).
+   **/
   public checkSessionExpired(): boolean {
     let currentTime = Math.floor((new Date).getTime() / 1000);
     const jsonWebToken = this.currentUserService.getJsonWebToken();
