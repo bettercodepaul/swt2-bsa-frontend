44cd5cfe3b3c10830fc9323f9950010bfd3793e1
alexhobel <hobelsbergeralex@gmail.com>
2023-04-21 14:49:26 +0200

    Code was checked on ticket request and it was determined that the error message is already good in terms of architecture, statement and code quality. A comment was written in the appropriate place.

--- bogenliga/src/app/modules/shared/components/buttons/shortcut-button/shortcut-button.component.ts
+++ bogenliga/src/app/modules/shared/components/buttons/shortcut-button/shortcut-button.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {NavigationCardsConfig} from '@shared/components/navigation-cards';
+import {CurrentUserService, UserPermission} from '@shared/services';
 import {
   ShortcutButtonsConfig
 } from '@shared/components/buttons/shortcut-button/types/shortcut-buttons-config.interface';
@@ -13,11 +14,20 @@ export class ShortcutButton implements OnInit {
 
   @Input()
   public id: string;
-  constructor() { }
+  constructor(private currentUserService: CurrentUserService) {
+  }
 
   @Input() public config: ShortcutButtonsConfig = {shortcutButtons: []};
 
   ngOnInit(): void {
+  }
+
+  public hasUserPermissions(userPermissions: UserPermission[]): boolean {
+    if (userPermissions === undefined) {
+      return true;
+    } else {
+      return this.currentUserService.hasAnyPermisson(userPermissions);
+    }
   }
 
 }
