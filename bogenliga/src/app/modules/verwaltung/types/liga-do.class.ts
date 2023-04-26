6f6fe5dc60053a65c51618b8c2f913ad0d3f4f09
Niklas Bretz <niklasbretz@yahoo.com>
2023-04-26 12:38:42 +0200

    Removed unnecessary ligaDetailId Attributes from all Liga Objects

--- bogenliga/src/app/modules/verwaltung/types/liga-do.class.ts
+++ bogenliga/src/app/modules/verwaltung/types/liga-do.class.ts
@@ -12,7 +12,6 @@ export class LigaDO implements VersionedDataObject {
   ligaUebergeordnetName: string;
   ligaVerantwortlichId: number;
   ligaVerantwortlichMail: string;
-  ligaDetailId: number;
   ligaDetail: string;
 
 }
