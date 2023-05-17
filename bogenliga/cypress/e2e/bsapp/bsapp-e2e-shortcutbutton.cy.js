5f54cb4ff5ede42e7f928d244fd6dd0de3097fc4
alexhobel <hobelsbergeralex@gmail.com>
2023-05-17 12:24:41 +0200

    - Ligaleiter Test - Überprüfung auf Pfade - Jeder Shortcut-Button der Rolle wird einmal geclickt

--- bogenliga/cypress/e2e/bsapp/bsapp-e2e-shortcutbutton.cy.js
+++ bogenliga/cypress/e2e/bsapp/bsapp-e2e-shortcutbutton.cy.js
@@ -5,16 +5,30 @@ describe('Shortcuts appear on login', function() {
 
     cy.contains('button.btn.btn-primary.btn-sm', 'Login für Team Ligaleiter').click();
 
+    // Überprüfen, ob wir uns auf der Landingpage befinden
+    cy.url().should('include', '/home');
+
     // Klicken Sie auf den Button zum Anlegen einer Veranstaltung
     cy.get('div.Button button.shortcut-btn[routerlinkactive="active"][ng-reflect-router-link="/verwaltung/veranstaltung/add"]').click();
 
     // Überprüfen, ob der Klick erfolgreich war und zur vorherigen Seite zurückkehren
     cy.go('back');
 
+    // Überprüfen, ob wir zurück auf der ursprünglichen Seite sind
+    cy.url().should('include', '/home');
+
     // Klicken Sie auf den Button für die Terminübersicht
     cy.get('div.Button button.shortcut-btn[routerlinkactive="active"][ng-reflect-router-link="/wkdurchfuehrung"]').click();
 
     // Überprüfen, ob der Klick erfolgreich war und zur vorherigen Seite zurückkehren
     cy.go('back');
+
+    // Klicken Sie auf den Button für die Vereinsübersicht
+    cy.get('div.Button button.shortcut-btn[routerlinkactive="active"][ng-reflect-router-link="/verwaltung/vereine"]').click();
+
+    // Überprüfen, ob der Klick erfolgreich war
+    cy.url().should('include', '/vereine');
   });
 });
+
+
