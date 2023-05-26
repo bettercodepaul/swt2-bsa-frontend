4a0d7b0d36d051fbdad4739f7b0bc8572d7fa072
godfred amoah sefa <godfredasefa@icloud.com>
2023-05-26 11:21:33 +0200

    - remove comments
    Ticket BSAPP_1439

--- bogenliga/cypress/e2e/bsapp/bsapp-e2e-shortcutbutton.cy.js
+++ bogenliga/cypress/e2e/bsapp/bsapp-e2e-shortcutbutton.cy.js
@@ -8,46 +8,34 @@ describe('Shortcut buttons appear on login with different roles', () => {
 
     cy.contains('button.btn.btn-primary.btn-sm', 'Login für Team Ligaleiter').click();
 
-    // Überprüfen, ob wir uns auf der Landingpage befinden
     cy.url().should('include', '/home');
 
-    // Klicken Sie auf den Button zum Anlegen einer Veranstaltung
     cy.get('button.shortcut-btn[routerlinkactive="active"][ng-reflect-router-link="/verwaltung/veranstaltung/add"]').click();
 
-    // Überprüfen, ob der Klick erfolgreich war und zur vorherigen Seite zurückkehren
     cy.url().should('include', '/verwaltung/veranstaltung/add');
     cy.go('back');
 
-    // Überprüfen, ob wir zurück auf der ursprünglichen Seite sind
     cy.url().should('include', '/home');
 
-    // Klicken Sie auf den Button für die Terminübersicht
     cy.get('button.shortcut-btn[routerlinkactive="active"][ng-reflect-router-link="/wkdurchfuehrung"]').click();
 
-    // Überprüfen, ob der Klick erfolgreich war und zur vorherigen Seite zurückkehren
     cy.url().should('include', '/wkdurchfuehrung');
     cy.go('back');
 
-    // Überprüfen, ob wir zurück auf der ursprünglichen Seite sind
     cy.url().should('include', '/home');
 
-    // Klicken Sie auf den Button für die Vereinsübersicht
     cy.get('button.shortcut-btn[routerlinkactive="active"][ng-reflect-router-link="/verwaltung/vereine"]').click();
 
-    // Überprüfen, ob der Klick erfolgreich war und zur vorherigen Seite zurückkehren
     cy.url().should('include', '/verwaltung/vereine');
     cy.go('back');
 
-    // Überprüfen, ob wir zurück auf der ursprünglichen Seite sind
     cy.url().should('include', '/home');
 
     cy.logout();
-
-
   });
 
 
-  it('Shortcutbuttons appear on Login as Sportleiter/Mannschaftsführer', () => {
+  it.only('Shortcutbuttons appear on Login as Sportleiter/Mannschaftsführer', () => {
     cy.visit('http://localhost:4200/');
 
     cy.dismissModal();
@@ -80,24 +68,21 @@ describe('Shortcut buttons appear on login with different roles', () => {
 
     cy.url().should('include', '/home');
 
-
     cy.get('button.shortcut-btn[routerlinkactive="active"][ng-reflect-router-link="/verwaltung/veranstaltung"]').click();
-
 
     cy.url().should('include', '/verwaltung/veranstaltung');
     cy.go('back');
-
 
     cy.url().should('include', '/home');
 
     cy.get('button.shortcut-btn[routerlinkactive="active"][ng-reflect-router-link="/wkdurchfuehrung"]').click();
 
-
     cy.url().should('include', '/wkdurchfuehrung');
     cy.go('back');
 
-
     cy.url().should('include', '/home');
+
+    cy.logout();
 
   })
 
@@ -116,10 +101,11 @@ describe('Shortcut buttons appear on login with different roles', () => {
 
     cy.url().should('include', '/wkdurchfuehrung');
 
-    // Weitere Aktionen für den Kampfrichter-Test hier...
-
     cy.go('back');
+
     cy.url().should('include', '/home');
+
+    cy.logout()
   });
 
 });
