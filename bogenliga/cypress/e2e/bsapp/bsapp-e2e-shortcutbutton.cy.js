dbd0a1f65cba8dfd4ea76294a4b9f2eeaf7f879d
godfred amoah sefa <godfredasefa@icloud.com>
2023-06-19 21:55:06 +0200

    - refactore cypress test it uses now the login mask - add script to insert the last test user to it Role Kampfrichter

--- bogenliga/cypress/e2e/bsapp/bsapp-e2e-shortcutbutton.cy.js
+++ bogenliga/cypress/e2e/bsapp/bsapp-e2e-shortcutbutton.cy.js
@@ -5,23 +5,18 @@ describe('Shortcut buttons appear on login with different roles', () => {
 
     cy.visit('http://localhost:4200/#/user/login')
 
-    /*
     cy.get('input#loginEmail').type('HSRT-Test@bogenliga.de')
 
     cy.get('input#loginPassword').type('mki4HSRT')
 
     cy.get('button#loginButton').click()
 
-    cy.pause()
-
-     */
-    cy.contains('button.btn.btn-primary.btn-sm', 'Login für Team Ligaleiter').click();
-
     cy.url().should('include', '/home');
 
     cy.get('[data-cy="shortcut-btn-Veranstaltung-anlegen"]').click();
 
     cy.url().should('include', '/verwaltung/veranstaltung/add');
+
     cy.go('back');
 
     cy.url().should('include', '/home');
@@ -47,20 +42,14 @@ describe('Shortcut buttons appear on login with different roles', () => {
   it('Shortcutbuttons appear on Login as Sportleiter/Mannschaftsführer', () => {
     cy.visit('http://localhost:4200/#/user/login');
 
-    /*
-cy.get('input#loginEmail').type('HSRT-Test3@bogenliga.de')
+    cy.get('input#loginEmail').type('HSRT-Test3@bogenliga.de')
 
-cy.get('input#loginPassword').type('mki4HSRT')
+    cy.get('input#loginPassword').type('mki4HSRT')
 
-cy.get('button#loginButton').click()
-
-cy.pause()
-
- */
+    cy.get('button#loginButton').click()
 
     cy.get('[data-cy="login-button"]').click()
 
-    cy.contains('button.btn.btn-primary.btn-sm', 'Login für Team Sportleiter').click();
 
     cy.get(':nth-child(1) > .Button > .shortcut-btn').click();
 
@@ -76,28 +65,15 @@ cy.pause()
   });
 
   it('Shortcutbuttons appear on Login as Kampfrichter',  () => {
+    cy.visit('http://localhost:4200/#/user/login');
 
-    cy.loginAdmin();
 
-    cy.createUserTest();
-
-    cy.visit('http://localhost:4200/#/verwaltung/user');
-
-    cy.assignRoleToTestUser("KAMPFRICHTER");
-
-    cy.logout();
-
-    cy.loginUserTest();
-    /*
 cy.get('input#loginEmail').type('HSRT-Test4@bogenliga.de')
 
 cy.get('input#loginPassword').type('mki4HSRT')
 
 cy.get('button#loginButton').click()
 
-cy.pause()
-
- */
 
     cy.url().should('include', '/home');
 
@@ -109,40 +85,21 @@ cy.pause()
 
     cy.logout();
 
-    cy.visit('http://localhost:4200/#/user/login');
-
-    cy.deleteTestUser();
 
   })
 
   it('Shortcutbuttons appear on Login as Ausrichter',  () => {
 
-    cy.loginAdmin();
 
-    cy.createUserTest();
+    cy.visit('http://localhost:4200/#/user/login');
 
-    cy.visit('http://localhost:4200/#/verwaltung/user');
-
-    cy.assignRoleToTestUser("AUSRICHTER");
-
-    cy.logout();
-
-    cy.loginUserTest();
-
-    cy.url().should('include', '/home');77
-
-    /*
     cy.get('input#loginEmail').type('HSRT-Test@bogenliga.de')
 
     cy.get('input#loginPassword').type('mki4HSRT')
 
     cy.get('button#loginButton').click()
 
-    cy.pause()
-
-     */
-
-    cy.get('[data-cy="shortcut-btn-Vorauswahl-Veranstaltungen"]').click();
+    cy.get('[data-cy="shortcut-btn-Veranstaltung-anlegen"]').click();
 
     cy.url().should('include', '/verwaltung/veranstaltung');
 
@@ -159,10 +116,6 @@ cy.pause()
     cy.url().should('include', '/home');
 
     cy.logout();
-
-    cy.visit('http://localhost:4200/#/user/login');
-
-    cy.deleteTestUser();
   });
 
 });
