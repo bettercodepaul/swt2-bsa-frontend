dde0869087bb1462db4f7c018b0bce92732ece14
SteffenAlber <steffen.alber@student.reutlingen-university.de>
2023-07-03 10:57:19 +0200

    Added cypress tests and minor changes in home.html

--- bogenliga/cypress.config.ts
+++ bogenliga/cypress.config.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
@@ -11,14 +10,4 @@ export default defineConfig({
     testIsolation: false
   },
 })
-=======
-import { defineConfig } from "cypress";
 
-export default defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-});
->>>>>>> BSAPP-1433-Sprint3
