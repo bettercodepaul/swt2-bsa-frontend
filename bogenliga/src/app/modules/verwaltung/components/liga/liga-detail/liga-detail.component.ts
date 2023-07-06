341ef6ff27cf4a3fc45f4e2f905bc0cb35716792
Sarikaya_M <sarikaya.mehmet@outlook.de>
2023-07-06 12:33:02 +0200

    Added functionality for Ligaleiter to edit Ligadetails

--- bogenliga/src/app/modules/verwaltung/components/liga/liga-detail/liga-detail.component.ts
+++ bogenliga/src/app/modules/verwaltung/components/liga/liga-detail/liga-detail.component.ts
@@ -22,12 +22,14 @@ import {LigaDO} from '../../../types/liga-do.class';
 import {RegionDO} from '../../../types/region-do.class';
 import {LIGA_DETAIL_CONFIG} from './liga-detail.config';
 import {SessionHandling} from '@shared/event-handling';
-import {CurrentUserService, OnOfflineService} from '@shared/services';
+import {CurrentUserService, OnOfflineService, UserPermission} from '@shared/services';
 import {DisziplinDO} from '@verwaltung/types/disziplin-do.class';
 import {DisziplinDTO} from '@verwaltung/types/datatransfer/disziplin-dto.class';
 import {DisziplinDataProviderService} from '@verwaltung/services/disziplin-data-provider-service';
 import {ActionButtonColors} from '@shared/components/buttons/button/actionbuttoncolors';
 import {HttpClient} from '@angular/common/http';
+import {UserRolleDO} from '@verwaltung/types/user-rolle-do.class';
+import {UserDataProviderService} from '@verwaltung/services/user-data-provider.service';
 
 const ID_PATH_PARAM = 'id';
 const NOTIFICATION_DELETE_LIGA = 'liga_detail_delete';
@@ -60,6 +62,7 @@ export class LigaDetailComponent extends CommonComponentDirective implements OnI
   public currentUser: UserProfileDO = new UserProfileDO();
   public allUsers: Array<UserProfileDO> = [new UserProfileDO()];
 
+  public isAdmin: Boolean = false;
 
 
   public deleteLoading = false;
@@ -69,9 +72,17 @@ export class LigaDetailComponent extends CommonComponentDirective implements OnI
   private sessionHandling: SessionHandling;
 
 
+  //FileUpload
+
+  public fileName = '';
+
+
+
+
   constructor(private ligaDataProvider: LigaDataProviderService,
               private regionProvider: RegionDataProviderService,
               private userProvider: UserProfileDataProviderService,
+              private userDataProviderService: UserDataProviderService,
               private disziplinDataProvider: DisziplinDataProviderService,
               private router: Router, private route: ActivatedRoute,
               private notificationService: NotificationService,
@@ -85,6 +96,10 @@ export class LigaDetailComponent extends CommonComponentDirective implements OnI
   }
 
   ngOnInit() {
+
+    this.userDataProviderService.findUserRoleById(this.currentUserService.getCurrentUserID()).then((roleresponse: BogenligaResponse<UserRolleDO[]>) => {
+      this.isAdmin = roleresponse.payload.filter(role => role.roleName == 'ADMIN').length > 0
+    })
     this.loading = true;
     this.notificationService.discardNotification();
     this.route.params.subscribe((params) => {
@@ -200,9 +215,7 @@ export class LigaDetailComponent extends CommonComponentDirective implements OnI
   }
 
   public onUpdate(ignore: any): void {
-    if (this.currentLiga.ligaDetail.length > 3000) {
-      //TODO: Tatsächlichen String holen
-      //alert('MANAGEMENT.LIGA_DETAIL.ALERT.MAX_WORDS_SURPASSED')
+    if (this.currentLiga.ligaDetail.length > 5000) {
       this.notificationService.showNotification({
         id: 'MaxWordsWarning',
         description: 'MANAGEMENT.LIGA_DETAIL.NOTIFICATION.MAX_WORDS_SURPASSED.DESCRIPTION',
@@ -218,7 +231,6 @@ export class LigaDetailComponent extends CommonComponentDirective implements OnI
     this.saveLoading = true;
     this.currentLiga.regionId = this.currentRegion.id;
     this.currentLiga.disziplinId = this.currentDisziplin.id;
-    console.log(this.currentDisziplin.id);
     this.currentLiga.ligaUebergeordnetId = this.currentUbergeordneteLiga.id;
     this.currentLiga.ligaVerantwortlichId = this.currentUser.id;
     // persist
@@ -275,7 +287,7 @@ export class LigaDetailComponent extends CommonComponentDirective implements OnI
     }
   }
 
-  
+
 
 
   // File Upload button, converts selected files to Base64 String
