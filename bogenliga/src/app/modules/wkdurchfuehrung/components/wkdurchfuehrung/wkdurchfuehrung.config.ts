ab8d7b95c154b21ecdf552e31aff502609584b35
INFDESK112\mki4 <meier_magdalena@t-online.de>
2023-10-11 14:53:07 +0200

    #1483 Map-Icon entfernt

--- bogenliga/src/app/modules/wkdurchfuehrung/components/wkdurchfuehrung/wkdurchfuehrung.config.ts
+++ bogenliga/src/app/modules/wkdurchfuehrung/components/wkdurchfuehrung/wkdurchfuehrung.config.ts
@@ -39,7 +39,8 @@ export const WETTKAMPF_TABLE_CONFIG: TableConfig = {
    ],
 
   actions: {
-    actionTypes: [TableActionType.VIEW, TableActionType.MAP ],
+    // TableActionType.MAP removed, maybe added again later on
+    actionTypes: [TableActionType.VIEW],
     width:       6
   }
   };
