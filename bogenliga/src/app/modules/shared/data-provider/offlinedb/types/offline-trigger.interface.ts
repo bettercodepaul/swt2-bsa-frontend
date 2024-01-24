0f843c99f0dd0f475b9a849e62246cea36f3ca1f
Magdalena Meier <meier_magdalena@t-online.de>
2024-01-24 20:39:11 +0100

    Underscores in Attributen entfernt

--- bogenliga/src/app/modules/shared/data-provider/offlinedb/types/offline-trigger.interface.ts
+++ bogenliga/src/app/modules/shared/data-provider/offlinedb/types/offline-trigger.interface.ts
@@ -1,10 +1,10 @@
 export interface OfflineTrigger {
   id?: number;
   kategorie?: string;
-  altsystem_id?: number;
+  altsystemId?: number;
   operation?: string;
   status?: string;
   nachricht?: string;
-  created_at_utc?: string
-  run_at_utc?: string;
+  createdAtUtc?: string
+  runAtUtc?: string;
 }
