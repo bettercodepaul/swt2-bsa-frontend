d0f8de208fe20dd8f98cb84f83b4bdabd0810ed6
Magdalena Meier <meier_magdalena@t-online.de>
2024-01-24 22:37:49 +0100

    Ausgabe als Tabelle

--- bogenliga/src/app/modules/verwaltung/components/migration/migration.config.ts
+++ bogenliga/src/app/modules/verwaltung/components/migration/migration.config.ts
@@ -10,30 +10,37 @@ export const MIGRATION_OVERVIEW_CONFIG: OverviewDialogConfig = {
     columns: [
       {
         translationKey: 'MANAGEMENT.MIGRATION.TABLE.HEADERS.OLD',
-        propertyName:   'migrationTimestamp',
-        width:          15,
+        propertyName:   'altsystemId',
+        width:          10,
       },
       {
         translationKey: 'MANAGEMENT.MIGRATION.TABLE.HEADERS.TABLENAME',
-        propertyName:   'migrationDescription',
-        width:          15,
+        propertyName:   'kategorie',
+        width:          20,
       },
       {
-        translationKey: 'MANAGEMENT.MIGRATION.TABLE.HEADERS.CATEGORY',
-        propertyName:   'migrationStatus',
-        width:          15,
+        translationKey: 'MANAGEMENT.MIGRATION.TABLE.HEADERS.OPERATION',
+        propertyName:   'operation',
+        width:          20,
       },
       {
         translationKey: 'MANAGEMENT.MIGRATION.TABLE.HEADERS.STATUS',
-        propertyName:   'migrationStatus',
-        width:          15,
+        propertyName:   'status',
+        width:          10,
+      },
+      {
+        translationKey: 'MANAGEMENT.MIGRATION.TABLE.HEADERS.MESSAGE',
+        propertyName:   'nachricht',
+        width:          20,
+      },
+      {
+        translationKey: 'MANAGEMENT.MIGRATION.TABLE.HEADERS.TIMESTAMP',
+        propertyName:   'runAtUtc',
+        width:          20,
       },
 
     ],
-    actions: {
-      actionTypes: [TableActionType.EDIT, TableActionType.DELETE],
-      width:       6
-    },
+
     editPermission : [UserPermission.CAN_MODIFY_SYSTEMDATEN],
     deletePermission : [UserPermission.CAN_DELETE_SYSTEMDATEN]
   },
