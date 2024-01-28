d413ca0ed43a621f616762a1fb442f5f6920ca4a
Nikolay Nikolaev <nikolay.borislavov@gmail.com>
2024-01-28 11:37:25 +0100

    Add DisbandModal Command and Finish Tests

--- bogenliga/cypress/e2e/bsapp/bsapp-e2e-migration.cy.js
+++ bogenliga/cypress/e2e/bsapp/bsapp-e2e-migration.cy.js
@@ -1,13 +1,22 @@
 describe('Verwaltung/Migration tests', function () {
   beforeEach(() => {
+    //Log in as Admin before each test
     cy.loginAdmin()
+    cy.wait(1000)
+  })
+
+  afterEach(() => {
+    //NOTE: Preparation for next test
+
+    cy.wait(1000)
+    cy.disbandModalIfShown()
     cy.wait(1000)
   })
 
   /**
    * This test opens the sidebar and clicks on the "VERWALTUNG" tab and checks if the url has changed successfully
    */
-  it('Anzeige Verwaltung',  () => {
+  it('Anzeige Verwaltung', () => {
     cy.url().should('include', '#/home')
     cy.get('[data-cy=sidebar-verwaltung-button]').click()
     cy.wait(1000)
@@ -15,9 +24,9 @@ describe('Verwaltung/Migration tests', function () {
   })
 
   /**
-   * This test opens the sidebar and clicks on the "VERWALTUNG" tab and checks if the Sync Section Button is present
+   * This test opens the sidebar and clicks on the "VERWALTUNG" tab and checks if the Migration Section (and Button) is present
    */
-  it('Anzeige Migration Overview',  () => {
+  it('Anzeige Migration Overview', () => {
     cy.url().should('include', '#/home')
     cy.get('[data-cy=sidebar-verwaltung-button]').click()
     cy.wait(1000)
@@ -27,23 +36,40 @@ describe('Verwaltung/Migration tests', function () {
   })
 
   /**
-   * This test opens the sidebar and clicks on the "VERWALTUNG" tab and checks if the Sync Section Button is present, clicks it and checks if route has changed
+   * This test opens the sidebar and clicks on the "VERWALTUNG" tab and:
+   * 1. check if the Migration Section is present
+   * 2. clicks on it
+   * 3. checks if route has changed
+   * 4. if a call was made to Backend
    */
-  it('Anzeige Migration Section',  () => {
+  it('Anzeige Migration Section and FindAll Call', () => {
     cy.url().should('include', '#/home')
+    cy.intercept({
+      method: 'GET',
+      url: `http://localhost:9000/v1/trigger`,
+    }).as('findall-request');
     cy.get('[data-cy=sidebar-verwaltung-button]').click()
     cy.wait(1000)
     cy.url().should('include', '#/verwaltung')
     cy.get('[data-cy=verwaltung-sync-button]').click()
     cy.wait(1000)
     cy.url().should('include', '#/verwaltung/migration')
+
+    cy.wait(1000)
+    cy.wait('@findall-request')
   })
 
   /**
-   * This test opens the sidebar and clicks on the "VERWALTUNG" tab, clicks the Sync Section Button, checks if route has changed, clicks the "New" Button and checks if it has made a request to Endpoint
+   * This test opens the sidebar and clicks on the "VERWALTUNG" tab and:
+   * 1. check if the Migration Section is present
+   * 2. clicks on it
+   * 3. checks if route has changed
+   * 4. disband the Error Modal
+   * 5. clicks on the "Neue Migration" Button
+   * 6. checks if a call was made to Backend
    */
 
-  it('Anzeige Migration Button Click',  () => {
+  it('Anzeige Migration Button Click', () => {
     cy.url().should('include', '#/home')
     cy.get('[data-cy=sidebar-verwaltung-button]').click()
     cy.wait(1000)
@@ -51,7 +77,12 @@ describe('Verwaltung/Migration tests', function () {
     cy.get('[data-cy=verwaltung-sync-button]').click()
     cy.wait(1000)
     cy.url().should('include', '#/verwaltung/migration')
+    cy.wait(1000)
+    cy.disbandModalIfShown();
+    cy.wait(1000)
     cy.get('[data-cy=dsb-mitglied-add-button]').click()
+
+    //Intercept Call to Backend
     cy.intercept({
       method: 'GET',
       url: `http://localhost:9000/v1/trigger/buttonSync`,
