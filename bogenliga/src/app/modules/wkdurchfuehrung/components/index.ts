8b11311b802fdfb5662e02e05ca4bcea18fd510d
SCel31 <celine.schmitt@student.reutlingen-university.de>
2024-04-18 11:20:14 +0200

    Route for /fullscreen

--- bogenliga/src/app/modules/wkdurchfuehrung/components/index.ts
+++ bogenliga/src/app/modules/wkdurchfuehrung/components/index.ts
@@ -4,3 +4,4 @@ export * from './schusszettel/number.directive';
 export {RingzahlTabIndexDirective, SchuetzenTabIndexDirective} from './schusszettel/tabindex.directive';
 export * from './tablet-admin/tablet-admin.component';
 export * from './tableteingabe/tableteingabe.component';
+export * from './fullscreen/fullscreen.component'
