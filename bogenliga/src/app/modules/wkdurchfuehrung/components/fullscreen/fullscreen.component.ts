89c145d8054e1d4618c52b61f68ec281ab85511e
SCel31 <celine.schmitt@student.reutlingen-university.de>
2024-04-29 18:28:32 +0200

    Ticket : #1607 Add Ligatabelle to Fullscreen and show data from year 2018

--- bogenliga/src/app/modules/wkdurchfuehrung/components/fullscreen/fullscreen.component.ts
+++ bogenliga/src/app/modules/wkdurchfuehrung/components/fullscreen/fullscreen.component.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { SessionHandling } from '@shared/event-handling';
-import { CurrentUserService, OnOfflineService } from '@shared/services';
-import { CommonComponentDirective} from '@shared/components';
+import {CurrentUserService, NotificationService, OnOfflineService} from '@shared/services';
+import {CommonComponentDirective, toTableRows} from '@shared/components';
 import { ActionButtonColors } from '@shared/components/buttons/button/actionbuttoncolors';
 import { LIGATABELLE_TABLE_CONFIG, WETTKAEMPFE_CONFIG } from '../../../ligatabelle/components/ligatabelle/ligatabelle.config';
 import { interval, Subscription } from 'rxjs';
+import {IconProp} from '@fortawesome/fontawesome-svg-core';
+import {faSitemap, faUndo} from '@fortawesome/free-solid-svg-icons';
+import {TableRow} from '@shared/components/tables/types/table-row.class';
+import {VeranstaltungDO} from '@verwaltung/types/veranstaltung-do.class';
+import {ActivatedRoute, Router} from '@angular/router';
+import {VeranstaltungDataProviderService} from '@verwaltung/services/veranstaltung-data-provider.service';
+import {LigatabelleDataProviderService} from '../../../ligatabelle/services/ligatabelle-data-provider.service';
+import {isUndefined} from '@shared/functions';
+import {BogenligaResponse} from '@shared/data-provider';
+import {LigatabelleErgebnisDO} from '../../../ligatabelle/types/ligatabelle-ergebnis-do.class';
 
-interface LigatabelleEntry {
-  platz: number;
-  mannschaft: string;
-  matchpunkte: string;
-  satzpunkte: string;
-  satzpunktdifferenz: number;
-}
+const ID_PATH_PARAM = 'id';
 @Component({
   selector: 'bla-fullscreen',
   templateUrl: './fullscreen.component.html',
@@ -21,28 +25,44 @@ interface LigatabelleEntry {
 export class FullscreenComponent extends CommonComponentDirective implements OnInit {
 
   private sessionHandling: SessionHandling;
+  public zuDenLigadetailsIcon: IconProp = faSitemap;
+  public zuruecksetzenIcon: IconProp = faUndo;
   public config = WETTKAEMPFE_CONFIG;
   public config_table = LIGATABELLE_TABLE_CONFIG;
   public ActionButtonColors = ActionButtonColors;
   public loading = true;
   public multipleSelections = true;
+  public PLACEHOLDER_VAR = 'Zur Suche Liga-Bezeichnung eingeben...';
+  public buttonForward: number;
+  public selectedVeranstaltungName: string;
 
   currentTime: string;
   private timeSubscription: Subscription;
 
-  //work in process -> Daten für die Visualisierung des Mockups
-  public ligatabelleData: LigatabelleEntry[] = [
-    { platz: 1, mannschaft: 'BSC Geislingen Steige-2', matchpunkte: '11 : 3', satzpunkte: '39 : 21', satzpunktdifferenz: 18 },
-    { platz: 2, mannschaft: 'SK Fellbach Schmiden-0', matchpunkte: '11 : 3', satzpunkte: '37 : 21', satzpunktdifferenz: 16 },
-    { platz: 3, mannschaft: 'SV Mögglingen-0', matchpunkte: '8 : 6', satzpunkte: '32 : 24', satzpunktdifferenz: 8 },
-    { platz: 4, mannschaft: 'SGi Welzheim-3', matchpunkte: '8 : 6', satzpunkte: '32 : 28', satzpunktdifferenz: 4 },
-    { platz: 5, mannschaft: 'SSV Steinheim Albuch-0', matchpunkte: '7 : 7', satzpunkte: '32 : 26', satzpunktdifferenz: 6 },
-    { platz: 6, mannschaft: 'BSV Brackenheim-0', matchpunkte: '7 : 7', satzpunkte: '27 : 29', satzpunktdifferenz: -2 },
-    { platz: 7, mannschaft: 'SGi Ditzingen-3', matchpunkte: '4 : 10', satzpunkte: '23 : 31', satzpunktdifferenz: -8 },
-    { platz: 8, mannschaft: 'SV Weil im Schönbuch-0', matchpunkte: '0 : 14', satzpunkte: '0 : 42', satzpunktdifferenz: -42 }
-  ];
+  public loadingLigatabelle = true;
+  public rowsLigatabelle: TableRow[];
+  public providedID: number;
+  private hasID: boolean;
+  private hasVeranstaltung: boolean = true;
+
+  private isDeselected: boolean = false;
+  private remainingLigatabelleRequests: number;
+
+  private loadedVeranstaltungen: Map<number, VeranstaltungDO[]>;
+  private selectedVeranstaltung: VeranstaltungDO;
+
+  private veranstaltungIdMap: Map<number, VeranstaltungDO>;
+  public selectedItemId: number;
+  private selectedYearForVeranstaltung: number; //In der Tabelle selektiertes Sportjahr
+
+
 
   constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private notificationService: NotificationService,
+    private veranstaltungsDataProvider: VeranstaltungDataProviderService,
+    private ligatabelleDataProvider: LigatabelleDataProviderService,
     private onOfflineService: OnOfflineService,
     private currentUserService: CurrentUserService,
   ) {
@@ -52,6 +72,28 @@ export class FullscreenComponent extends CommonComponentDirective implements OnI
 
   ngOnInit() {
     this.startClock();
+    if(this.isDeselected == false) {
+      console.log('Component is not deselected.');
+      this.loadTableData();
+      console.log('Table data loaded.');
+      this.providedID = undefined;
+      this.hasID = false;
+      this.notificationService.discardNotification();
+      this.route.params.subscribe((params) => {
+        console.log('Route params subscribed:', params);
+        if (!isUndefined(params[ID_PATH_PARAM])) {
+          this.providedID = parseInt(params[ID_PATH_PARAM], 10);
+          console.log('Provided ID:', this.providedID);
+          this.hasID = true;
+          params[ID_PATH_PARAM] === "ligaid" ? this.hasID = false : undefined;
+          this.selectedYearForVeranstaltung != undefined && this.hasID
+            ? this.loadVeranstaltungFromLigaIDAndSportYear(this.providedID, this.selectedYearForVeranstaltung) : undefined;
+        } else {
+          console.log('No ID provided in route params.');
+          this.router.navigate(['/wkdurchfuehrung/fullscreen']);
+        }
+      });
+    }
   }
 
   startClock() {
@@ -66,11 +108,105 @@ export class FullscreenComponent extends CommonComponentDirective implements OnI
    *  If the boolean value is true, then the page will be reloaded and due to the expired session, the user will
    *  be logged out automatically.
    */
+  public loadVeranstaltungFromLigaIDAndSportYear(urlLigaID: number, selectedSportYear: number){
+    this.veranstaltungsDataProvider.findByLigaIdAndYear(urlLigaID, selectedSportYear)
+        .then((response: BogenligaResponse<VeranstaltungDO>) => this.handleFindVeranstaltungSuccess(response))
+        .catch((response: BogenligaResponse<VeranstaltungDO>) => this.handleFindVeranstaltungFailure(response));
+  }
+
+  private handleFindVeranstaltungSuccess(response: BogenligaResponse<VeranstaltungDO>): void {
+    this.hasVeranstaltung = true;
+    this.selectedItemId = response.payload.id;
+  }
+
+  private handleFindVeranstaltungFailure(error: any): void {
+    // Routing zurück zur Ligatabelle URL, wenn keine ID gefunden wird
+    this.hasVeranstaltung = false;
+    const link = '/ligatabelle';
+    this.router.navigateByUrl(link);
+  }
+
+
+  /** When a MouseOver-Event is triggered, it will call this inMouseOver-function.
+   *  This function calls the checkSessionExpired-function in the sessionHandling class and get a boolean value back.
+   *  If the boolean value is true, then the page will be reloaded and due to the expired session, the user will
+   *  be logged out automatically.
+   */
   public onMouseOver(event: any) {
     const isExpired = this.sessionHandling.checkSessionExpired();
     if (isExpired) {
       window.location.reload();
     }
+  }
+
+  private async loadTableData() {
+    this.loadedVeranstaltungen = new Map();
+    this.veranstaltungIdMap = new Map();
+
+    try {
+      const year = 2018; // Jahr festlegen
+
+      // Direkt nach dem Jahr 2018 filtern
+      const responseVeranstaltung = await this.veranstaltungsDataProvider.findBySportjahrDestinct(year);
+
+      for (const veranstaltung of responseVeranstaltung.payload) {
+        this.veranstaltungIdMap.set(veranstaltung.id, veranstaltung); // -> Ligatabelle
+        this.loadedVeranstaltungen.set(year, responseVeranstaltung.payload);  // -> "Liga"
+      }
+      console.log('Veranstaltungen für das Jahr 2018:', responseVeranstaltung.payload);
+      if (responseVeranstaltung.payload.length > 0) {
+        const selectedVeranstaltung = responseVeranstaltung.payload[0];
+        this.selectedVeranstaltung = selectedVeranstaltung;
+        this.loadLigaTableRows();
+      } else {
+        console.log('Keine Veranstaltungen gefunden für das Jahr 2018.');
+      }
+
+      this.loading = false;
+      this.loadingLigatabelle = false;
+    } catch (e) {
+      this.loading = false;
+      this.loadingLigatabelle = false;
+      console.log('Fehler beim Laden der Daten:', e);
+    }
+  }
+
+  private loadLigaTableRows() {
+    this.loadingLigatabelle = true;
+    this.ligatabelleDataProvider.getLigatabelleVeranstaltung(this.selectedVeranstaltung.id)
+        .then((response: BogenligaResponse<LigatabelleErgebnisDO[]>) => this.handleLigatabelleSuccess(response))
+        .catch(() => this.handleLigatabelleFailure());
+  }
+
+  private handleLigatabelleFailure() {
+    console.log('failure');
+    this.rowsLigatabelle = [];
+    this.loadingLigatabelle = false;
+  }
+
+  private handleLigatabelleSuccess(response: BogenligaResponse<LigatabelleErgebnisDO[]>) {
+    console.log('success');
+    this.rowsLigatabelle = []; // reset array to ensure change detection
+    this.remainingLigatabelleRequests = response.payload.length;
+    if (response.payload.length <= 0
+    ) {
+      this.loadingLigatabelle = false;
+    } else {
+      this.rowsLigatabelle = toTableRows(response.payload);
+      this.loadingLigatabelle = false;
+    }
+  }
+
+  public ligatabelleLinking() {
+    const link = '/wettkaempfe/' + this.buttonForward;
+    this.router.navigateByUrl(link);
+  }
+
+
+  public goToLigaDetails(){
+    console.log("IDDD" + this.providedID);
+    const link = '/home/' +  this.providedID;
+    this.router.navigateByUrl(link);
   }
 
 }
