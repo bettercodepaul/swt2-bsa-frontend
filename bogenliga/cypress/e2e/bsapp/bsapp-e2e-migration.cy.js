b465343fdece55ea34c516f6ded594e0fb925be3
BenjaminKem <BenjaminK.Businesswork@gmail.com>
2024-05-02 10:55:47 +0200

    Added Cypress Tests For Frontend funtionality ~Beni and Erik und Enzo

--- bogenliga/cypress/e2e/bsapp/bsapp-e2e-migration.cy.js
+++ bogenliga/cypress/e2e/bsapp/bsapp-e2e-migration.cy.js
@@ -7,7 +7,6 @@ describe('Verwaltung/Migration tests', function () {
 
   afterEach(() => {
     //NOTE: Preparation for next test
-
     cy.wait(1000)
     cy.disbandModalIfShown()
     cy.wait(1000)
@@ -46,8 +45,8 @@ describe('Verwaltung/Migration tests', function () {
     cy.url().should('include', '#/home')
     cy.intercept({
       method: 'GET',
-      url: `http://localhost:9000/v1/trigger`,
-    }).as('findall-request');
+      url: `http://localhost:9000/v1/trigger/findErrors?offsetMultiplicator=0&queryPageLimit=500`,
+    }).as('findallErrors-request');
     cy.get('[data-cy=sidebar-verwaltung-button]').click()
     cy.wait(1000)
     cy.url().should('include', '#/verwaltung')
@@ -56,7 +55,7 @@ describe('Verwaltung/Migration tests', function () {
     cy.url().should('include', '#/verwaltung/migration')
 
     cy.wait(1000)
-    cy.wait('@findall-request')
+    cy.wait('@findallErrors-request')
   })
 
   /**
@@ -68,45 +67,102 @@ describe('Verwaltung/Migration tests', function () {
    * 5. clicks on the "Neue Migration" Button
    * 6. checks if a call was made to Backend
    */
-
-  it('Anzeige Filter Migration Button Click', () => {
-    cy.url().should('include', '#/home')
-    cy.get('[data-cy=sidebar-verwaltung-button]').click()
+  it('Filtern nach erfolgreichen Einträgen', () => {
+    cy.get('[data-cy=status-filter-selection]').select('1: Erfolgreich')
     cy.wait(1000)
-    cy.url().should('include', '#/verwaltung')
-    cy.get('[data-cy=verwaltung-sync-button]').click()
+    //Intercept Call to Backend
+    cy.intercept({
+      method: 'GET',
+      url: `http://localhost:9000/v1/trigger/findSuccessed?offsetMultiplicator=0&queryPageLimit=500`,
+    }).as('filter');
+    cy.wait('@filter')
+  })
+  it('Filtern nach laufenden Einträgen', () => {
+    cy.get('[data-cy=status-filter-selection]').select('2: Laufend')
     cy.wait(1000)
-    cy.url().should('include', '#/verwaltung/migration')
+    //Intercept Call to Backend
+    cy.intercept({
+      method: 'GET',
+      url: `http://localhost:9000/v1/trigger/findInProgress?offsetMultiplicator=0&queryPageLimit=500`,
+    }).as('filter');
+    cy.wait('@filter')
+  })
+  it('Filtern nach neuen Einträgen', () => {
+    cy.get('[data-cy=status-filter-selection]').select('3: Neu')
     cy.wait(1000)
+    //Intercept Call to Backend
+    cy.intercept({
+      method: 'GET',
+      url: `http://localhost:9000/v1/trigger/findNews?offsetMultiplicator=0&queryPageLimit=500`,
+    }).as('filter');
+    cy.wait('@filter')
+  })
+  it('Filtern nach allen Einträgen', () => {
+    cy.get('[data-cy=status-filter-selection]').select('4: Alle')
+    cy.wait(1000)
+    //Intercept Call to Backend
+    cy.intercept({
+      method: 'GET',
+      url: `http://localhost:9000/v1/trigger/findAllWithPages?offsetMultiplicator=0&queryPageLimit=500`,
+    }).as('filter');
+    cy.wait('@filter')
+  })
+  it('Previous Page Test', () => {
+    cy.get('[data-cy=next-page-button]').click()
+    cy.wait(1000)
+    //Intercept Call to Backend
+    cy.intercept({
+      method: 'GET',
+      url: `http://localhost:9000/v1/trigger/findAllWithPages?offsetMultiplicator=1&queryPageLimit=500`,
+    }).as('nextPage');
+    cy.wait('@nextPage')
+  })
+  it('Next Page Test', () => {
+    cy.get('[data-cy=previous-page-button]').click()
+    cy.wait(1000)
+    //Intercept Call to Backend
+    cy.intercept({
+      method: 'GET',
+      url: `http://localhost:9000/v1/trigger/findAllWithPages?offsetMultiplicator=0&queryPageLimit=500`,
+    }).as('previousPage');
+    cy.wait('@previousPage')
+  })
+  it('Next Page Test Above', () => {
+    cy.get('[data-cy=next-page-button-above]').click()
+    cy.wait(1000)
+    //Intercept Call to Backend
+    cy.intercept({
+      method: 'GET',
+      url: `http://localhost:9000/v1/trigger/findAllWithPages?offsetMultiplicator=1&queryPageLimit=500`,
+    }).as('nextPage');
+    cy.wait('@nextPage')
+  })
+  it('Previous Page Test Above', () => {
+    cy.get('[data-cy=previous-page-button-above]').click()
+    cy.wait(1000)
+    //Intercept Call to Backend
+    cy.intercept({
+      method: 'GET',
+      url: `http://localhost:9000/v1/trigger/findAllWithPages?offsetMultiplicator=0&queryPageLimit=500`,
+    }).as('previousPage');
+    cy.wait('@previousPage')
+  })
+  it('Filtern nach error Einträgen', () => {
+    cy.get('[data-cy=status-filter-selection]').select('0: Fehlgeschlagen')
+    cy.wait(1000)
+    //Intercept Call to Backend
+    cy.intercept({
+      method: 'GET',
+      url: `http://localhost:9000/v1/trigger/findErrors?offsetMultiplicator=0&queryPageLimit=500`,
+    }).as('filter');
+    cy.wait('@filter')
+  })
+  it('Anzeige Filter Migration und start der Migration', () => {
     cy.disbandModalIfShown();
     cy.wait(1000)
     cy.get('[data-cy=dsb-mitglied-add-button]').click()
     cy.wait(1000)
     cy.get('[data-cy=filter-migration-button]').click()
-
-    //Intercept Call to Backend
-    cy.intercept({
-      method: 'GET',
-      url: `http://localhost:9000/v1/trigger/buttonSync`,
-    }).as('request');
-    cy.wait('@request')
-  })
-
-
-  it('Change Pages Buttons', () => {
-    cy.url().should('include', '#/home')
-    cy.get('[data-cy=sidebar-verwaltung-button]').click()
-    cy.wait(1000)
-    cy.url().should('include', '#/verwaltung')
-    cy.get('[data-cy=verwaltung-sync-button]').click()
-    cy.wait(1000)
-    cy.url().should('include', '#/verwaltung/migration')
-    cy.wait(1000)
-    cy.disbandModalIfShown();
-    cy.wait(1000)
-    cy.get('[data-cy=next-page-button]').click()
-    cy.wait(1000)
-    cy.get('[data-cy=previous-page-button]').click()
 
     //Intercept Call to Backend
     cy.intercept({
