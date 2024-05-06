d30d4d9213c0462e6b223508fa90472e25a66639
BenjaminKem <BenjaminK.Businesswork@gmail.com>
2024-05-06 18:19:30 +0200

    Anfang des Tickets mit hinzufügen von den benötigten Buttons uns einer Methode im Backend damit ein Branch erstellt und gepushed wird. ~Beni and Erik

--- bogenliga/src/app/modules/verwaltung/services/migration-data-provider.service.ts
+++ bogenliga/src/app/modules/verwaltung/services/migration-data-provider.service.ts
@@ -138,7 +138,24 @@ export class MigrationProviderService extends DataProviderService {
           });
     });
   }
+  public findDeleteEntries(): Promise<BogenligaResponse<TriggerDO[]>> {
+    // return promise
+    // sign in success -> resolve promise
+    // sign in failure -> reject promise with result
+    return new Promise((resolve, reject) => {
+      this.restClient.GET<Array<VersionedDataTransferObject>>(new UriBuilder().fromPath(this.getUrl()).path('deleteEntries').build())
+          .then((data: VersionedDataTransferObject[]) => {
+            resolve({result: RequestResult.SUCCESS, payload: fromPayloadArray(data)});
+          }, (error: HttpErrorResponse) => {
 
+            if (error.status === 0) {
+              reject({result: RequestResult.CONNECTION_PROBLEM});
+            } else {
+              reject({result: RequestResult.FAILURE});
+            }
+          });
+    });
+  }
   public startMigration() {
     this.restClient.GET(new UriBuilder().fromPath(this.getUrl()).path('buttonSync').build())
   }
