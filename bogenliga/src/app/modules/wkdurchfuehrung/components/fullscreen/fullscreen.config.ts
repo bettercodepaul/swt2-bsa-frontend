7c6771030217b1d230c21a4f19dbdf0f2251462c
AnnaMariaLioka <Anna-Maria.Lioka@Student.Reutlingen-University.de>
2024-05-23 18:32:33 +0200

    #1631 Neue fullscreen.config.ts erstellt. Neue config in fullscreen.component.ts verknüpft. In die neue config, die Zeilen für "Matchpunkte" entfernt, da diese nicht in die Vollbildanzeige angezeigt werden sollen.

---
+++ bogenliga/src/app/modules/wkdurchfuehrung/components/fullscreen/fullscreen.config.ts
@@ -0,0 +1,50 @@
+import {NavigationDialogConfig} from '../../../shared/components/dialogs';
+import {TableConfig} from '@shared/components/tables/types/table-config.interface';
+
+export const WETTKAEMPFE_CONFIG: NavigationDialogConfig = {
+  moduleTranslationKey:    'WETTKAEMPFE',
+  pageTitleTranslationKey: 'WETTKAEMPFE.WETTKAEMPFE.TITLE',
+  navigationCardsConfig:   {
+    navigationCards: []
+  }
+};
+
+// the Ligatabelle is not sortable
+export const FULLSCREEN_TABLE_CONFIG: TableConfig = {
+
+  columns: [
+    {
+      translationKey: 'WETTKAEMPFE.LIGATABELLE.MATCH_ID',
+      propertyName:   'match_nr',
+      width:           15,
+      sortable:        false,
+    },
+    {
+      translationKey: 'WETTKAEMPFE.LIGATABELLE.TABELLENPLATZ',
+      propertyName:   'tabellenplatz',
+      width:           15,
+      sortable:        false,
+    },
+    {
+      translationKey: 'WETTKAEMPFE.LIGATABELLE.MANNSCHAFTNAME',
+      propertyName:   'mannschaft_name',
+      width:           70,
+      sortable:        false,
+    },
+    {
+      translationKey: 'WETTKAEMPFE.LIGATABELLE.SATZPUNKTE',
+      propertyName:   'satzpunkte',
+      width:           15,
+      sortable:        false,
+    },
+    {
+      translationKey: 'WETTKAEMPFE.LIGATABELLE.SATZPUNKTDIFFERENZ',
+      propertyName:   'satzpkt_differenz',
+      width:           15,
+      sortable:        false,
+    }
+  ]
+
+};
+
+
