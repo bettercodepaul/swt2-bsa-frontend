d67706fe2516559723a1c11202ce597d354bc7fe
Lennart Raach <Lennart.Raach@student.reutlingen-university.de>
2024-06-19 13:04:50 +0200

    pushing new configurations for the matchstatistik

---
+++ bogenliga/src/app/modules/wettkampf/components/wettkampf/wettkampergebnis/tabelle.sechsmatch.config.ts
@@ -0,0 +1,50 @@
+import {TableConfig} from '@shared/components/tables/types/table-config.interface';
+
+/**
+ * Table layout for a Wettkampf with 4 Mannschaften (Matchstatistik)
+ */
+export const WETTKAMPF_TABLE_SECHS_MATCHES_CONFIG: TableConfig = {
+  // Tabellenspaltenbezeichner in die JSON auslagern
+  actions: {actionTypes: []},
+  columns: [
+    {
+      translationKey: 'MANNSCHAFTEN.MANNSCHAFTEN.TABLE.COLUMNS.RUECKENNUMMER',
+      propertyName:   'rueckennummer',
+      width:          100,
+    },
+    {
+      translationKey: 'MANNSCHAFTEN.MANNSCHAFTEN.TABLE.COLUMNS.SCHUETZE',
+      propertyName:   'dsbMitgliedName',
+      width:          100,
+    },
+    {
+      translationKey: 'MANNSCHAFTEN.MANNSCHAFTEN.TABLE.COLUMNS.MATCH1',
+      propertyName:   'match1',
+      width:          100,
+    },
+    {
+      translationKey: 'MANNSCHAFTEN.MANNSCHAFTEN.TABLE.COLUMNS.MATCH2',
+      propertyName:   'match2',
+      width:          30,
+    },
+    {
+      translationKey: 'MANNSCHAFTEN.MANNSCHAFTEN.TABLE.COLUMNS.MATCH3',
+      propertyName:   'match3',
+      width:          30,
+    },
+    {
+      translationKey: 'MANNSCHAFTEN.MANNSCHAFTEN.TABLE.COLUMNS.MATCH4',
+      propertyName:   'match4',
+      width:          30,
+    },
+    {
+      translationKey: 'MANNSCHAFTEN.MANNSCHAFTEN.TABLE.COLUMNS.MATCH5',
+      propertyName:   'match5',
+      width:          30,
+    },
+    {
+      translationKey: 'MANNSCHAFTEN.MANNSCHAFTEN.TABLE.COLUMNS.MATCH6',
+      propertyName:   'match6',
+      width:          30,
+    }],
+};
