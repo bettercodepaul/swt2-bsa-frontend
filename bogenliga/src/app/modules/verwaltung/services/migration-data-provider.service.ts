1472c366591f7cf473f81106f09ddafd34cb1bcf
erikberner <bernererik@web.de>
2024-07-20 11:18:54 +0200

    #1696 ~ Fehlerbehandlung implementiert

--- bogenliga/src/app/modules/verwaltung/services/migration-data-provider.service.ts
+++ bogenliga/src/app/modules/verwaltung/services/migration-data-provider.service.ts
@@ -168,7 +168,7 @@ export class MigrationProviderService extends DataProviderService {
   public countEntriesByStatusAndDateInterval(timestamp:string, status:string):Promise<BogenligaResponse<TriggerCountDO>>{
 
     return new Promise((resolve, reject) => {
-      this.restClient.GET<DataTransferObject>(new UriBuilder().fromPath(this.getUrl()).path('&dateInterval=' + timestamp.toString() + '&status=' + status.toString()).build())
+      this.restClient.GET<DataTransferObject>(new UriBuilder().fromPath(this.getUrl()).path('countEntriesByStatusAndDateInterval?dateInterval=' + timestamp + '&status=' + status).build())
           .then((data: DataTransferObject) => {
             resolve({result: RequestResult.SUCCESS, payload: fromCountPayload(data)});
           }, (error: HttpErrorResponse) => {
