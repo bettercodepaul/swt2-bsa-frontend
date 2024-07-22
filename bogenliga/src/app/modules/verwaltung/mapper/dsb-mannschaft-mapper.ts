96f8a7714ff6d19235e6a38cab7f6c4659e2b166
BenjaminKem <BenjaminK.Businesswork@gmail.com>
2024-07-22 16:04:26 +0200

    finale fixes ~Beni and Erik

--- bogenliga/src/app/modules/verwaltung/mapper/dsb-mannschaft-mapper.ts
+++ bogenliga/src/app/modules/verwaltung/mapper/dsb-mannschaft-mapper.ts
@@ -1,7 +1,6 @@
 import {VersionedDataTransferObject} from '../../shared/data-provider';
 import {DsbMannschaftDTO} from '../types/datatransfer/dsb-mannschaft-dto.class';
-import {DsbMannschaftVerAndWettDo} from '@verwaltung/types/dsb-mannschaft-ver-and-wett-do';
-import {DsbMannschaftVerAndWettDto} from '@verwaltung/types/datatransfer/dsb-mannschaft-ver-and-wett-dto';
+
 
 // export function toDO(mannschaftDTO: MannschaftDTO): MannschaftDO {
 //
@@ -14,17 +13,9 @@ import {DsbMannschaftVerAndWettDto} from '@verwaltung/types/datatransfer/dsb-man
 export function fromPayload(payload: VersionedDataTransferObject): DsbMannschaftDTO {
   return DsbMannschaftDTO.copyFrom(payload);
 }
-export function fromPayloadVerAndWett(payload: DsbMannschaftVerAndWettDo): DsbMannschaftVerAndWettDto {
-  return DsbMannschaftVerAndWettDto.copyFrom(payload);
-}
 export function fromPayloadArray(payload: VersionedDataTransferObject[]): DsbMannschaftDTO[] {
   const list: DsbMannschaftDTO[] = [];
   payload.forEach((single) => list.push(fromPayload(single)));
   return list;
 }
 
-export function fromPayloadArrayVerAndWett(payload: DsbMannschaftVerAndWettDo[]): DsbMannschaftVerAndWettDto[] {
-  const list: DsbMannschaftVerAndWettDto[] = [];
-  payload.forEach((single) => list.push(fromPayloadVerAndWett(single)));
-  return list;
-}
