6be56b28072c7c2678bd5670ffdd25bb8111c1ce
Sarah Keifer <sarah_andrea.keifer@student.reutlingen>
2024-11-07 13:18:05 +0100

    Hinzufügen von Permission um Vereinsseite als Anynymer User öffnen zu können

--- bogenliga/src/app/modules/verwaltung/guards/verein-overview.guard.ts
+++ bogenliga/src/app/modules/verwaltung/guards/verein-overview.guard.ts
@@ -9,7 +9,8 @@ export class VereinOverviewGuard implements CanActivate {
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     // CAN_MODIFY_STAMMDATEN or CAN_CREATE_MANNSCHAFT required to activate Verein Overview
+    // added "CAN_READ_DEFAULT" for opening Vereinsseite for every permission
     return this.currentUserService.hasAnyPermisson(
-      [UserPermission.CAN_MODIFY_STAMMDATEN, UserPermission.CAN_CREATE_MANNSCHAFT]);
+      [UserPermission.CAN_READ_DEFAULT, UserPermission.CAN_MODIFY_STAMMDATEN, UserPermission.CAN_CREATE_MANNSCHAFT]);
   }
 }
