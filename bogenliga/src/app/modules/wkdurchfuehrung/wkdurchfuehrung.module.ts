80fd5bbf958bd7aa5a61b205fa1b17829d13c42c
Simon Knoblich <simonknoblich.sk@gamil.com>
2024-12-12 15:30:15 +0100

    Datenbankanbindung für Spotter interface und QR-Code für den weg zum Spotter Interface

--- bogenliga/src/app/modules/wkdurchfuehrung/wkdurchfuehrung.module.ts
+++ bogenliga/src/app/modules/wkdurchfuehrung/wkdurchfuehrung.module.ts
@@ -26,12 +26,14 @@ import {
 import { TeilnemendeManschaftenTabelleComponent } from './components/teilnemende-manschaften-tabelle/teilnemende-manschaften-tabelle.component';
 import { FullscreenComponent } from './components/fullscreen/fullscreen.component';
 import { TabletAdminPopUpComponent } from './components/tablet-admin/tablet-admin-pop-up/tablet-admin-pop-up.component';
+import {QRCodeModule} from 'angularx-qrcode';
 @NgModule({
-  imports:      [
+  imports: [
     CommonModule,
     RouterModule.forChild(wkdurchfuehrung_ROUTES),
     SharedModule.forChild(),
-    FormsModule
+    FormsModule,
+    QRCodeModule
   ],
   declarations: [
     WkdurchfuehrungComponent,
@@ -70,4 +72,3 @@ export class WkdurchfuehrungModule {
   }
 
 }
-
