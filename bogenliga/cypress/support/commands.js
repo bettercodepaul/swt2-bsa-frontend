e083591acd462d90c439f19e819979164ba224c9
Sarah Keifer <sarah_andrea.keifer@student.reutlingen>
2025-01-09 16:35:07 +0100

    Hinzufügen von Cypress Tests für Rolle des Ausrichters

--- bogenliga/cypress/support/commands.js
+++ bogenliga/cypress/support/commands.js
@@ -225,8 +225,6 @@ Cypress.Commands.add('loginUserTest', () => {
 
 })
 
-
-
 Cypress.Commands.add('loginAusrichter', () => {
   cy.visit('http://localhost:4200/#/home');
   cy.get('[data-cy=login-button]').click();
