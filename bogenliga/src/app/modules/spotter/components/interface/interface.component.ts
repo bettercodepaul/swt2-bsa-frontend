41593a491d7ef68cf3cf96cba2b41fad4977bea2
rosalie wieland <rosaliewieland.uni@gmail.com>
2025-01-23 22:18:26 +0100

    acess gets blocked to create new accesstoken if there is already a spotter on the page also from the same tab cant access the page again until delete and restore the access for the bahn

--- bogenliga/src/app/modules/spotter/components/interface/interface.component.ts
+++ bogenliga/src/app/modules/spotter/components/interface/interface.component.ts
@@ -42,6 +42,7 @@ export class InterfaceComponent implements OnInit, OnDestroy{
   allowedToSaveSet = false;
   TabletSession: TabletSessionProviderService;
   session: TabletSessionDO;
+  private finish: boolean;
 
   constructor(private router: Router, private spotterService: SpotterService, private tabletSession: TabletSessionProviderService ) {
     this.spotterMatches = [];
