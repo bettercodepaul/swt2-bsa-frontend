ef86b3b2c49379bb79c0367dd8c9fcbf817001ad
mklemmingen <104225647+mklemmingen@users.noreply.github.com>
2025-05-21 14:21:33 +0200

    removed stale routes, so they cant be accessed without going through and init by tablet switch state after get, also removed old mocks of the old plan, and del wettkampfleiter-tablet-view

--- bogenliga/src/app/modules/schusszettel/schusszettel.routing.ts
+++ bogenliga/src/app/modules/schusszettel/schusszettel.routing.ts
@@ -1,17 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { RegisterRueckennummerComponent } from './maske1registrierung/register-rueckennummer.component';
-import { PasseEingabeComponent } from './maske2eingabe/passe-eingabe.component';
-import { WarteBestaetigungComponent } from './maske3aktualisierung/warte-bestaetigung.component';
-import { SchusszettelSetupGuard } from './schusszettel-setup.guard';
-import { SchusszettelInitComponent } from './setup/schusszettel-init.component';
 import { SchusszettelTabletAdminComponent } from './setup/schusszettel-tablet-admin.component';
 import {TabletComponent} from './tablet/tablet.component';
 const routes: Routes = [
-  { path: 'setup', component: SchusszettelInitComponent },
-  { path: 'registrierung', component: RegisterRueckennummerComponent },
-  { path: 'eingabe', component: PasseEingabeComponent, canActivate: [SchusszettelSetupGuard] },
-  { path: 'aktualisierung', component: WarteBestaetigungComponent, canActivate: [SchusszettelSetupGuard] },
   {path: 'wettkampfleiter-tablet-view', component: SchusszettelTabletAdminComponent},
   {path: 'tablet', component: TabletComponent}
 ];
