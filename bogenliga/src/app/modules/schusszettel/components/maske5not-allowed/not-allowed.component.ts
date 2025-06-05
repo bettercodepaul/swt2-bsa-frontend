cff54111af0bb4cf2bb3848ef22421df0a933290
yummi2 <youmna.samouneh@student.reutlingen-university.de>
2025-06-05 17:40:22 +0200

    feat: Navbar und Footer für not-allowed Masken ausgeblendet

--- bogenliga/src/app/modules/schusszettel/components/maske5not-allowed/not-allowed.component.ts
+++ bogenliga/src/app/modules/schusszettel/components/maske5not-allowed/not-allowed.component.ts
@@ -1,16 +1,24 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {TabletSchusszettel} from '../../models/tablet-schusszettel.model';
+import { AppComponent } from 'src/app/app.component';
 
 @Component({
   selector: 'bla-maske5not-allowed',
   templateUrl: './not-allowed.component.html',
   styleUrls: ['./not-allowed.component.scss']
 })
-export class NotAllowedComponent {
+export class NotAllowedComponent implements OnInit, OnDestroy {
   @Input() infos!: TabletSchusszettel;
 
+  constructor(private app: AppComponent) {}
   /** reloads the page to let the user try again */
   retry(): void {
     window.location.reload();
+  }
+  ngOnInit(): void {
+    this.app.fullscreen = true; //  Navbar und Footer ausblenden
+  }
+  ngOnDestroy(): void {
+    this.app.fullscreen = false; //  Beim Verlassen wieder anzeigen
   }
 }
