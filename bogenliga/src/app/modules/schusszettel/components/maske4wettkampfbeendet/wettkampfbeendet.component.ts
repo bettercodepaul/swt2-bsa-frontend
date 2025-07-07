c7ac8e6dfaa3a9644ff6453d9f57eba6226c68bd
mklemmingen <104225647+mklemmingen@users.noreply.github.com>
2025-07-07 15:51:12 +0200

    linked to ligamatchbe, localized, edge case handled

--- bogenliga/src/app/modules/schusszettel/components/maske4wettkampfbeendet/wettkampfbeendet.component.ts
+++ bogenliga/src/app/modules/schusszettel/components/maske4wettkampfbeendet/wettkampfbeendet.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
 import {TabletSchusszettel} from '../../models/tablet-schusszettel.model';
 import { AppComponent } from 'src/app/app.component';
 
@@ -12,11 +12,19 @@ export class WettkampfbeendetComponent implements OnInit, OnDestroy {
   /** full tablet state; only render when set */
   @Input() infos: TabletSchusszettel | null = null;
 
+  /** Event emitter for weiter button */
+  @Output() weiter = new EventEmitter<void>();
+
   constructor(private app: AppComponent) {}
 
   /** allow user to reload or navigate away */
   reload(): void {
     window.location.reload();
+  }
+
+  /** emit weiter event to show final state */
+  onWeiter(): void {
+    this.weiter.emit();
   }
 
   ngOnInit(): void {
