80e4dbfb17f006fc5ce7c08b3e7d5aa4a44d34b7
Joshua Jeglinski <joshua.jeglinski@student.reutlingen-university.de>
2025-11-01 13:57:02 +0100

    Add "Ligaübersicht" module and component with routing and analytics tracking

---
+++ bogenliga/src/app/modules/liga-overview/index.ts
@@ -0,0 +1,2 @@
+export * from './liga-overview.module';
+export * from './components/liga-overview/liga-overview.component';
---
+++ bogenliga/src/app/modules/liga-overview/liga-overview.module.ts
@@ -0,0 +1,25 @@
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {SharedModule} from '../shared';
+import {LigaOverviewComponent} from './components/liga-overview/liga-overview.component';
+import {LIGA_OVERVIEW_ROUTES} from './liga-overview.routing';
+
+/**
+ * Modul für die Ligaübersicht
+ *
+ * Enthält alle Komponenten, Services und Routen für die Ligaübersicht-Funktionalität.
+ */
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(LIGA_OVERVIEW_ROUTES),
+    SharedModule
+  ],
+  declarations: [
+    LigaOverviewComponent
+  ]
+})
+export class LigaOverviewModule {
+}
