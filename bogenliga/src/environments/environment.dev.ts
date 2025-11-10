242aa98ee6bebca0bfeaad0f1a09719ed7ab23f2
Moritz34 <moritzfeucht3@gmail.com>
2025-11-10 20:21:56 +0100

    Revert "Add analytics instrumentation for Ligaübersicht events"
    This reverts commit 8566cd5b822bfa965c4545ff120ddf3ea3db7731.

--- bogenliga/src/environments/environment.dev.ts
+++ bogenliga/src/environments/environment.dev.ts
@@ -2,9 +2,5 @@ export const environment = {
   production: true,
   showLabel: false,
   label: 'PROD',
-  backendBaseUrl: 'http://app.bogenliga.de/api',
-  analytics: {
-    enabled: true,
-    transport: 'auto' as 'console' | 'auto',
-  }
+  backendBaseUrl: 'http://app.bogenliga.de/api'
 };
