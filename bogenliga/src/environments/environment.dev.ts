8566cd5b822bfa965c4545ff120ddf3ea3db7731
Moritz34 <moritzfeucht3@gmail.com>
2025-11-10 20:15:18 +0100

    Add analytics instrumentation for Ligaübersicht events

--- bogenliga/src/environments/environment.dev.ts
+++ bogenliga/src/environments/environment.dev.ts
@@ -2,5 +2,9 @@ export const environment = {
   production: true,
   showLabel: false,
   label: 'PROD',
-  backendBaseUrl: 'http://app.bogenliga.de/api'
+  backendBaseUrl: 'http://app.bogenliga.de/api',
+  analytics: {
+    enabled: true,
+    transport: 'auto' as 'console' | 'auto',
+  }
 };
--- bogenliga/src/environments/environment.prod.ts
+++ bogenliga/src/environments/environment.prod.ts
@@ -2,5 +2,9 @@ export const environment = {
   production: true,
   showLabel: false,
   label: 'PROD',
-  backendBaseUrl: 'https://liga.bsapp.de/api'
+  backendBaseUrl: 'https://liga.bsapp.de/api',
+  analytics: {
+    enabled: true,
+    transport: 'auto' as 'console' | 'auto',
+  }
 };
