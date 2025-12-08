d34d1186c9c79677b674ae441a5fd4ccf0d2f520
Cedric Rollinger <cedric.ro@gmx.de>
2025-12-08 16:35:47 +0100

    display mannschaft name

--- bogenliga/src/app/modules/mannschaftsuebersicht/components/mannschaftsuebersicht/mannschaftsuebersicht.component.ts
+++ bogenliga/src/app/modules/mannschaftsuebersicht/components/mannschaftsuebersicht/mannschaftsuebersicht.component.ts
@@ -1,13 +1,20 @@
 import {Component, OnInit} from '@angular/core';
 import {isUndefined} from "@shared/functions";
-import {CommonComponentDirective} from "@shared/components";
+import {CommonComponentDirective, NavigationDialogConfig} from "@shared/components";
 import {ActivatedRoute, Router} from "@angular/router";
 import {BogenligaResponse} from "@shared/data-provider";
-import {VereinDTO} from "@verwaltung/types/datatransfer/verein-dto.class";
 import {DsbMannschaftDataProviderService} from "@verwaltung/services/dsb-mannschaft-data-provider.service";
 import {DsbMannschaftDTO} from "@verwaltung/types/datatransfer/dsb-mannschaft-dto.class";
 
 const ID_PATH_PARAM = 'id';
+
+const MANNSCHAFTSUEBERSICHT_CONFIG: NavigationDialogConfig = {
+  moduleTranslationKey: 'MANNSCHAFTSUEBERSICHT',
+  pageTitleTranslationKey: 'MANNSCHAFTSUEBERSICHT.TITLE',
+  navigationCardsConfig: {
+    navigationCards: []
+  },
+};
 
 @Component({
   selector: 'bla-mannschaftsuebersicht',
@@ -16,7 +23,7 @@ const ID_PATH_PARAM = 'id';
 })
 export class MannschaftsuebersichtComponent extends CommonComponentDirective implements OnInit {
   private providedID: number | null = null;
-  private mannschaft: DsbMannschaftDTO | null = null;
+  public mannschaft: DsbMannschaftDTO | null = null;
   constructor(
     private router: Router,
     private route: ActivatedRoute,
@@ -29,23 +36,34 @@ export class MannschaftsuebersichtComponent extends CommonComponentDirective imp
     // Intentionally left blank for now
     console.log('Bin in Mannschaftsuebersicht');
     this.providedID = null;
-    // this.providedID = this.findFirstVereinID();
 
     this.loading = true;
     this.route.params.subscribe((params) => {
       if (!isUndefined(params[ID_PATH_PARAM])) {
         this.providedID = parseInt(params[ID_PATH_PARAM], 10);
         console.log('This.providedID: ' + this.providedID);
+        // Load after we have the ID
+        this.loadMannschaftData();
+      } else {
+        // no id provided
+        this.loading = false;
       }
     });
-    this.loadMannschaftData()
   }
   loadMannschaftData(): void{
+    // set loading state
+    this.loading = true;
     this.mannschaftDataProvider.findById(this.providedID)
       .then((response: BogenligaResponse<DsbMannschaftDTO>) => {
         console.log(response)
         this.mannschaft = response.payload!;
+        this.loading = false;
       })
-      .catch((response: BogenligaResponse<DsbMannschaftDTO>) => {console.error(response); });
+      .catch((response: BogenligaResponse<DsbMannschaftDTO>) => {
+        console.error(response);
+        this.loading = false;
+      });
   }
+
+  protected readonly config = MANNSCHAFTSUEBERSICHT_CONFIG;
 }
