c2efc406b9d7fcdd64ea7650fe142eedfd9b4c12
Jakob <jakob.frey@student.reutlingen-university.de>
2026-01-06 14:13:16 +0100

    finishing touches for mannschaftsübersicht

--- bogenliga/src/app/app.routing.ts
+++ bogenliga/src/app/app.routing.ts
@@ -16,7 +16,6 @@ export const ROUTES: Routes = [
   {path: 'user', loadChildren: () => import('src/app/modules/user/user.module').then((m) => m.UserModule)},
   {path: 'vereine', loadChildren: () => import('src/app/modules/vereine/vereine.module').then((m) => m.VereineModule)},
   {path: 'vereine/:id', loadChildren: () => import('src/app/modules/vereine/vereine.module').then((m) => m.VereineModule)},
-  {path: 'vereine/:id/:mannschaftID', loadChildren: () => import('src/app/modules/mannschaft/mannschaft.module').then((m) => m.MannschaftModule)},
   {path: 'playground', loadChildren: () => import('src/app/modules/playground/playground.module').then((m) => m.PlaygroundModule)},
   {path: 'ligatabelle', loadChildren: () => import('src/app/modules/ligatabelle/ligatabelle.module').then((m) => m.LigatabelleModule)},
   {path: 'ligatabelle/:id', loadChildren: () => import('src/app/modules/ligatabelle/ligatabelle.module').then((m) => m.LigatabelleModule)},
@@ -24,7 +23,6 @@ export const ROUTES: Routes = [
   {path: 'spotter', loadChildren: () => import('src/app/modules/spotter/spotter.module').then((m) => m.SpotterModule)},
   {path: 'schusszettel', loadChildren: () => import('./modules/schusszettel/schusszettel.module').then((m) => m.SchusszettelModule)},
   {path: 'hilfe', loadChildren: () => import('src/app/modules/hilfe/hilfe.module').then((m) => m.HilfeModule)},
-  {path: 'mannschaft/:id', loadChildren: () => import('src/app/modules/mannschaft/mannschaft.module').then((m) => m.MannschaftModule)},
   {path: 'mannschaft/:id/:mannschaftId', loadChildren: () => import('src/app/modules/mannschaft/mannschaft.module').then((m) => m.MannschaftModule)},
   {path: 'wkdurchfuehrung/tabletadmin/:id/:id/:id', loadChildren: () => import('src/app/modules/spotter/spotter.module').then((m) => m.SpotterModule)},
 ];
--- bogenliga/src/app/modules/mannschaft/components/mannschaft/mannschaft.component.ts
+++ bogenliga/src/app/modules/mannschaft/components/mannschaft/mannschaft.component.ts
@@ -1,16 +1,14 @@
 import {Component, OnInit} from '@angular/core';
-import {isUndefined} from "@shared/functions";
-import {CommonComponentDirective, toTableRows} from "@shared/components";
-import {ActivatedRoute, Router} from "@angular/router";
-import {BogenligaResponse} from "@shared/data-provider";
-import {DsbMannschaftDataProviderService} from "@verwaltung/services/dsb-mannschaft-data-provider.service";
-import {DsbMannschaftDTO} from "@verwaltung/types/datatransfer/dsb-mannschaft-dto.class";
-import {VereinDataProviderService} from "@verwaltung/services/verein-data-provider.service";
-import {VereinDTO} from "@verwaltung/types/datatransfer/verein-dto.class";
-import {MANNSCHAFT_CONFIG, MANNSCHAFTEN_TABLE_CONFIG} from "./mannschaft.config";
-import {TableRow} from "@shared/components/tables/types/table-row.class";
-import {MannschaftTabelleDO} from "@verwaltung/types/mannschfttabelle-do.class";
-import {TranslateService} from "@ngx-translate/core";
+import {isUndefined} from '@shared/functions';
+import {CommonComponentDirective} from '@shared/components';
+import {ActivatedRoute, Router} from '@angular/router';
+import {BogenligaResponse} from '@shared/data-provider';
+import {DsbMannschaftDataProviderService} from '@verwaltung/services/dsb-mannschaft-data-provider.service';
+import {DsbMannschaftDTO} from '@verwaltung/types/datatransfer/dsb-mannschaft-dto.class';
+import {VereinDTO} from '@verwaltung/types/datatransfer/verein-dto.class';
+import {MANNSCHAFT_CONFIG, MANNSCHAFTEN_TABLE_CONFIG} from './mannschaft.config';
+import {TableRow} from '@shared/components/tables/types/table-row.class';
+import {TranslateService} from '@ngx-translate/core';
 import {
   NotificationOrigin,
   NotificationService,
@@ -18,7 +16,7 @@ import {
   NotificationType,
   NotificationUserAction
 } from '@shared/services/notification';
-import {ActionButtonColors} from "@shared/components/buttons/button/actionbuttoncolors";
+import {ActionButtonColors} from '@shared/components/buttons/button/actionbuttoncolors';
 
 
 const MANNSCHAFT_PATH_PARAM = 'mannschaftId';
@@ -31,14 +29,16 @@ const VEREIN_PATH_PARAM = 'id';
   styleUrls: ['./mannschaft.component.scss']
 })
 export class MannschaftComponent extends CommonComponentDirective implements OnInit {
-  private mannschaftId: number | null = null;
-  private vereinId: number | null = null;
   public mannschaften: DsbMannschaftDTO[] | null = null;
   public verein: VereinDTO | null = null;
-  public currentMannschaft : DsbMannschaftDTO = new DsbMannschaftDTO()
+  public currentMannschaft: DsbMannschaftDTO = new DsbMannschaftDTO();
   public rows: TableRow[] | null = null;
   public loadingTable: boolean;
-  public tableContent: MannschaftTabelleDO[]=[];
+  public config = MANNSCHAFT_CONFIG;
+  readonly config_table = MANNSCHAFTEN_TABLE_CONFIG;
+  public readonly ActionButtonColors = ActionButtonColors;
+  private mannschaftId: number | null = null;
+  private vereinId: number | null = null;
 
   constructor(
     private router: Router,
@@ -54,10 +54,14 @@ export class MannschaftComponent extends CommonComponentDirective implements OnI
     this.mannschaftId = null;
 
     this.loading = true;
-    this.loadingTable =true;
+    this.loadingTable = true;
     this.route.params.subscribe((params) => {
       if (!isUndefined(params[VEREIN_PATH_PARAM])) {
-        this.vereinId = parseInt(params[VEREIN_PATH_PARAM], 10);
+        const new_id = parseInt(params[VEREIN_PATH_PARAM], 10);
+        if (new_id !== this.vereinId) {
+          this.mannschaften = null;
+        }
+        this.vereinId = new_id;
         // Load after we have the ID
         this.loadMannschaftenData();
       } else {
@@ -74,14 +78,15 @@ export class MannschaftComponent extends CommonComponentDirective implements OnI
     });
   }
 
-  loadMannschaftenData(): void{
-    // set loading state
+  loadMannschaftenData(): void {
+    if (this.mannschaften !== null ) { return; } // Don't load Mannschaften again if we already have them
+
     this.loading = true;
     this.mannschaftDataProvider.findAllByVereinsId(this.vereinId)
       .then((response: BogenligaResponse<DsbMannschaftDTO[]>) => {
-        console.log(response)
+        console.log(response);
         this.mannschaften = response.payload!;
-        if (this.mannschaften.length===0){
+        if (this.mannschaften.length === 0) {
           this.notificationService.showNotification({
             id: 'LigaIDWarning',
             description: '',
@@ -91,19 +96,18 @@ export class MannschaftComponent extends CommonComponentDirective implements OnI
             type: NotificationType.OK,
             severity: NotificationSeverity.INFO,
           });
-          this.router.navigate(['/home'])
+          this.router.navigate(['/home']);
         }
-        if (this.mannschaftId!=null){
-          this.currentMannschaft = this.mannschaften.find(mannschaft => mannschaft.id === this.mannschaftId)!;
-          if (this.currentMannschaft==null){
-            this.router.navigate(['/mannschaft',this.vereinId])
+        if (this.mannschaftId != null) {
+          this.currentMannschaft = this.mannschaften.find((mannschaft) => mannschaft.id === this.mannschaftId)!;
+          if (this.currentMannschaft == null) {
+            this.router.navigate(['/mannschaft', this.vereinId]);
           }
-        }else {
+        } else {
           this.currentMannschaft = this.mannschaften[0];
-          this.router.navigate([this.currentMannschaft.id], {relativeTo: this.route});
+          this.router.navigate(['../', this.currentMannschaft.id], {relativeTo: this.route});
         }
         this.loading = false;
-        //this.mannschaftenrows();
       })
       .catch((response: BogenligaResponse<DsbMannschaftDTO>) => {
         console.error(response);
@@ -112,44 +116,10 @@ export class MannschaftComponent extends CommonComponentDirective implements OnI
   }
 
   public onPushtoVerein(): void {
-    this.router.navigate(['/verein',this.vereinId]);
+    this.router.navigate(['/vereine', this.vereinId]);
   }
 
   public onSelectMannschaft(): void {
-    this.router.navigate([this.currentMannschaft.id], {relativeTo: this.route});
+    this.router.navigate(['../', this.currentMannschaft.id], {relativeTo: this.route});
   }
-
-  /*mannschaftenrows(): void{
-    this.mannschaften.forEach(mannschaft =>{
-      const test: MannschaftTabelleDO = new MannschaftTabelleDO(mannschaft.name, mannschaft.id);
-      this.tableContent.push(test)
-    });
-
-    this.rows = toTableRows(this.tableContent);
-    this.tableContent = [];
-    this.loadingTable = false;
-  }*/
-
-  public async getSelectedRow($event): Promise<void> {
-    const rowValues = $event;
-    console.log(rowValues);
-    this.router.navigate(['/mannschaftsuebersicht',rowValues.id]);
-  }
-
-  private updateBreadcrumb(): void {
-    const name = this.currentMannschaft?.name || this.translate.instant('MANNSCHAFT.STATUS.UNNAMED') || 'unbenannte Mannschaft';
-    this.config = {
-      ...this.config,
-      //navigationCardsConfig = new class implements NavigationCardsConfig {},
-      //breadcrumb: [
-      //  { label: this.translate.instant('NAV.HOME'), routerLink: ['/'] },
-      //  { label: this.translate.instant('MANNSCHAFT.LIST'), routerLink: ['/mannschaften'] },
-      //  { label: name }
-      //]
-    };
-  }
-
-  public config = MANNSCHAFT_CONFIG;
-  readonly config_table = MANNSCHAFTEN_TABLE_CONFIG;
-  protected readonly ActionButtonColors = ActionButtonColors;
 }
