ed4b58b48104a6cf52750d17e977050af429548e
Jakob <jakob.frey@student.reutlingen-university.de>
2026-01-22 00:22:34 +0100

    cleanup routing

--- bogenliga/src/app/app.routing.ts
+++ bogenliga/src/app/app.routing.ts
@@ -14,6 +14,8 @@ export const ROUTES: Routes = [
   {path: 'wkdurchfuehrung/:id', loadChildren: () => import('src/app/modules/wkdurchfuehrung/wkdurchfuehrung.module').then((m) => m.WkdurchfuehrungModule)},
   {path: 'user', loadChildren: () => import('src/app/modules/user/user.module').then((m) => m.UserModule)},
   {path: 'vereine/:id/:mannschaftId', loadChildren: () => import('src/app/modules/mannschaft/mannschaft.module').then((m) => m.MannschaftModule)},
+  {path: 'vereine', loadChildren: () => import('src/app/modules/vereinsmannschaftenuebersicht/vereinsmannschaftenuebersicht.module').then((m) => m.VereinsmannschaftenuebersichtModule)},
+  {path: 'vereine/:id', loadChildren: () => import('src/app/modules/vereinsmannschaftenuebersicht/vereinsmannschaftenuebersicht.module').then((m) => m.VereinsmannschaftenuebersichtModule)},
   {path: 'playground', loadChildren: () => import('src/app/modules/playground/playground.module').then((m) => m.PlaygroundModule)},
   {path: 'ligatabelle', loadChildren: () => import('src/app/modules/ligatabelle/ligatabelle.module').then((m) => m.LigatabelleModule)},
   {path: 'ligatabelle/:id', loadChildren: () => import('src/app/modules/ligatabelle/ligatabelle.module').then((m) => m.LigatabelleModule)},
@@ -21,7 +23,5 @@ export const ROUTES: Routes = [
   {path: 'spotter', loadChildren: () => import('src/app/modules/spotter/spotter.module').then((m) => m.SpotterModule)},
   {path: 'schusszettel', loadChildren: () => import('./modules/schusszettel/schusszettel.module').then((m) => m.SchusszettelModule)},
   {path: 'hilfe', loadChildren: () => import('src/app/modules/hilfe/hilfe.module').then((m) => m.HilfeModule)},
-  {path: 'vereine', loadChildren: () => import('src/app/modules/vereinsmannschaftenuebersicht/vereinsmannschaftenuebersicht.module').then((m) => m.VereinsmannschaftenuebersichtModule)},
-  {path: 'vereine/:id', loadChildren: () => import('src/app/modules/vereinsmannschaftenuebersicht/vereinsmannschaftenuebersicht.module').then((m) => m.VereinsmannschaftenuebersichtModule)},
   {path: 'wkdurchfuehrung/tabletadmin/:id/:id/:id', loadChildren: () => import('src/app/modules/spotter/spotter.module').then((m) => m.SpotterModule)},
 ];
