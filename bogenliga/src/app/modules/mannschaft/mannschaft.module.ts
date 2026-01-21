2446d41982c139a99a9f26e65ec6c697eb450932
Jakob <jakob.frey@student.reutlingen-university.de>
2026-01-21 10:28:21 +0100

    remove Gesamtstatistik & rename component
    Gesamstatistik was redundant so we removed it

--- bogenliga/src/app/modules/mannschaft/mannschaft.module.ts
+++ bogenliga/src/app/modules/mannschaft/mannschaft.module.ts
@@ -4,7 +4,7 @@ import {FormsModule} from '@angular/forms';
 import {RouterModule} from '@angular/router';
 import {SharedModule} from '@shared/shared.module';
 import {MANNSCHAFTSUEBERSICHT_ROUTES} from './mannschaft.routing';
-import {MannschaftComponent, MannschaftTextComponent} from './components';
+import {MannschaftComponent, MannschaftTableComponent} from './components';
 
 @NgModule({
   imports: [
@@ -13,8 +13,8 @@ import {MannschaftComponent, MannschaftTextComponent} from './components';
     SharedModule,
     FormsModule
   ],
-  declarations: [MannschaftComponent, MannschaftTextComponent],
-  exports: [MannschaftComponent, MannschaftTextComponent]
+  declarations: [MannschaftComponent, MannschaftTableComponent],
+  exports: [MannschaftComponent, MannschaftTableComponent]
 })
 export class MannschaftModule {
 }
