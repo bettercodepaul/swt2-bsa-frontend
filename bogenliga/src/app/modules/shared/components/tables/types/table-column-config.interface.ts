7d37d657b99f93a144e81bfeb5b29deb013efb75
Sven <sven.sudin@student.reutlingen-university.de>
2026-01-21 21:26:48 +0100

    Added functionality for Tables to handle links as actionbuttons and added two additional target parameters to support up to two differnt links in a single row with linkTarget: 'own' | 'opponent'

--- bogenliga/src/app/modules/shared/components/tables/types/table-column-config.interface.ts
+++ bogenliga/src/app/modules/shared/components/tables/types/table-column-config.interface.ts
@@ -7,7 +7,7 @@ export interface TableColumnConfig {
   translationKey: string; // key of the column
   propertyName?: string; // to access the payload parameter field
   propertyMapper?: Function; // if defined maps extracted property
-
+  linkTarget?: string;
   type?: TableColumnType; // optional: Use a specific data type pipe; default = TEXT
   localizationSet?: string; // optional: Used with TableColumnType.TRANSLATION_KEY
   width?: number; // optional: Set the column width in percent, e.g. '25'; default = width of content
