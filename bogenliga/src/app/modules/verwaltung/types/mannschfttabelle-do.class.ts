80c08936edc6f708fd0eb1511333835e30e48ab0
Cedric Rollinger <cedric.ro@gmx.de>
2026-02-12 13:50:09 +0100

    Enhance MannschaftTabelleDO constructor and update routing with LigaResolver

--- bogenliga/src/app/modules/verwaltung/types/mannschfttabelle-do.class.ts
+++ bogenliga/src/app/modules/verwaltung/types/mannschfttabelle-do.class.ts
@@ -1,14 +1,16 @@
-import {VersionedDataObject} from '../../shared/data-provider/models/versioned-data-object.interface';
+import {VersionedDataObject} from '@shared/data-provider/models/versioned-data-object.interface';
 
 export class MannschaftTabelleDO implements VersionedDataObject {
 
-  constructor(name: string, id: number) {
+  constructor(name: string, id: number, veranstaltungId: number) {
     this.name = name;
     this.id = id;
     this.version = 1;
+    this.veranstaltungId = veranstaltungId;
   }
 
   id: number;
   name: string;
   version: number;
+  veranstaltungId: number;
 }
