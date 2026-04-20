d96bc7a5e1853ea23d7476b8a48154f86fbdd722
Annika Hartung <annika.hartung@student.reutlingen-university.de>
2026-04-20 20:52:13 +0200

    added cypress tests for start buttons

--- bogenliga/cypress.config.ts
+++ bogenliga/cypress.config.ts
@@ -1,12 +1,20 @@
-import { defineConfig } from 'cypress';
+import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config);
+      return require("./cypress/plugins/index.js")(on, config);
     },
-    testIsolation: false
+    testIsolation: false,
+  },
+
+  component: {
+    devServer: {
+      framework: "angular",
+      bundler: "webpack",
+    },
+    specPattern: "**/*.cy.ts",
   },
 });
