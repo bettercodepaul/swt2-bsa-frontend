6dedf2cad7afc4bc46add2b879d332e9faffc9cb
AlexanderFriesch <Alexander.Friesch@Student.Reutlingen-University.DE>
2026-04-21 17:19:56 +0200

    now works like before the DB changes

--- bogenliga/src/app/modules/shared/services/current-user/types/user-permission.enum.ts
+++ bogenliga/src/app/modules/shared/services/current-user/types/user-permission.enum.ts
@@ -97,4 +97,5 @@ export enum UserPermission {
 
   //Spezielles Recht für den Ligaleiter der untersten Region, der eine neue Liga als unterste Liga anlegen darf und diese bearbeiten kann
   CAN_CREATE_MY_LIGA,
+  CAN_READ_SYSTEMDATEN_SPORTLEITER
 }
