feb25b00d63cb2fc34e486b3a69a4fe131e59901
Anonymous <anonymous@example.com>
2026-05-07 18:43:40 +0200

    Anzeigen Manager minor typo fix

--- bogenliga/src/app/modules/wkdurchfuehrung/wkdurchfuehrung.routing.ts
+++ bogenliga/src/app/modules/wkdurchfuehrung/wkdurchfuehrung.routing.ts
@@ -6,7 +6,8 @@ import {SchusszettelComponent} from './components/schusszettel/schusszettel.comp
 import {TabletEingabeComponent} from './components/tableteingabe/tableteingabe.component';
 import {TabletAdminComponent} from './components/tablet-admin/tablet-admin.component';
 import {WkdurchfuehrungGuard} from './guards/wkdurchfuehrung.guard';
-import {AnzeigeManagerComponent, FullscreenComponent} from '@wkdurchfuehrung/components';
+import {FullscreenComponent} from '@wkdurchfuehrung/components';
+import {AnzeigeManagerComponent} from '@wkdurchfuehrung/components';
 import {TabletAdminPopUpComponent} from './components/tablet-admin/tablet-admin-pop-up/tablet-admin-pop-up.component';
 import {SchusszettelTabletAdminComponent} from '@schusszettel/components/setup/schusszettel-tablet-admin.component';
 
