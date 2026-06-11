29f53498a6eaa98b55eb460d4209c1489b679201
Scheuffen <zubarewsergej@gmail.com>
2026-06-11 22:21:46 +0200

    feat(frontend): zeige eigene Fehlermeldung bei Löschsperre nach Meldedeadline an
    - i18n
    - permission for sportleiter
    - onDeleteMannschaft changed
    - modified deleteById

--- bogenliga/src/app/modules/verwaltung/components/verein/verein-detail/verein-detail.config.ts
+++ bogenliga/src/app/modules/verwaltung/components/verein/verein-detail/verein-detail.config.ts
@@ -43,7 +43,7 @@ export const VEREIN_DETAIL_TABLE_CONFIG: TableConfig = {
       ],
       width:       15
     },
-    deletePermission : [UserPermission.CAN_DELETE_STAMMDATEN],
+    deletePermission : [UserPermission.CAN_MODIFY_MY_VEREIN, UserPermission.CAN_DELETE_STAMMDATEN],
     editPermission: [UserPermission.CAN_MODIFY_MY_VEREIN, UserPermission.CAN_MODIFY_STAMMDATEN_LIGALEITER],
     addPermission: [UserPermission.CAN_MODIFY_MY_VEREIN, UserPermission.CAN_MODIFY_STAMMDATEN_LIGALEITER]
 
