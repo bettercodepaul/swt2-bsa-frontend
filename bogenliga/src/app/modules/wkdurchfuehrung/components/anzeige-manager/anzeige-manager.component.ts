a28cee0fdd421c3230b6c6c26f6af008a74744c4
TabeaKV <tabea.kirchmaier@gmail.com>
2026-06-18 13:49:14 +0200

    Added Pop up that opens when Button to generate ID is clicked

--- bogenliga/src/app/modules/wkdurchfuehrung/components/anzeige-manager/anzeige-manager.component.ts
+++ bogenliga/src/app/modules/wkdurchfuehrung/components/anzeige-manager/anzeige-manager.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
 import {BogenligaResponse} from '@shared/data-provider';
 import {isNullOrUndefined} from '@shared/functions';
 import {AnzeigenDO} from '@wkdurchfuehrung/types/anzeige-do.class';
@@ -12,7 +12,7 @@ import {ActivatedRoute} from '@angular/router';
   styleUrls: ['./anzeige-manager.component.scss']
 })
 
-export class AnzeigeManagerComponent implements OnInit {
+export class AnzeigeManagerComponent implements OnInit, OnDestroy {
   /*
   *  AnzeigenProviderService hat oben das hier:
   *   @Injectable({
@@ -27,6 +27,36 @@ export class AnzeigeManagerComponent implements OnInit {
 
   public wettkampfId: number;
 
+  // Für das Fenster der generierten ID
+  public IdGeneratePopup = false;
+  public generatedId = 'test';
+  public popupPosition = {x: 120, y: 120};
+
+  private isDragging = false;
+  private dragOffsetX = 0;
+  private dragOffsetY = 0;
+
+  // Fenster mit dem Mauszeiger ist bewegbar
+  private onWindowMouseMove = (ev: MouseEvent) => {
+    if (!this.isDragging) {
+      return;
+    }
+    this.popupPosition.x = ev.clientX - this.dragOffsetX;
+    this.popupPosition.y = ev.clientY - this.dragOffsetY;
+    if (this.popupPosition.x < 0) {
+      this.popupPosition.x = 0;
+    }
+    if (this.popupPosition.y < 0) {
+      this.popupPosition.y = 0;
+    }
+  }
+
+  private onWindowMouseUp = (_ev: MouseEvent) => {
+    if (this.isDragging) {
+      this.isDragging = false;
+    }
+  }
+
   constructor(private anzeigenProvider: AnzeigenProviderService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
@@ -34,7 +64,16 @@ export class AnzeigeManagerComponent implements OnInit {
     this.route.paramMap.subscribe((params) => {
       this.wettkampfId = parseInt(params.get('selectedWettkampfId'), 10);
     });
+    window.addEventListener('mousemove', this.onWindowMouseMove);
+    window.addEventListener('mouseup', this.onWindowMouseUp);
   }
+
+  ngOnDestroy(): void {
+    window.removeEventListener('mousemove', this.onWindowMouseMove);
+    window.removeEventListener('mouseup', this.onWindowMouseUp);
+  }
+
+
 
   public nextMatch(): void {
     this.currentMatch++;
@@ -90,5 +129,50 @@ export class AnzeigeManagerComponent implements OnInit {
     });
   }
 
-  public showGeneratedID(): void{}
+  public showGeneratedID(): void {
+    this.IdGeneratePopup = true;
+    this.popupPosition = {x: 120, y: 120};
+  }
+
+  public closeGeneratedIdPopup(): void {
+    this.IdGeneratePopup = false;
+    this.isDragging = false;
+  }
+
+  public startDrag(event: MouseEvent): void {
+    if (event.button !== 0) {
+      return;
+    }
+    this.isDragging = true;
+    this.dragOffsetX = event.clientX - this.popupPosition.x;
+    this.dragOffsetY = event.clientY - this.popupPosition.y;
+    event.preventDefault();
+  }
+
+  public copyGeneratedId(): void {
+    if (!this.generatedId) {
+      alert('Es ist noch keine ID vorhanden.');
+      return;
+    }
+
+    if (navigator.clipboard && navigator.clipboard.writeText) {
+      navigator.clipboard.writeText(this.generatedId).then(() => {
+        alert('ID kopiert');
+      }, () => {
+        alert('Kopieren fehlgeschlagen');
+      });
+    } else {
+      const ta = document.createElement('textarea');
+      ta.value = this.generatedId;
+      document.body.appendChild(ta);
+      ta.select();
+      try {
+        document.execCommand('copy');
+        alert('ID kopiert');
+      } catch (e) {
+        alert('Kopieren fehlgeschlagen');
+      }
+      document.body.removeChild(ta);
+    }
+  }
 }
