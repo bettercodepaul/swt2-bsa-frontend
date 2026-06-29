70bd5dd1597dd4ea2ebb3a1a18892ad4abc48cb8
Rama Arabi <rama.arabi@student.reutlingen-university.de>
2026-06-29 21:45:23 +0200

    test: stabilize frontend unit test suite

--- bogenliga/src/app/modules/shared/components/buttons/shortcut-button/shortcut-button.component.spec.ts
+++ bogenliga/src/app/modules/shared/components/buttons/shortcut-button/shortcut-button.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ShortcutButton } from './shortcut-button.component';
+import {CurrentUserService} from '@shared/services';
+import {LoginDataProviderService} from '@user/services/login-data-provider.service';
+import {UserDataProviderService} from '@verwaltung/services/user-data-provider.service';
 
 describe('ShortcutButtonComponent', () => {
   let component: ShortcutButton;
@@ -8,7 +11,31 @@ describe('ShortcutButtonComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ShortcutButton ]
+      declarations: [ ShortcutButton ],
+      providers: [
+        {
+          provide: CurrentUserService,
+          useValue: {
+            getVerein: () => 1,
+            getCurrentUserID: () => 1,
+            hasAnyPermisson: () => true,
+            hasAnyRole: () => true,
+            isLoggedIn: () => true
+          }
+        },
+        {
+          provide: UserDataProviderService,
+          useValue: {
+            findUserRoleById: () => Promise.resolve({payload: [{roleName: 'ADMIN'}]})
+          }
+        },
+        {
+          provide: LoginDataProviderService,
+          useValue: {
+            signInDefaultUser: () => Promise.resolve()
+          }
+        }
+      ]
     })
     .compileComponents();
   });
