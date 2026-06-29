3dd9b3cbc954ba2d168ed80d4eb11c290d190202
TabeaKV <tabea.kirchmaier@gmail.com>
2026-06-29 18:33:34 +0200

    added fallback method so screen registration screen updates once ID is typed in

--- bogenliga/src/app/modules/wkdurchfuehrung/services/anzeigen-provider.service.ts
+++ bogenliga/src/app/modules/wkdurchfuehrung/services/anzeigen-provider.service.ts
@@ -102,4 +102,19 @@ export class AnzeigenProviderService extends DataProviderService {
         });
     });
   }
+
+  public findAll(): Promise<BogenligaResponse<AnzeigenDO[]>> {
+    return new Promise((resolve, reject) => {
+      this.restClient.GET(this.getUrl())
+        .then((data: any) => {
+          resolve({result: RequestResult.SUCCESS, payload: data});
+        }, (error: HttpErrorResponse) => {
+          if (error.status === 0) {
+            reject({result: RequestResult.CONNECTION_PROBLEM});
+          } else {
+            reject({result: RequestResult.FAILURE});
+          }
+        });
+    });
+  }
 }
