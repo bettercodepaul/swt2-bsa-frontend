abb06b0f4ec27ab3cfd2f17f17d72b3eb4f8b62d
LexyRoo <benjamin.paulus@student.reutlingen-university.de>
2026-07-01 23:55:25 +0200

    base for polling

--- bogenliga/src/app/modules/wkdurchfuehrung/components/anzeige-physische_ID/anzeige_physische_ID.component.ts
+++ bogenliga/src/app/modules/wkdurchfuehrung/components/anzeige-physische_ID/anzeige_physische_ID.component.ts
@@ -1,20 +1,32 @@
-import {Component, OnInit, HostListener} from '@angular/core';
+import {Component, OnInit, HostListener, OnDestroy} from '@angular/core';
 import {AnzeigenProviderService} from '@wkdurchfuehrung/services/anzeigen-provider.service';
+import {interval, Subscription} from 'rxjs';
 
 @Component({
   selector: 'bla-screen-registration',
   templateUrl: './anzeige_physische_ID.component.html',
   styleUrls: ['./anzeige_physische_ID.component.scss']
 })
-export class AnzeigePhysischeIDComponent implements OnInit {
+export class AnzeigePhysischeIDComponent implements OnInit, OnDestroy {
   physischeBildschirmID = '';
   isFullscreen = true;
+  private pollingSubscription!: Subscription;
 
   constructor(private anzeigenProvider: AnzeigenProviderService) {}
 
   async ngOnInit(): Promise<void> {
     await this.showGeneratedID();
     this.activateFullscreen();
+    this.pollingSubscription = interval(30000).subscribe(() => {
+      // findAnzeigenMatchByPhysischeBildschirmId(physischeBildschirmID);
+    });
+  }
+
+  async ngOnDestroy() {
+    // Verhindert Memory Leaks, falls die Komponente jemals zerstört wird
+    if (this.pollingSubscription) {
+      this.pollingSubscription.unsubscribe();
+    }
   }
 
   public async showGeneratedID(): Promise<void> {
