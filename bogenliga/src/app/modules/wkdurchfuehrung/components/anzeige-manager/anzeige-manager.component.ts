6952cb30f66a54cc841cb7905cc52b62a1adb725
TabeaKV <tabea.kirchmaier@gmail.com>
2026-07-01 20:55:13 +0200

    removed falsley added routing from anzeige-manager

--- bogenliga/src/app/modules/wkdurchfuehrung/components/anzeige-manager/anzeige-manager.component.ts
+++ bogenliga/src/app/modules/wkdurchfuehrung/components/anzeige-manager/anzeige-manager.component.ts
@@ -3,8 +3,7 @@ import {BogenligaResponse} from '@shared/data-provider';
 import {isNullOrUndefined} from '@shared/functions';
 import {AnzeigenDO} from '@wkdurchfuehrung/types/anzeige-do.class';
 import {AnzeigenProviderService} from '@wkdurchfuehrung/services/anzeigen-provider.service';
-import {ActivatedRoute} from '@angular/router';
-
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'bla-anzeige-manager',
@@ -26,25 +25,15 @@ export class AnzeigeManagerComponent implements OnInit {
   public currentMatch = 1;
 
   public wettkampfId: number;
-  public veranstaltungId: number;
-  public wettkampftag = 1;
 
-  constructor(private anzeigenProvider: AnzeigenProviderService, private route: ActivatedRoute) {}
+  constructor(private anzeigenProvider: AnzeigenProviderService, private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit(): void {
     // Holt den in der wkdurchfuehrung.routing.ts definierten Parameter aus der URL
     this.route.paramMap.subscribe((params) => {
       this.wettkampfId = parseInt(params.get('selectedWettkampfId'), 10);
-      this.veranstaltungId = parseInt(params.get('veranstaltungId'), 10);
-      const wettkampftagParam = parseInt(params.get('wettkampftag'), 10);
-      if (!isNaN(wettkampftagParam)) {
-        this.wettkampftag = wettkampftagParam;
-      }
-
-      if (!isNaN(this.wettkampfId)) {
-        this.loadDisplays();
-      }
     });
+    this.loadDisplays();
   }
 
   public async loadDisplays(): Promise<void> {
